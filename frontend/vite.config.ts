272ddec64713489928a8d7dae49a5d61080a7084
KIMHYEONJI <hyeonji0718@gmail.com>
2024-04-24 17:12:13 +0900

    ✨ feat : 절대 경로 설정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'window',
+  },
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '@apis', replacement: path.resolve(__dirname, 'src/apis') },
+      { find: '@common', replacement: path.resolve(__dirname, 'src/common') },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, 'src/components'),
+      },
+      { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
+      { find: '@stores', replacement: path.resolve(__dirname, 'src/stores') },
+      { find: '@style', replacement: path.resolve(__dirname, 'src/style') },
+      { find: '@hooks', replacement: path.resolve(__dirname, 'src/hooks') },
+      {
+        find: '@constants',
+        replacement: path.resolve(__dirname, 'src/constants'),
+      },
+    ],
+  },
 })
