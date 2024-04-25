9cd061b17276d7eb597dd7ffc16e3e56b27b9721
KIMHYEONJI <hyeonji0718@gmail.com>
2024-04-25 11:31:13 +0900

    ✨ feat : 예시 route 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
 import '@/App.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import HomePage from '@pages/HomePage'
+
+const HomeRoutes = () => (
+  <Routes>
+    <Route path="/" element={<HomePage />} />
+  </Routes>
+)
 
 function App() {
   return (
     <BrowserRouter>
+      <HomeRoutes />
       <ReactQueryDevtools initialIsOpen={false} />
     </BrowserRouter>
   )
