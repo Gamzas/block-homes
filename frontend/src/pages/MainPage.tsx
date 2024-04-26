f9caaa7260ffa724583b2ce408dde9115d9fbc94
KIMHYEONJI <hyeonji0718@gmail.com>
2024-04-26 12:03:37 +0900

    ✨ feat : mainPage에 TopCard 컴포넌트 추가

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,11 +1,13 @@
 import * as h from '@pages/style/MainPageStyle'
 import Header from '@common/Header'
 import Footer from '@common/Footer'
+import TopCard from '@components/MainPage/TopCard'
 
 const MainPage = () => {
   return (
     <h.Container>
       <Header />
+      <TopCard />
       <Footer />
     </h.Container>
   )
