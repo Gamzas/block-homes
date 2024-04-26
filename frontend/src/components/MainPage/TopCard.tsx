1e8e7e78d38a77b7db3936f02e90df8c219455ec
KIMHYEONJI <hyeonji0718@gmail.com>
2024-04-26 10:50:46 +0900

    ✨ feat : top-card 상단에 위치하는 top card header 생성

--- frontend/src/components/MainPage/TopCard.tsx
+++ frontend/src/components/MainPage/TopCard.tsx
@@ -1,5 +1,14 @@
+import * as t from '@components/MainPage/style/TopCardStyle'
+
 const TopCard = () => {
-  return <div></div>
+  return (
+    <t.TopCardContainer>
+      <t.TopCardHeader>
+        <image className="logo">로고</image>
+        <button className="loginBtn">로그인</button>
+      </t.TopCardHeader>
+    </t.TopCardContainer>
+  )
 }
 
 export default TopCard
