4abf49aeecb4da9f65739731517f9b9a158f9392
정유나 <jyn7077@naver.com>
2024-04-26 15:04:08 +0900

    ✨ feat : 부동산 매물 조회 페이지 route 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,12 @@ import '@/App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import HomePage from '@pages/HomePage'
+import RealEstatePage from '@pages/RealEstatePage'
 
 const HomeRoutes = () => (
   <Routes>
     <Route path="/" element={<HomePage />} />
+    <Route path="/estate" element={<RealEstatePage />} />
   </Routes>
 )
 
