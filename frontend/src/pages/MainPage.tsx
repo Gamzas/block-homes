2a72ca321dacf620bfc25820cc1b7df5d9c4cdaa
KIMHYEONJI <hyeonji0718@gmail.com>
2024-04-26 15:19:54 +0900

    ✨ feat : mainPage에 RealEstateCategory 컴포넌트 추가

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -2,12 +2,14 @@ import * as h from '@pages/style/MainPageStyle'
 import Header from '@common/Header'
 import Footer from '@common/Footer'
 import TopCard from '@components/MainPage/TopCard'
+import RealEstateCategory from '@components/MainPage/RealEstateCategory'
 
 const MainPage = () => {
   return (
     <h.Container>
       <Header />
       <TopCard />
+      <RealEstateCategory />
       <Footer />
     </h.Container>
   )
