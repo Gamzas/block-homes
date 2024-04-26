a93dfd02f1d932beab9b30f537662b745cc6bc6c
정유나 <jyn7077@naver.com>
2024-04-26 15:03:40 +0900

    🎨 style : 지도 style 적용

---
+++ frontend/src/components/EstateList/styles/EstateListMapStyle.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const EstateMapContainer = styled.div`
+  width: 100vw;
+  height: 80vh;
+`
+
+export const CurrentLocation = styled.div`
+  display: flex;
+  width: 20vw;
+  height: 26px;
+  flex-direction: column;
+  justify-content: center;
+  flex-shrink: 0;
+  .location-icon {
+  }
+  .current-location {
+    font-weight: 600;
+}
+`
