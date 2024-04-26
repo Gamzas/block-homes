da64c0363f032c3bf9f563d5d1dc3a518e793f95
오기선 <pakox123@gmail.com>
2024-04-26 15:22:24 +0900

    feat : 계약서 라우터 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,12 +5,21 @@ import MainPage from '@pages/MainPage'
 import { GlobalStyle } from '@style/GlobalStyles'
 import RealEstatePage from '@pages/RealEstatePage'
 import SmartContractStart from './pages/SmartContractStart'
+import SmartContractAgreement from './pages/SmartContractAgreement'
+import SmartContractMain from './pages/SmartContractMain'
+import SmartContractPayment from './pages/SmartContractPayment'
 
 const HomeRoutes = () => (
   <Routes>
     <Route path="/" element={<MainPage />} />
     <Route path="/estate" element={<RealEstatePage />} />
     <Route path="/smart-contract-start" element={<SmartContractStart />} />
+    <Route
+      path="/smart-contract-agreement"
+      element={<SmartContractAgreement />}
+    />
+    <Route path="/smart-contract-main" element={<SmartContractMain />} />
+    <Route path="/smart-contract-payment" element={<SmartContractPayment />} />
   </Routes>
 )
 
