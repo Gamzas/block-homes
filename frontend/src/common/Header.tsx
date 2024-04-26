e625bdc0d1f20d6de5e3cbdcf3692f52bd3dd38e
KIMHYEONJI <hyeonji0718@gmail.com>
2024-04-26 16:58:52 +0900

    ✨ feat : HeaderProps가 적절하게 배치되도록 Header component 구체화

--- frontend/src/common/Header.tsx
+++ frontend/src/common/Header.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import * as h from '@common/style/HeaderStyle'
 import { HeaderPropsType } from '@/types/header'
 
 const Header: React.FC<HeaderPropsType> = HeaderProps => {
-  return <div></div>
+  return (
+    <h.HeaderContainer>
+      <img
+        className="back-arrow"
+        alt="뒤로가기"
+        src="public/icon/icon_back_arrow.png"
+      />
+      {HeaderProps.isSearch ? (
+        <div className="title">{HeaderProps.title}</div>
+      ) : (
+        <h.SearchContainer></h.SearchContainer>
+      )}
+      {HeaderProps.rightIconSrc && (
+        <img
+          className="right-icon"
+          alt="오른쪽 아이콘"
+          src={`${HeaderProps.rightIconSrc}`}
+        />
+      )}
+    </h.HeaderContainer>
+  )
 }
 
 export default Header
