ff23c911f7912e7bf799a9b20a9e44ec952b14ee
KIMHYEONJI <hyeonji0718@gmail.com>
2024-04-26 15:18:08 +0900

    ✨ feat : 부동산 종류 더미 데이터 추가

--- frontend/src/components/MainPage/RealEstateCategory.tsx
+++ frontend/src/components/MainPage/RealEstateCategory.tsx
@@ -1,4 +1,33 @@
+import { RealEstateCategoryType } from '@/types/realEstate'
+
 const RealEstateCategory = () => {
+  const realEstateCategoryList: RealEstateCategoryType[] = [
+    {
+      src: 'public/image/image_apartment.png',
+      title: '아파트',
+      transactionType1: '매매',
+      transactionType2: '전 · 월세',
+    },
+    {
+      src: 'public/image/image_villa_or_tworoom.png',
+      title: '빌라 · 투룸 +',
+      transactionType1: '매매',
+      transactionType2: '전 · 월세',
+    },
+    {
+      src: 'public/image/image_oneroom.png',
+      title: '원룸',
+      transactionType1: '전 · 월세',
+      transactionType2: null,
+    },
+    {
+      src: 'public/image/image_officetels.png',
+      title: '오피스텔',
+      transactionType1: '전 · 월세',
+      transactionType2: null,
+    },
+  ]
+
   return <div></div>
 }
 
