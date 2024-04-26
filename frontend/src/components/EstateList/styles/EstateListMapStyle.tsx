1197ae80b4ecc7b662b3d80caf9b193401bd04bd
정유나 <jyn7077@naver.com>
2024-04-26 17:42:04 +0900

    ✨ feat : 현재 위치 표시 컴포넌트 추가

--- frontend/src/components/EstateList/styles/EstateListMapStyle.tsx
+++ frontend/src/components/EstateList/styles/EstateListMapStyle.tsx
@@ -2,19 +2,36 @@ import styled from 'styled-components'
 
 export const EstateMapContainer = styled.div`
   width: 100%;
-  height: 80vh;
+  height: 100vh;
+  position: relative;
+  z-index: 0;
 `
 
-export const CurrentLocation = styled.div`
+export const CurrentLocationContainer = styled.div`
   display: flex;
-  width: 20vw;
-  height: 26px;
-  flex-direction: column;
   justify-content: center;
+  align-items: center;
+  width: 6rem;
+  height: 2rem;
+  // ---------------------------------
+  background-color: white;
+  border-radius: 1rem;
+  /* border: 0.1rem solid; */
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  // --------------------------------------
   flex-shrink: 0;
+  position: absolute;
+  z-index: 3;
+  top: 2%; // 상단에서부터의 위치를 설정
+  left: 2%; // 좌측에서부터의 위치를 설정
   .location-icon {
+    width: 1rem;
+    height: 1rem;
+    margin-right: 0.6rem;
   }
   .current-location {
     font-weight: 600;
+    font-size: 1rem;
+    line-height: 1.5rem;
   }
 `
