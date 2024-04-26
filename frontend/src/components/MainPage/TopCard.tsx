ff7ca541bab93ffae8e35109cc60092727d1c3e4
KIMHYEONJI <hyeonji0718@gmail.com>
2024-04-26 11:01:42 +0900

    ✨ feat : top-card 중간에 위치해 서비스의 정보를 알려주는 TopCardInfoTextContainer 생성

--- frontend/src/components/MainPage/TopCard.tsx
+++ frontend/src/components/MainPage/TopCard.tsx
@@ -7,6 +7,14 @@ const TopCard = () => {
         <image className="logo">로고</image>
         <button className="loginBtn">로그인</button>
       </t.TopCardHeader>
+      <t.TopCardInfoTextContainer>
+        <div className="mainInfoText">
+          로그인하고 <br />
+          안전하고 쉬운 부동산 거래를 <br />
+          시작해보세요!
+        </div>
+        <div className="infoReport">매물 안전도 리스트란?</div>
+      </t.TopCardInfoTextContainer>
     </t.TopCardContainer>
   )
 }
