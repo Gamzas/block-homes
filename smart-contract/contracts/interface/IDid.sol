974873114ca5553cbf941e09c27cb2d09a7c0f81
천세진 <sejinnnnnn@naver.com>
2024-04-26 15:05:36 +0900

    ✨ feat : Did Interface 추가

---
+++ smart-contract/contracts/interface/IDid.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+interface Idid {
+
+    // 
+    // event DeactivateID(string did);
+
+    // function deactivateID(string calldata did, bytes calldata signer) external;
+
+
+    // event AddKey(string did, bytes publicKey, string[] controller);
+
+    // function addKey(string calldata did, bytes calldata newPublicKey, string[] calldata publicKeyController, bytes calldata signer) external;
+
+    
+    // event AddAddress(string did, address addr, string[] controller);
+
+    // function addAddress(string calldata did, address addr, string[] calldata publicKeyController, bytes calldata signer) external;
+
+
+    // event DeactivateKey(string did, bytes publicKey);
+
+    // function deactivateKey(string calldata did, bytes calldata publicKey, bytes calldata signer) external;
+
+
+    // event DeactivateAddress(string did, address addr);
+
+    // function deactivateAddress(string calldata did, address addr, bytes calldata signer) external;
+
+
+    // event AddNewAuthKey(string did, bytes publicKey, string[] controller);
+
+    // function addNewAuthKey(string calldata did, bytes calldata publicKey, string[] calldata controller, bytes calldata signer) external;
+
+    // function addNewAuthKeyByController(string calldata did, bytes calldata publicKey, string[] calldata controller, 
+    //     string calldata controllerSigner, bytes calldata signer) external;
+
+    
+    // event SetAuthKey(string did, bytes publicKey);
+
+    // function setAuthKey(string calldata did, bytes calldata publicKey, bytes calldata signer) external;
+
+    // function setAuthKeyByController(string calldata did, bytes calldata publicKey, string calldata controller, bytes calldata signer) external;
+
+
+    // event SetAuthAddress(string did, address addr);
+
+    // function setAuthAddress(string calldata did, address addr, bytes calldata signer) external;
+
+    // function setAuthAddressByController(string calldata did, address addr, string calldata controller, bytes calldata signer) external;
+
+
+    // event DeactivateAuthKey(string did, bytes publicKey);
+
+    // function deactivateAuthKey(string calldata did, bytes calldata publicKey, bytes calldata signer) external;
+
+    // function deactivateAuthKeyByController(string calldata did, bytes calldata publicKey, string calldata controller, bytes calldata signer) external;
+
+
+}
