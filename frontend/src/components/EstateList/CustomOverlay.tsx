4944a94401c42080109edaa406de2a0a6bdee4e8
정유나 <jyn7077@naver.com>
2024-04-26 15:02:54 +0900

    ✨ feat : 부동산 매물 지도 커스텀 오버레이 컴포넌트 추가

---
+++ frontend/src/components/EstateList/CustomOverlay.tsx
@@ -0,0 +1,13 @@
+import * as c from '@components/EstateList/styles/CustomOverlayStyle'
+
+const CustomOverlay = () => {
+  return (
+    <div>
+      <c.EstateItem>
+        <p>매물</p>
+      </c.EstateItem>
+    </div>
+  )
+}
+
+export default CustomOverlay
