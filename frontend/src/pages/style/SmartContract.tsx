63ba020adb3050e51cd3fd7da8e141a8de43ab14
오기선 <pakox123@gmail.com>
2024-04-26 11:51:57 +0900

    feat : pdf 저장 추가

---
+++ frontend/src/pages/style/SmartContract.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+export const PaperContainer = styled.div`
+  width: 90%;
+  height: 75vh;
+  border: 1px solid #f8bbbb;
+  background: #fff url('/contract.PNG') no-repeat center center;
+  background-size: 100% 100%;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+`
