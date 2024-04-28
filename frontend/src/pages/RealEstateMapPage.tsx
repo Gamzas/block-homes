549533b54b5239d872a4ec83ba29df70af6f39fe
정유나 <jyn7077@naver.com>
2024-04-29 08:40:01 +0900

    ✨ feat : 매물 지도 페이지 추가

---
+++ frontend/src/pages/RealEstateMapPage.tsx
@@ -0,0 +1,11 @@
+import EstateListMap from '@/components/EstateList/EstateListMap'
+
+const RealEstateMapPage = () => {
+  return (
+    <>
+      <EstateListMap />
+    </>
+  )
+}
+
+export default RealEstateMapPage
