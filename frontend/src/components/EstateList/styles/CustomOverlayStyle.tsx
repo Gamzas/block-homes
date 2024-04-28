c9015f9a4fc7901cc6a7ee509a6ec06451a17b62
정유나 <jyn7077@naver.com>
2024-04-28 19:23:47 +0900

    ✨ feat	: 매물 상태 별 overlay 문구 변경

--- frontend/src/components/EstateList/styles/CustomOverlayStyle.tsx
+++ frontend/src/components/EstateList/styles/CustomOverlayStyle.tsx
@@ -1,6 +1,30 @@
 import styled from 'styled-components'
 
-export const EstateItem = styled.div`
+interface conditionProps {
+  $condition: string
+}
+
+const getOverLayColor = (condition: string) => {
+  switch (condition) {
+    case 'good':
+      return {
+        circle: '#845BD3',
+        innerCircle: '#D2C5F1',
+      }
+    case 'normal':
+      return {
+        circle: '#24d0d6',
+        innerCircle: '#e9f7f7',
+      }
+    case 'bad':
+      return {
+        circle: '#FE754E',
+        innerCircle: '#FFD8CD',
+      }
+  }
+}
+
+export const EstateItem = styled.div<conditionProps>`
   display: flex;
   justify-content: center;
   text-align: center;
@@ -9,7 +33,7 @@ export const EstateItem = styled.div`
   position: relative;
   .circle {
     border-radius: 100%;
-    background-color: #24d0d6;
+    background-color: ${props => getOverLayColor(props.$condition)?.circle};
     width: 3rem;
     height: 3rem;
     position: absolute;
@@ -18,19 +42,20 @@ export const EstateItem = styled.div`
     transform: translate(-50%, -50%);
   }
   .inner-circle {
-    background-color: #e9f7f7;
+    background-color: ${props =>
+      getOverLayColor(props.$condition)?.innerCircle};
     border-radius: 100%;
-    width: 2.3rem;
-    height: 2.3rem;
+    width: 2.5rem;
+    height: 2.5rem;
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
   }
   .estate-sign {
-    color: #24d0d6;
+    color: ${props => getOverLayColor(props.$condition)?.circle};
     font-size: 1rem;
     font-weight: 500;
-    line-height: 2.2rem;
+    line-height: 2.5rem;
   }
 `
