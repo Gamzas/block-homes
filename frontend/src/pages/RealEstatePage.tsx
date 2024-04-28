d2c538c1208f9f4dd6441af6b0554a2462ece7bb
정유나 <jyn7077@naver.com>
2024-04-29 08:40:12 +0900

    ✨ feat : 매물 메인페이지 수정

--- frontend/src/pages/RealEstatePage.tsx
+++ frontend/src/pages/RealEstatePage.tsx
@@ -1,12 +1,10 @@
-import EstateListMap from "@/components/EstateList/EstateListMap";
-import Header from "@/common/Header";
 import Footer from "@/common/Footer";
 import * as r from '@pages/style/RealEstatePageStyle'
+import { Outlet } from "react-router-dom";
 const RealEstatePage = () => {
   return (
     <r.Container>
-      <Header />
-      <EstateListMap />
+      <Outlet />
       <Footer />
     </r.Container>
   );
