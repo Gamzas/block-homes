6c84e89a38875ee928794205a6aa4cb07c9972a2
정유나 <jyn7077@naver.com>
2024-04-29 08:37:24 +0900

    ✨ feat : 매물 리스트 컴포넌트 추가

---
+++ frontend/src/components/EstateList/EstateItemList.tsx
@@ -0,0 +1,101 @@
+import useCurrentLocation from '@/hooks/useCurrentLocation'
+import CurrentStatus from './CurrentStatus'
+import EstateItemCard from './EstateItemCard'
+import { useEffect } from 'react'
+import EstateItemFilter from './EstateItemFilter'
+
+const EstateList = [
+  {
+    condition: 'normal',
+    address: '남동길 30번길 13 3층',
+    infos: ['남향', '교육시설', '역세권'],
+    leaseType: '전세',
+    price: '3억',
+    roomSize: '37',
+    roomCount: '투룸',
+    createDate: '2024년 3월 21일',
+    latitude: 35.2057145,
+    longitude: 126.8115472,
+  },
+  {
+    condition: 'bad',
+    address: '광주광역시 광산구 오선동',
+    infos: ['남향', '교육시설', '역세권'],
+    leaseType: '매매',
+    price: '3억',
+    roomSize: '37',
+    roomCount: '투룸',
+    createDate: '2024년 3월 21일',
+    latitude: 35.205615,
+    longitude: 126.812546,
+  },
+  {
+    condition: 'good',
+    address: '광주광역시 광산구 장덕동',
+    infos: ['남향', '교육시설', '역세권'],
+    leaseType: '월세',
+    price: '3억',
+    roomSize: '37',
+    roomCount: '투룸',
+    createDate: '2024년 3월 21일',
+    latitude: 35.204315,
+    longitude: 126.812546,
+  },
+  {
+    condition: 'normal',
+    address: '남동길 30번길 13 3층',
+    infos: ['남향', '교육시설', '역세권'],
+    leaseType: '전세',
+    price: '3억',
+    roomSize: '37',
+    roomCount: '투룸',
+    createDate: '2024년 3월 21일',
+    latitude: 35.204615,
+    longitude: 126.813546,
+  },
+  {
+    condition: 'bad',
+    address: '광주광역시 광산구 오선동',
+    infos: ['남향', '교육시설', '역세권'],
+    leaseType: '매매',
+    price: '3억',
+    roomSize: '37',
+    roomCount: '투룸',
+    createDate: '2024년 3월 21일',
+    latitude: 35.202615,
+    longitude: 126.814546,
+  },
+  {
+    condition: 'good',
+    address: '광주광역시 광산구 장덕동',
+    infos: ['남향', '교육시설', '역세권'],
+    leaseType: '월세',
+    price: '3억',
+    roomSize: '37',
+    roomCount: '투룸',
+    createDate: '2024년 3월 21일',
+    latitude: 35.204615,
+    longitude: 126.815546,
+  },
+]
+const EstateItemList = () => {
+  const { currentPosition, getCurrentLocation } = useCurrentLocation()
+
+  useEffect(() => {
+    getCurrentLocation()
+  }, [])
+  return (
+    <div>
+      <CurrentStatus
+        getCurrentLocation={getCurrentLocation}
+        currentPosition={currentPosition}
+      />
+      {EstateList.map((item, index) => (
+        <EstateItemCard key={index} {...item} />
+      ))}
+      <EstateItemFilter />
+    </div>
+  )
+}
+
+export default EstateItemList
