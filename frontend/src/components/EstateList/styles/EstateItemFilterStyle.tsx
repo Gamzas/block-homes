607eb7597a006b002b237a9ecc93c0859dd369ae
정유나 <jyn7077@naver.com>
2024-04-29 15:02:58 +0900

    ✨ feat	 : 슬라이더 스타일 삭제

--- frontend/src/components/EstateList/styles/EstateItemFilterStyle.tsx
+++ frontend/src/components/EstateList/styles/EstateItemFilterStyle.tsx
@@ -1,5 +1,11 @@
 import styled from 'styled-components'
 
+interface FilterPropsType {
+  $isActive?: boolean
+  $value?: number
+}
+
+// bottom sheet style
 export const FilterContainer = styled.div`
   width: 100%;
   height: 60vh;
@@ -9,6 +15,8 @@ export const FilterContainer = styled.div`
   background: #fff;
   padding: 2rem;
 `
+
+// 거래형태 및 옵션
 export const OptionContainer = styled.div`
   width: 100%;
   height: fit-content;
@@ -54,6 +62,8 @@ export const TransactionContainer = styled.div`
     line-height: 2rem;
   }
 `
+
+// 금액
 export const PriceContainer = styled.div`
   display: flex;
   justify-content: space-evenly;
@@ -70,17 +80,100 @@ export const PriceContainer = styled.div`
     line-height: 2.2rem;
   }
 `
+// 옵션
+export const SquareFootageSlider = styled.input<FilterPropsType>`
+  -webkit-appearance: none;
+  appearance: none;
+  width: 100%;
+  height: 14px;
+  border-radius: 10px;
+  background: ${props => {
+    const percentage = ((Number(props.value) - 1) / 5) * 100
+    return `linear-gradient(to right, '#845BD3' 0%, '#845BD3' ${percentage}%, '#FFFFF' ${percentage}%, '#FFFFF' 100%)`
+  }};
 
-export const detailContainer = styled.div`
+  &:focus {
+    outline: none;
+  }
+
+  &::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 28px;
+    height: 28px;
+    background-color: '#FFFFF';
+    border: 1px solid '#845BD3';
+    border-radius: 50%;
+    cursor: pointer;
+  }
+
+  &::-moz-range-thumb {
+    width: 28px;
+    height: 28px;
+    background-color: '#FFFFF';
+    border: 1px solid '#845BD3';
+    border-radius: 50%;
+    cursor: pointer;
+  }
+`
+export const SliderBar = styled.div`
+  width: 100%;
+  height: 0.6rem;
+  background: #c2b7d8;
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .range-circle {
+    width: 0.3rem;
+    height: 0.3rem;
+    border-radius: 100%;
+    background-color: white;
+  }
+`
+
+export const InnerSliderBar = styled.div`
+  position: absolute;
+  left: 20%;
+  right: 20%;
+  height: 0.6rem;
+  background: #845bd3;
+`
+export const RangeInputContainer = styled.div`
+  position: relative;
+`
+export const RangeInputMin = styled.input`
+  position: absolute;
+  top: -1rem;
+  width: 100%;
+  -webkit-appearance: none;
+  pointer-events: none;
+  background: none;
+  &::-webkit-slider-thumb {
+    height: 1.5rem;
+    width: 1.5rem;
+    border-radius: 100%;
+    border: 2px solid #845bd3;
+    background-color: white;
+    -webkit-appearance: none;
+    pointer-events: auto;
+  }
+`
+export const RangeInputMax = styled(RangeInputMin)``
+
+// 상세조건
+
+export const DetailContainer = styled.div`
   display: flex;
   width: 100%;
-  .detail-box {
-    font-size: 0.6rem;
-    padding: 0.5rem;
-    width: fit-content;
-    border-radius: 1rem;
-    border: 1px solid #845bd3;
-    background: #f3f0f7;
-    margin-right: 0.5rem;
-  }
+`
+
+export const DetailOptionBtn = styled.div<FilterPropsType>`
+  font-size: 0.6rem;
+  padding: 0.5rem;
+  width: fit-content;
+  border-radius: 1rem;
+  border: 1px solid #845bd3;
+  background: ${props => (props.$isActive ? `#FFFFFF` : `#f3f0f7`)};
+  margin-right: 0.5rem;
 `
