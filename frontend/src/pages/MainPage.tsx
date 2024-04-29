42c05e9d813fbba2ee66b4d5d8fc9437cb796d6d
KIMHYEONJI <hyeonji0718@gmail.com>
2024-04-29 17:13:25 +0900

    ✨ feat : user type이 임차인일 경우 소유 부동산 카드 목록이 보이도록 설정

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -5,6 +5,7 @@ import RealEstateCategory from '@components/MainPage/RealEstateCategory'
 import UserTypeToggle from '@common/UserTypeToggle'
 import { useAtom } from 'jotai'
 import { userAtom } from '@stores/atoms/userStore'
+import OwnEstateDidList from '@components/MainPage/OwnEstateDidList'
 
 const MainPage = () => {
   const [currentUser] = useAtom(userAtom)
@@ -12,7 +13,7 @@ const MainPage = () => {
     <h.MainPageContainer>
       <TopCard />
       {currentUser.type === 0 && <RealEstateCategory />}
-      {currentUser.type === 1 && <></>}
+      {currentUser.type === 1 && <OwnEstateDidList />}
       <UserTypeToggle />
       <Footer />
     </h.MainPageContainer>
