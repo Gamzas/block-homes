8844d955e4643ca47dd1dbd1886f88d71c750be1
KIMHYEONJI <hyeonji0718@gmail.com>
2024-04-29 11:02:10 +0900

    ✨ feat : footer에 들어갈 FooterComponentList 생성

--- frontend/src/common/Footer.tsx
+++ frontend/src/common/Footer.tsx
@@ -1,6 +1,26 @@
 import * as f from '@common/style/FooterStyle'
+import { FooterComponentType } from '@/types/footer'
 
 const Footer = () => {
+  const FooterComponentList: FooterComponentType[] = [
+    {
+      title: '홈',
+      iconSrc: 'public/icon/icon_footer_home.png',
+    },
+    {
+      title: '관심 목록',
+      iconSrc: 'public/icon/icon_footer_heart.png',
+    },
+    {
+      title: '채팅',
+      iconSrc: 'public/icon/icon_footer_chat.png',
+    },
+    {
+      title: '마이페이지',
+      iconSrc: 'public/icon/icon_footer_mypage.png',
+    },
+  ]
+
   return <f.Container>FOOTER</f.Container>
 }
 
