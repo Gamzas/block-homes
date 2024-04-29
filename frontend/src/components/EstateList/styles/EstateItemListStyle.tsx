837db0c0728a07d1a6fb1fe23a811ce99a0bcda2
정유나 <jyn7077@naver.com>
2024-04-29 17:59:01 +0900

    🎨 style : card 스타일 수정

---
+++ frontend/src/components/EstateList/styles/EstateItemListStyle.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const EstateItemListContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  /* background-color: #f3f0f7; */
+`
