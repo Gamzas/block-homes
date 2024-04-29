40b5906618eb3e0aa578618e87e2b2f8b6660b46
오기선 <pakox123@gmail.com>
2024-04-29 17:09:52 +0900

    ♻️ refactor : 라우터 하나로 통일

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import MainPage from '@pages/MainPage'
 import { GlobalStyle } from '@style/GlobalStyles'
 import RealEstatePage from '@pages/RealEstatePage'
-import SmartContractStart from './pages/SmartContract/SmartContractStart'
-import SmartContractAgreement from './pages/SmartContract/SmartContractAgreement'
-import SmartContractMain from './pages/SmartContract/SmartContractMain'
+import SmartContract from './pages/SmartContract/SmartContract'
 import SmartContractPayment from './pages/SmartContract/SmartContractPayment'
 import RealEstateMapPage from '@pages/RealEstateMapPage'
 import RealEstateListPage from '@pages/RealEstateListPage'
@@ -18,12 +16,8 @@ const HomeRoutes = () => (
       <Route path="" element={<RealEstateListPage />} />
       <Route path="map" element={<RealEstateMapPage />} />
     </Route>
-    <Route path="/smart-contract-start" element={<SmartContractStart />} />
-    <Route
-      path="/smart-contract-agreement"
-      element={<SmartContractAgreement />}
-    />
-    <Route path="/smart-contract-main" element={<SmartContractMain />} />
+    <Route path="/smart-contract" element={<SmartContract />} />
+
     <Route path="/smart-contract-payment" element={<SmartContractPayment />} />
   </Routes>
 )
