c8a5172e3b8e7e6ff8a7b88e98934c143d61b205
giseon <pakox123@gmail.com>
2024-04-29 22:42:40 +0900

    ✨ feat : 버튼 위치 통일

--- frontend/src/components/SmartContract/style/ContractStart.tsx
+++ frontend/src/components/SmartContract/style/ContractStart.tsx
@@ -1,10 +1,16 @@
 import styled from 'styled-components'
 
 export const ContractStartContainer = styled.div`
-  margin-top: 21vh;
+  margin-top: 26vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
   height: 60vh;
+  border: 1px solid black;
+  .button-box{
+    position: absolute;
+    bottom: 10vh;
+    /* margin-bottom: 3vh; */
+  }
 `
