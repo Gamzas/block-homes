2f77d923c5ccfe3bcbd0e7ddc0504f3fba7b68b5
천세진 <sejinnnnnn@naver.com>
2024-04-30 12:09:13 +0900

    ✨ feat : 임시저장

---
+++ smart-contract/contracts/real-estate/RealEstateDIDRegistry.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../structs/DIDStructs.sol";
+
+contract RealEstateDIDRegistry {
+
+    mapping(string => DIDStructs.DIDDocument) private didDocuments;
+
+    event RealEstateDIDDocumentCreated(string did, string id);
+
+    function 
+
+}
