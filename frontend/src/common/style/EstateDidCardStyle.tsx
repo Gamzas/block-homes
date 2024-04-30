ad6de1fe7e07db3dda66111dc0e56619d168e33f
KIMHYEONJI <hyeonji0718@gmail.com>
2024-04-30 12:11:04 +0900

    🎨 style : 부동산 did 카드 style 적용

--- frontend/src/common/style/EstateDidCardStyle.tsx
+++ frontend/src/common/style/EstateDidCardStyle.tsx
@@ -1,3 +1,78 @@
 import styled from 'styled-components'
 
-export const EstateDidCardContainer = styled.div``
+export const EstateDidCardContainer = styled.div`
+  width: 13rem;
+  height: 17rem;
+  border-radius: 1.5rem;
+  overflow: hidden;
+  filter: drop-shadow(0px 3.62px 3.62px rgba(0, 0, 0, 0.25));
+`
+
+export const TopContainer = styled.div`
+  height: 65%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  background: #e9f7f7;
+  position: relative;
+  padding-top: 0.5rem;
+
+  .ministry-of-land-logo {
+    width: 40%;
+  }
+
+  .building-type-image {
+    width: 65%;
+    opacity: 0.8;
+    z-index: 10;
+    position: relative;
+    bottom: -0.5rem;
+  }
+`
+
+export const BackgroundWaveContainer = styled.div`
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  left: 0;
+
+  .big-wave {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+  }
+
+  .small-wave {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+  }
+`
+
+export const BottomContainer = styled.div`
+  height: 35%;
+  display: flex;
+  flex-flow: wrap;
+  align-content: space-evenly;
+  justify-content: space-around;
+  background-color: white;
+`
+
+export const InfoElement = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 40%;
+  margin-left: 1rem;
+
+  .element-title {
+    font-size: 0.6rem;
+    font-weight: 500;
+    margin-bottom: 0.3rem;
+  }
+
+  .element-content {
+    font-size: 0.8rem;
+    font-weight: 600;
+  }
+`
