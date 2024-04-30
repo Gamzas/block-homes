ca56dd231b82565001445394352c162b7d01d284
오기선 <pakox123@gmail.com>
2024-04-30 11:44:45 +0900

    ✨ feat : 결제 부분 구조 변경

--- frontend/src/components/SmartContract/ConstractPayment.tsx
+++ frontend/src/components/SmartContract/ConstractPayment.tsx
@@ -3,47 +3,42 @@ import Step from './Step'
 import { Arrow, ScreenContainer } from './style/ContractPayment'
 import { Button } from '@/common/style/Button'
 
-
 const ConstractPayment = () => {
-
   const [screenIndex, setScreenIndex] = useState(0)
-  const totalScreens =2
-  const nextScreen = () =>{
-
-    if (screenIndex<totalScreens-1){
-      setScreenIndex(screenIndex+1)
+  const totalScreens = 2
+  const nextScreen = () => {
+    if (screenIndex < totalScreens - 1) {
+      setScreenIndex(screenIndex + 1)
     }
   }
 
-  const prevScreen = () =>{
-    if(screenIndex>0){
-      setScreenIndex(screenIndex-1)
+  const prevScreen = () => {
+    if (screenIndex > 0) {
+      setScreenIndex(screenIndex - 1)
     }
-
   }
   return (
-
     <>
       <Step currentindex={2}></Step>
       <ScreenContainer>
-      
         {screenIndex > 0 && (
-          <Arrow style={{left: '10px'}} onClick={prevScreen}>&larr;</Arrow>
+          <Arrow style={{ left: '10px' }} onClick={prevScreen}>
+            &larr;
+          </Arrow>
         )}
+        <div>
+          계약 내용을<br></br>확인해주세요
+        </div>
 
-          {screenIndex === 0 && <div style={{color:'black'}}>Screen 1 Contents
-          <Button disabled>결제</Button>
-          </div>}
-          {screenIndex === 1 && <div style={{color:'black'}}>Screen 2 Contents
-          
-          <Button>결제</Button>
-          </div>}
+        {screenIndex === 0 && <div>Screen 1 Contents</div>}
+        {screenIndex === 1 && <div>Screen 2 Contents</div>}
 
-        {screenIndex < totalScreens - 1 && (
-          <Arrow onClick={nextScreen}></Arrow>
-        )}
+        <div className="button-box">
+          <Button disabled={screenIndex === 0}>결제</Button>
+        </div>
+
+        {screenIndex < totalScreens - 1 && <Arrow onClick={nextScreen}></Arrow>}
       </ScreenContainer>
-      
     </>
   )
 }
