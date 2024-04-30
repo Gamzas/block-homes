7904c8fb4499425f7203dbee7399188c8e9f167d
정유나 <jyn7077@naver.com>
2024-04-30 13:55:27 +0900

    🚚 rename : 파일명 수정

---
+++ frontend/src/types/estateListType.ts
@@ -0,0 +1,11 @@
+export interface RealEstateCategoryType {
+  src: string
+  title: string
+  transactionType1: string
+  transactionType2: string | null
+}
+
+export interface RealEstateStatusType {
+  condition : string
+  color1 : string
+}
