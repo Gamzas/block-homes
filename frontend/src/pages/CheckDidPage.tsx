45f25471970d2cc40621798fa7ada2eeea5dc2dd
KINHYEONJI <hyeonji0718@gmail.com>
2024-05-01 23:56:08 +0900

    ✨ feat : InfoText 적용

--- frontend/src/pages/CheckDidPage.tsx
+++ frontend/src/pages/CheckDidPage.tsx
@@ -1,9 +1,20 @@
+import * as c from '@pages/style/CheckDidPageStyle'
+import InfoText from '@components/CheckDidPage/InfoText'
+
 const CheckDidPage = () => {
+
+  const textProps = {
+    largeText1: '블록체인 기술을 이용하여',
+    highlightText: '부동산 정보',
+    largeText2: '를 조회해 오는 중입니다.',
+    smallText: '조금만 더 기달려 주세요!',
+  }
+
   return (
-    <div>
+    <c.CheckDidPageContainer>
+      <InfoText {...textProps} />
+    </c.CheckDidPageContainer>
+  )
+}
 
-    </div>
-  );
-};
-
-export default CheckDidPage;
+export default CheckDidPage
