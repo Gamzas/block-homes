0e974bf08d0308904bb67d77a87ad076f7378543
KINHYEONJI <hyeonji0718@gmail.com>
2024-05-01 22:25:05 +0900

    ✨ feat : did card 조회 과정을 안내하는 InfoText 생성

---
+++ frontend/src/components/CheckDidPage/InfoText.tsx
@@ -0,0 +1,24 @@
+import * as i from '@components/CheckDidPage/style/InfoTextStyle'
+
+interface InfoTextPropsType {
+  largeText1: string
+  highlightText: string
+  largeText2: string
+  smallText: string
+}
+
+const InfoText = (InfoTextProps: InfoTextPropsType) => {
+  const { largeText1, highlightText, largeText2, smallText } = InfoTextProps
+  return (
+    <div>
+      <i.InfoTextContainer>
+        <div className='info-text-large'>{largeText1}</div>
+        <div className='info-text-highlight'>{highlightText}</div>
+        <div className='info-text-large'>{largeText2}</div>
+        <div className='info-text-small'>{smallText}</div>
+      </i.InfoTextContainer>
+    </div>
+  )
+}
+
+export default InfoText
