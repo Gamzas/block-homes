f38becc067574e6487059e46476fbe4023e48a8d
KINHYEONJI <hyeonji0718@gmail.com>
2024-05-02 00:15:21 +0900

    🎨 style : CheckDidPage style 추가

--- frontend/src/pages/style/CheckDidPageStyle.tsx
+++ frontend/src/pages/style/CheckDidPageStyle.tsx
@@ -1,6 +1,11 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const CheckDidPageContainer = styled.div`
   width: 100%;
   min-height: 100vh;
+  padding: 7rem 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
 `
