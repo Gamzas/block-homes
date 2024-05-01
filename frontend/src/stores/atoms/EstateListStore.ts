cd3f2c4cd32ea5a675dbfcd411f7d3d92e7047db
정유나 <jyn7077@naver.com>
2024-05-02 08:38:35 +0900

    ✨ feat : 현재 위치 전역 상태 관리

--- frontend/src/stores/atoms/EstateListStore.ts
+++ frontend/src/stores/atoms/EstateListStore.ts
@@ -1,6 +1,19 @@
+import { atomWithStorage } from 'jotai/utils'
 import { atom } from 'jotai'
+import { CurrentPosition } from '@/types/estateListType'
 
 export const filterAtom = atom<boolean>(false) // false 필터 닫힘, true 필터 열림
 
 export const mapAtom = atom<boolean>(false) // false list true map
 
+const defaultPosition = {
+  currentPosition: '정자동',
+  location: {
+    latitude: 37.365264512305174,
+    longitude: 127.10676860117488,
+  },
+}
+export const currentPositonAtom = atomWithStorage<CurrentPosition>(
+  'currentPosition',
+  defaultPosition,
+)
