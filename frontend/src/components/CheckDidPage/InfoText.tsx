657504114e4a1e402d478c0467ad4cc6a9070a6e
KINHYEONJI <hyeonji0718@gmail.com>
2024-05-01 23:48:05 +0900

    ♻️ refactor : 의미없는 div 삭제

--- frontend/src/components/CheckDidPage/InfoText.tsx
+++ frontend/src/components/CheckDidPage/InfoText.tsx
@@ -4,14 +4,12 @@ import { InfoTextPropsType } from '@/types/components/CheckDidInputTextType'
 const InfoText = (InfoTextProps: InfoTextPropsType) => {
   const { largeText1, highlightText, largeText2, smallText } = InfoTextProps
   return (
-    <div>
-      <i.InfoTextContainer>
-        <div className='info-text-large'>{largeText1}</div>
-        <div className='info-text-highlight'>{highlightText}</div>
-        <div className='info-text-large'>{largeText2}</div>
-        <div className='info-text-small'>{smallText}</div>
-      </i.InfoTextContainer>
-    </div>
+    <i.InfoTextContainer>
+      <div className='info-text-large'>{largeText1}</div>
+      <div className='info-text-highlight'>{highlightText}</div>
+      <div className='info-text-large'>{largeText2}</div>
+      <div className='info-text-small'>{smallText}</div>
+    </i.InfoTextContainer>
   )
 }
 
