45cec20b83c19e19adb274e6d494cb25423a4058
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-02 11:32:22 +0900

    ✨ feat : did 조회를 위한 비어있는 did card 생성

---
+++ frontend/src/components/CheckDidPage/EmptyEstateDidCard.tsx
@@ -0,0 +1,13 @@
+import * as e from '@components/CheckDidPage/style/EmptyEstateDidCardStyle'
+
+const EmptyEstateDidCard = () => {
+  return (
+    <e.EmptyEstateDidCardContainer>
+      <div className="plus-container">
+        <div className="plus">+</div>
+      </div>
+    </e.EmptyEstateDidCardContainer>
+  )
+}
+
+export default EmptyEstateDidCard
