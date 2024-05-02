b5d32614df4fc3d49b30075494f1de063ae7135f
오기선 <pakox123@gmail.com>
2024-05-02 10:58:14 +0900

    ✨ feat : 웨이브 배경화면 적용

--- frontend/src/pages/SmartContract/SmartContract.tsx
+++ frontend/src/pages/SmartContract/SmartContract.tsx
@@ -7,12 +7,14 @@ import ContractStart from '@/components/SmartContract/ContractStart'
 import { readContractStepAtom } from '@/stores/smartcontract'
 import { useAtom } from 'jotai'
 import ContractComplete from '../../components/SmartContract/ContractComplete'
+import WaveContainer from '@/common/WaveContainer'
 
 const SmartContract = () => {
   const [step] = useAtom(readContractStepAtom)
 
   return (
     <s.ContractContainer>
+      <WaveContainer></WaveContainer>
       <Header
         title="부동산 거래"
         isSearch={false}
