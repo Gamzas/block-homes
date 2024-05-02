58a583d0ead7bf54029e613a9a1d0ea94f184883
오기선 <pakox123@gmail.com>
2024-05-02 17:56:22 +0900

    ✨ feat : apaxchart 적용(주석)

---
+++ frontend/src/components/Report/style/GuageStyle.tsx
@@ -0,0 +1,33 @@
+// import styled from 'styled-components'
+
+// export const GaugeContainer = styled.div`
+//   width: 120px; /* 게이지의 너비 */
+//   height: 60px; /* 게이지의 높이 */
+//   /* overflow: hidden; 내부 요소가 범위를 벗어나지 않도록 함 */
+//   position: relative; /* 자식 요소의 위치 기준점 설정 */
+//   background-color: #fff; /* 배경색 설정 */
+//   border-radius: 100px 100px 0 0; /* 상단 둥근 모서리 효과 */
+//   .gauge {
+//     width: 120px;
+//     height: 120px;
+//     border-radius: 50%;
+//     position: absolute;
+//     bottom: -100%;
+//     left: 0;
+//     transform: rotate(-90deg); /* 게이지 시작 각도 */
+//     transition: transform 1s;
+//     border: 10px solid;
+//   }
+
+//   .pointer {
+//     width: 2px; /* 포인터 너비 */
+//     height: 50px; /* 포인터 높이 */
+//     background-color: black; /* 포인터 색상 */
+//     position: absolute; /* 절대 위치, gauge-container 기준 */
+//     bottom: 0;
+//     left: 50%; /* 중앙 정렬 */
+//     transform-origin: bottom; /* 회전의 기준점을 하단 중심으로 설정 */
+//     transform: rotate(-45deg); /* 초기 포인터 각도 설정 */
+//     transition: transform 2s; /* 회전에 대한 애니메이션 효과 */
+//   }
+// `
