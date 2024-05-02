14ad5c7ab4cc8dd30998ba97b07f53540895647a
정유나 <jyn7077@naver.com>
2024-05-02 11:42:19 +0900

    ✨ feat	: 임시 부동산 데이터 추가

--- frontend/src/components/EstateList/EstateItemList.tsx
+++ frontend/src/components/EstateList/EstateItemList.tsx
@@ -5,88 +5,18 @@ import { useEffect } from 'react'
 import EstateItemFilter from './EstateItemFilter'
 import * as l from '@components/EstateList/styles/EstateItemListStyle'
 import { useAtom } from 'jotai'
-import { filterAtom, mapAtom } from '@/stores/atoms/EstateListStore'
+import {
+  estateItemListAtom,
+  filterAtom,
+  mapAtom,
+} from '@/stores/atoms/EstateListStore'
 import EstateListMap from './EstateListMap'
-
-const EstateList = [
-  {
-    condition: 'normal',
-    address: '남동길 30번길 13 3층',
-    infos: ['남향', '교육시설', '역세권'],
-    leaseType: '전세',
-    price: '3억',
-    roomSize: '37',
-    roomCount: '투룸',
-    createDate: '2024년 3월 21일',
-    latitude: 35.2057145,
-    longitude: 126.8115472,
-  },
-  {
-    condition: 'bad',
-    address: '광주광역시 광산구 오선동',
-    infos: ['남향', '교육시설', '역세권'],
-    leaseType: '매매',
-    price: '3억',
-    roomSize: '37',
-    roomCount: '투룸',
-    createDate: '2024년 3월 21일',
-    latitude: 35.205615,
-    longitude: 126.812546,
-  },
-  {
-    condition: 'good',
-    address: '광주광역시 광산구 장덕동',
-    infos: ['남향', '교육시설', '역세권'],
-    leaseType: '월세',
-    price: '3억',
-    roomSize: '37',
-    roomCount: '투룸',
-    createDate: '2024년 3월 21일',
-    latitude: 35.204315,
-    longitude: 126.812546,
-  },
-  {
-    condition: 'normal',
-    address: '남동길 30번길 13 3층',
-    infos: ['남향', '교육시설', '역세권'],
-    leaseType: '전세',
-    price: '3억',
-    roomSize: '37',
-    roomCount: '투룸',
-    createDate: '2024년 3월 21일',
-    latitude: 35.204615,
-    longitude: 126.813546,
-  },
-  {
-    condition: 'bad',
-    address: '광주광역시 광산구 오선동',
-    infos: ['남향', '교육시설', '역세권'],
-    leaseType: '매매',
-    price: '3억',
-    roomSize: '37',
-    roomCount: '투룸',
-    createDate: '2024년 3월 21일',
-    latitude: 35.202615,
-    longitude: 126.814546,
-  },
-  {
-    condition: 'good',
-    address: '광주광역시 광산구 장덕동',
-    infos: ['남향', '교육시설', '역세권'],
-    leaseType: '월세',
-    price: '3억',
-    roomSize: '37',
-    roomCount: '투룸',
-    createDate: '2024년 3월 21일',
-    latitude: 35.204615,
-    longitude: 126.815546,
-  },
-]
 
 const EstateItemList = () => {
   const { currentPosition, getCurrentLocation } = useCurrentLocation()
   const [filter] = useAtom(filterAtom)
   const [menu] = useAtom(mapAtom)
+  const [estateItemList] = useAtom(estateItemListAtom)
   useEffect(() => {
     getCurrentLocation()
   }, [])
@@ -101,7 +31,7 @@ const EstateItemList = () => {
       <l.EstateItemListContainer>
         {menu ? (
           <>
-            {EstateList.map((item, index) => (
+            {estateItemList.map((item, index) => (
               <EstateItemCard key={index} {...item} />
             ))}
           </>
--- frontend/src/components/EstateList/EstateListMap.tsx
+++ frontend/src/components/EstateList/EstateListMap.tsx
@@ -3,81 +3,8 @@ import * as e from '@components/EstateList/styles/EstateListMapStyle'
 import { renderToString } from 'react-dom/server'
 import CustomOverlay from '@components/EstateList/CustomOverlay'
 import useCurrentLocation from '@/hooks/useCurrentLocation'
-
-const EstateList = [
-  {
-    condition: 'normal',
-    address: '남동길 30번길 13 3층',
-    infos: ['남향', '교육시설', '역세권'],
-    leaseType: '전세',
-    price: '3억',
-    roomSize: '37',
-    roomCount: '투룸',
-    createDate: '2024년 3월 21일',
-    latitude: 35.2057145,
-    longitude: 126.8115472,
-  },
-  {
-    condition: 'bad',
-    address: '광주광역시 광산구 오선동',
-    infos: ['남향', '교육시설', '역세권'],
-    leaseType: '매매',
-    price: '3억',
-    roomSize: '37',
-    roomCount: '투룸',
-    createDate: '2024년 3월 21일',
-    latitude: 35.205615,
-    longitude: 126.812546,
-  },
-  {
-    condition: 'good',
-    address: '광주광역시 광산구 장덕동',
-    infos: ['남향', '교육시설', '역세권'],
-    leaseType: '월세',
-    price: '3억',
-    roomSize: '37',
-    roomCount: '투룸',
-    createDate: '2024년 3월 21일',
-    latitude: 35.204315,
-    longitude: 126.812546,
-  },
-  {
-    condition: 'normal',
-    address: '남동길 30번길 13 3층',
-    infos: ['남향', '교육시설', '역세권'],
-    leaseType: '전세',
-    price: '3억',
-    roomSize: '37',
-    roomCount: '투룸',
-    createDate: '2024년 3월 21일',
-    latitude: 35.204615,
-    longitude: 126.813546,
-  },
-  {
-    condition: 'bad',
-    address: '광주광역시 광산구 오선동',
-    infos: ['남향', '교육시설', '역세권'],
-    leaseType: '매매',
-    price: '3억',
-    roomSize: '37',
-    roomCount: '투룸',
-    createDate: '2024년 3월 21일',
-    latitude: 35.202615,
-    longitude: 126.814546,
-  },
-  {
-    condition: 'good',
-    address: '광주광역시 광산구 장덕동',
-    infos: ['남향', '교육시설', '역세권'],
-    leaseType: '월세',
-    price: '3억',
-    roomSize: '37',
-    roomCount: '투룸',
-    createDate: '2024년 3월 21일',
-    latitude: 35.204615,
-    longitude: 126.815546,
-  },
-]
+import { useAtom } from 'jotai'
+import { estateItemListAtom } from '@/stores/atoms/EstateListStore'
 
 declare global {
   interface Window {
@@ -88,6 +15,8 @@ const { kakao } = window
 
 const EstateListMap = () => {
   const { location, getCurrentLocation } = useCurrentLocation()
+  // 부동산 매물 리스트
+  const [estateItemList] = useAtom(estateItemListAtom)
 
   useEffect(() => {
     getCurrentLocation()
@@ -97,11 +26,17 @@ const EstateListMap = () => {
     // 지도생성
     const container = document.getElementById('map')
     const options = {
-      center: new kakao.maps.LatLng(location.location.latitude, location.location.longitude),
+      center: new kakao.maps.LatLng(
+        location.location.latitude,
+        location.location.longitude,
+      ),
       level: 3,
       animate: true,
     }
     const map = new kakao.maps.Map(container, options)
+
+    // 지도 확대 최대 레벨 설정
+    map.setMaxLevel(5)
 
     // 지도 줌 컨트롤러
     const zoomControl = new kakao.maps.ZoomControl()
@@ -120,7 +55,7 @@ const EstateListMap = () => {
 
     // 커스텀 오버레이 렌더링
     // 오버레이 위치 리스트에 대해 처리
-    EstateList.forEach(({ latitude, longitude, condition }) => {
+    estateItemList.forEach(({ latitude, longitude, condition }) => {
       const position = new kakao.maps.LatLng(latitude, longitude)
       const overlayString = renderToString(
         <CustomOverlay condition={condition} />,
