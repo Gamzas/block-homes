73fc119d0ff4f14201431c6f76f5a2573d9876b8
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-02 11:39:00 +0900

    ✨ feat : check-did로 routing 되도록 설정

--- frontend/src/components/CheckDidPage/EmptyEstateDidCard.tsx
+++ frontend/src/components/CheckDidPage/EmptyEstateDidCard.tsx
@@ -1,9 +1,13 @@
 import * as e from '@components/CheckDidPage/style/EmptyEstateDidCardStyle'
+import { useNavigate } from 'react-router-dom'
 
 const EmptyEstateDidCard = () => {
+  const navigate = useNavigate()
+  const checkEstateDid = () => navigate('/check-did')
+
   return (
     <e.EmptyEstateDidCardContainer>
-      <div className="plus-container">
+      <div className="plus-container" onClick={checkEstateDid}>
         <div className="plus">+</div>
       </div>
     </e.EmptyEstateDidCardContainer>
