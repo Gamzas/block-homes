11d284df4185f692104ba5a96f0e507d89547907
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-02 13:33:44 +0900

    ♻️ refactor : tab size 수정

--- frontend/src/components/MainPage/style/OwnEstateDidListStyle.tsx
+++ frontend/src/components/MainPage/style/OwnEstateDidListStyle.tsx
@@ -1,18 +1,18 @@
 import styled from 'styled-components'
 
 export const OwnEstateDidListContainer = styled.div`
-    padding: 1rem;
-    margin-top: 0.5rem;
+  padding: 1rem;
+  margin-top: 0.5rem;
 
-    .estate-did-info-text {
-        font-size: 1.2rem;
-        padding-bottom: 1rem;
-        font-weight: 500;
-    }
+  .estate-did-info-text {
+    font-size: 1.2rem;
+    padding-bottom: 1rem;
+    font-weight: 500;
+  }
 `
 
 export const DidListCarousel = styled.div`
-    height: 40vh;
-    width: 90%;
-    margin: 0 auto;
+  height: 35vh;
+  width: 90%;
+  margin: 0 auto;
 `
