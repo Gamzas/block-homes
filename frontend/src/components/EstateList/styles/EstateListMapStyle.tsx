2437aa46424f7d4b469137ca817ee23a9c9d4eb7
정유나 <jyn7077@naver.com>
2024-05-02 15:51:58 +0900

    ✨ feat : 사용하지 않는 style 삭제

--- frontend/src/components/EstateList/styles/EstateListMapStyle.tsx
+++ frontend/src/components/EstateList/styles/EstateListMapStyle.tsx
@@ -7,34 +7,6 @@ export const EstateMapContainer = styled.div`
   z-index: 0;
 `
 
-export const CurrentLocationContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 6rem;
-  height: 2rem;
-  // ---------------------------------
-  background-color: white;
-  border-radius: 1rem;
-  /* border: 0.1rem solid; */
-  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-  // --------------------------------------
-  flex-shrink: 0;
-  position: absolute;
-  z-index: 3;
-  top: 2%; // 상단에서부터의 위치를 설정
-  left: 2%; // 좌측에서부터의 위치를 설정
-  .location-icon {
-    width: 1rem;
-    height: 1rem;
-    margin-right: 0.6rem;
-  }
-  .current-location {
-    font-weight: 600;
-    font-size: 1rem;
-    line-height: 1.5rem;
-  }
-`
 
 export const DetailCardContainer = styled.div`
   position: absolute;
