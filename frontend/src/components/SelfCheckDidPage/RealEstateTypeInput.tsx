e7bbee73fa8e7d71674061c6e48a565a7554df42
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-03 17:10:09 +0900

    ✨ feat : 매물 형태를 선택하는 RealEstateTypeInput 생성

--- frontend/src/components/SelfCheckDidPage/RealEstateTypeInput.tsx
+++ frontend/src/components/SelfCheckDidPage/RealEstateTypeInput.tsx
@@ -1,5 +1,41 @@
+import * as r from '@components/SelfCheckDidPage/style/RealEstateTypeInputStyle'
+import React, { useState } from 'react'
+
 const RealEstateTypeInput = () => {
-  return <div></div>
+  const [isOpen, setIsOpen] = useState(false)
+  // const [estateType, setEstateType] = useAtom()
+  const handleToggle = () => {
+    setIsOpen(!isOpen)
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    console.log(e.target.value)
+    // setEstateType(e.target.value)
+    setIsOpen(false)
+  }
+
+  return (
+    <r.RealEstateTypeInputContainer>
+      <label htmlFor="estateType" className="select-title">
+        분류
+      </label>
+      <select
+        className={`select-box ${isOpen ? 'open' : ''}`}
+        id="estateType"
+        // value={estateType}
+        onChange={handleChange}
+        onClick={handleToggle}
+      >
+        <option disabled selected>
+          목적물의 유형을 선택해주세요.
+        </option>
+        <option value="oneRoom">원룸</option>
+        <option value="villaOrTowRoom">빌라 · 투룸</option>
+        <option value="apartment">아파트</option>
+        <option value="offictel">오피스텔</option>
+      </select>
+    </r.RealEstateTypeInputContainer>
+  )
 }
 
 export default RealEstateTypeInput
