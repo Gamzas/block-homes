af49118f788697ea8192a941d431fe25c380ce6e
오기선 <pakox123@gmail.com>
2024-05-03 15:25:33 +0900

    ✨ feat : 매물 리포트 페이지 컴포넌트 작성

--- frontend/src/pages/ReportPage.tsx
+++ frontend/src/pages/ReportPage.tsx
@@ -1,5 +1,14 @@
+import Header from '@/common/Header'
+import { ReportContainer } from './style/ReportPageStyle'
+import ReportList from '@/components/Report/ReportList'
+
 const ReportPage = () => {
-  return <div>레포트 페이지</div>
+  return (
+    <ReportContainer>
+      <Header title="매물 레포트" isSearch={false} rightIconSrc="" />
+      <ReportList></ReportList>
+    </ReportContainer>
+  )
 }
 
 export default ReportPage
