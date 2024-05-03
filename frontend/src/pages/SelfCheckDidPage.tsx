a08bb41a60eaa0f497cbd85a56c9488fe4cbf713
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-03 17:08:28 +0900

    ✨ feat : SimpleInput props 전달

--- frontend/src/pages/SelfCheckDidPage.tsx
+++ frontend/src/pages/SelfCheckDidPage.tsx
@@ -9,16 +9,28 @@ const SelfCheckDidPage = () => {
   return (
     <s.SelfUploadDidPageContainer>
       <Header title="부동산 DID 등록 요청" isSearch={false} rightIconSrc="" />
-      <div className="custom-button-container">
-        <CustomButtonStyle> 등록 요청하기 </CustomButtonStyle>
-      </div>
       <div className="info-text">
         등록하고자 하는 목적물의 정보를 기입해주세요.
       </div>
-      <AddressInput />
-      <SimpleInput />
-      <RealEstateTypeInput />
-      <SimpleInput />
+      <s.InputsContainer>
+        <AddressInput />
+        <SimpleInput
+          inputTextEnglish="owner"
+          inputTextKorean="소유자"
+          placeholder="홍길동"
+          unit={null}
+        />
+        <RealEstateTypeInput />
+        <SimpleInput
+          inputTextEnglish="area"
+          inputTextKorean="총 면적"
+          placeholder="33"
+          unit="m2"
+        />
+      </s.InputsContainer>
+      <div className="custom-button-container">
+        <CustomButtonStyle> 등록 요청하기 </CustomButtonStyle>
+      </div>
     </s.SelfUploadDidPageContainer>
   )
 }
