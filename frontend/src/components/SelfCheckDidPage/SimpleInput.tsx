9076b11a314b603d74e706b6628a6fd96618eacc
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-03 17:07:27 +0900

    ✨ feat : 간단한 input을 받는 SimpleInput 구현

--- frontend/src/components/SelfCheckDidPage/SimpleInput.tsx
+++ frontend/src/components/SelfCheckDidPage/SimpleInput.tsx
@@ -1,5 +1,22 @@
-const SimpleInput = () => {
-  return <div></div>
+import * as s from '@components/SelfCheckDidPage/style/SimpleInputStyle'
+
+interface SimpleInputPropsType {
+  inputTextKorean: string
+  inputTextEnglish: string
+  placeholder: string
+  unit: string | null
+}
+
+const SimpleInput = (props: SimpleInputPropsType) => {
+  return (
+    <s.SimpleInputContainer>
+      <div className="input-title">{props.inputTextKorean}</div>
+      <s.InputWrapper>
+        <input className="simple-input" placeholder={props.placeholder} />
+        <div className="unit"> {props.unit}</div>
+      </s.InputWrapper>
+    </s.SimpleInputContainer>
+  )
 }
 
 export default SimpleInput
