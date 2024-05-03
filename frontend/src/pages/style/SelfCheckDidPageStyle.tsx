b3db59610c9c9df91c175a5a6088ea6274b54b2f
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-03 14:06:57 +0900

    🎨 style : 목적물 정보를 기입하는 페이지 SelfCheckDidPage style 적용

--- frontend/src/pages/style/SelfCheckDidPageStyle.tsx
+++ frontend/src/pages/style/SelfCheckDidPageStyle.tsx
@@ -1,3 +1,22 @@
 import styled from 'styled-components'
 
-export const SelfUploadDidPageContainer = styled.div``
+export const SelfUploadDidPageContainer = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  padding-top: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .custom-button-container {
+    position: absolute;
+    bottom: 1rem;
+  }
+
+  .info-text {
+    width: 90%;
+    font-size: 1.1rem;
+    font-weight: 500;
+    margin: 5%;
+  }
+`
