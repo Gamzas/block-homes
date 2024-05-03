9a04312feb3a796d716833a57d18cfb056331725
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-03 14:06:41 +0900

    ✨ feat : 목적물 정보를 기입하는 페이지 SelfCheckDidPage 생성

--- frontend/src/pages/SelfCheckDidPage.tsx
+++ frontend/src/pages/SelfCheckDidPage.tsx
@@ -1,7 +1,19 @@
 import * as s from '@pages/style/SelfCheckDidPageStyle'
+import Header from '@common/Header'
+import { CustomButtonStyle } from '@common/style/CustomButtonStyle'
 
 const SelfCheckDidPage = () => {
-  return <s.SelfUploadDidPageContainer></s.SelfUploadDidPageContainer>
+  return (
+    <s.SelfUploadDidPageContainer>
+      <Header title="부동산 DID 등록 요청" isSearch={false} rightIconSrc="" />
+      <div className="custom-button-container">
+        <CustomButtonStyle> 등록 요청하기 </CustomButtonStyle>
+      </div>
+      <div className="info-text">
+        등록하고자 하는 목적물의 정보를 기입해주세요.
+      </div>
+    </s.SelfUploadDidPageContainer>
+  )
 }
 
 export default SelfCheckDidPage
