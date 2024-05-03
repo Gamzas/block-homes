0e83f13e3aa8e6cdfab1692089a7027b4aa4f7a0
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-03 11:23:09 +0900

    ✨ feat : 건물 형태 클릭시 list page로 넘어가도록 설정

--- frontend/src/components/MainPage/RealEstateCategory.tsx
+++ frontend/src/components/MainPage/RealEstateCategory.tsx
@@ -1,59 +1,65 @@
 import * as r from '@components/MainPage/style/RealEstateCategoryStyle'
 import { RealEstateCategoryType } from '@/types/estateListType'
+import { useNavigate } from 'react-router-dom'
 
 const RealEstateCategory = () => {
-	const realEstateCategoryList: RealEstateCategoryType[] = [
-		{
-			src: '/image/image_apartment.png',
-			title: '아파트',
-			transactionType1: '매매',
-			transactionType2: '전 · 월세',
-		},
-		{
-			src: '/image/image_villa_or_tworoom.png',
-			title: '빌라 · 투룸 +',
-			transactionType1: '매매',
-			transactionType2: '전 · 월세',
-		},
-		{
-			src: '/image/image_oneroom.png',
-			title: '원룸',
-			transactionType1: '전 · 월세',
-			transactionType2: null,
-		},
-		{
-			src: '/image/image_officetels.png',
-			title: '오피스텔',
-			transactionType1: '전 · 월세',
-			transactionType2: null,
-		},
-	]
+  const navigate = useNavigate()
+  const realEstateCategoryList: RealEstateCategoryType[] = [
+    {
+      src: '/image/image_apartment.png',
+      title: '아파트',
+      transactionType1: '매매',
+      transactionType2: '전 · 월세',
+    },
+    {
+      src: '/image/image_villa_or_tworoom.png',
+      title: '빌라 · 투룸 +',
+      transactionType1: '매매',
+      transactionType2: '전 · 월세',
+    },
+    {
+      src: '/image/image_oneroom.png',
+      title: '원룸',
+      transactionType1: '전 · 월세',
+      transactionType2: null,
+    },
+    {
+      src: '/image/image_officetels.png',
+      title: '오피스텔',
+      transactionType1: '전 · 월세',
+      transactionType2: null,
+    },
+  ]
 
-	return (
-		<r.RealEstateCategoryContainer>
-			<div className="real-estate-category-info-text">
-				어떤 집을 찾고 계세요?
-			</div>
-			<r.RealEstateCategories>
-				{realEstateCategoryList.map((realEstateCategory, index) => (
-					<r.CategoryContainer key={index}>
-						<div className="category-title">{realEstateCategory.title}</div>
-						<div className="category-transaction-type">
-							{realEstateCategory.transactionType1}
-						</div>
-						<div className="category-transaction-type">
-							{realEstateCategory.transactionType2}
-						</div>
-						<img
-							alt={`${realEstateCategory.title}`}
-							src={`${realEstateCategory.src}`}
-							className="category-img"
-						/>
-					</r.CategoryContainer>
-				))}
-			</r.RealEstateCategories>
-		</r.RealEstateCategoryContainer>
-	)
+  const navigateToCategory = () => {
+    navigate('estate')
+  }
+
+  return (
+    <r.RealEstateCategoryContainer>
+      <div className="real-estate-category-info-text">
+        어떤 집을 찾고 계세요?
+      </div>
+      <r.RealEstateCategories>
+        {realEstateCategoryList.map((realEstateCategory, index) => (
+          <r.CategoryContainer key={index} onClick={navigateToCategory}>
+            <div className="category-title">{realEstateCategory.title}</div>
+            <div className="category-transaction-type">
+              {realEstateCategory.transactionType1}
+            </div>
+            <div className="category-transaction-type">
+              {realEstateCategory.transactionType2}
+            </div>
+            <img
+              alt={`${realEstateCategory.title}`}
+              src={`${realEstateCategory.src}`}
+              className="category-img"
+            />
+          </r.CategoryContainer>
+        ))}
+      </r.RealEstateCategories>
+    </r.RealEstateCategoryContainer>
+  )
 }
 
 export default RealEstateCategory
