35e9f3d655dd82414ee8fb2e8cca2a1cbda444f0
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-03 12:07:13 +0900

    ♻️ refactor : tab-size 수정

--- frontend/src/pages/style/RegistrateEstatePageStyle.tsx
+++ frontend/src/pages/style/RegistrateEstatePageStyle.tsx
@@ -1,39 +1,39 @@
 import styled from 'styled-components'
-import {Button} from "@common/style/Button";
+import { CustomButtonStyle } from '@common/style/CustomButtonStyle'
 
 export const RegistrateEstatePageStyleContainer = styled.div`
-    width: 100%;
-    height: 100vh;
+  width: 100%;
+  height: 100vh;
 
-    position: relative;
+  position: relative;
 `
 
 export const AccodionWrapper = styled.div`
-    width: 100%;
-    height: 80%;
-    position: absolute;
-    top: 50px;
-    left: 0;
+  width: 100%;
+  height: 80%;
+  position: absolute;
+  top: 50px;
+  left: 0;
 
-    overflow-y: auto;
-    // Chrome, Edge, Safari를 위한 스타일
+  overflow-y: auto;
+  // Chrome, Edge, Safari를 위한 스타일
 
-    &::-webkit-scrollbar {
-        display: none; // 스크롤바 영역을 표시하지 않습니다.
-    }
+  &::-webkit-scrollbar {
+    display: none; // 스크롤바 영역을 표시하지 않습니다.
+  }
 
-    // Firefox를 위한 스타일
-    scrollbar-width: none; // 스크롤바를 숨깁니다.
+  // Firefox를 위한 스타일
+  scrollbar-width: none; // 스크롤바를 숨깁니다.
 `
 
-export const NextButton = styled(Button)`
-    position: fixed;
-    bottom: 4vh;
-    left: 50%;
-    transform: translateX(-50%);
+export const NextButton = styled(CustomButtonStyle)`
+  position: fixed;
+  bottom: 4vh;
+  left: 50%;
+  transform: translateX(-50%);
 
-    &:hover,
-    &:active {
-        transform: translateX(-50%);
-    }
+  &:hover,
+  &:active {
+    transform: translateX(-50%);
+  }
 `
