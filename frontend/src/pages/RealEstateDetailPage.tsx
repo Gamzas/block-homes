6cae8fad5b509220ad0e6008431ab62331757cf1
정유나 <jyn7077@naver.com>
2024-05-07 08:19:16 +0900

    ✨ feat : 디테일 페이지 요소 추가

--- frontend/src/pages/RealEstateDetailPage.tsx
+++ frontend/src/pages/RealEstateDetailPage.tsx
@@ -1,18 +1,18 @@
 import * as r from '@pages/style/RealEstatePageStyle'
 import Header from '@/common/Header'
-import Footer from '@/common/Footer'
 import ItemSafetyCard from '@/components/EstateDetailPage/ItemSafetyCard'
+import DetailTabMenu from '@/components/EstateDetailPage/DetailTabMenu'
+import RoomInfo from '@/components/EstateDetailPage/RoomInfo'
+import DetailFooter from '@/components/EstateDetailPage/DetailFooter'
 
 const RealEstateDetailPage = () => {
   return (
     <r.Container>
-      <Header
-        title={'매물 상세 조회'}
-        isSearch={false}
-        rightIconSrc={''}
-      />
+      <Header title={'매물 상세 조회'} isSearch={false} rightIconSrc={''} />
+      <DetailTabMenu />
       <ItemSafetyCard />
-      <Footer />
+      <RoomInfo />
+      <DetailFooter />
     </r.Container>
   )
 }
