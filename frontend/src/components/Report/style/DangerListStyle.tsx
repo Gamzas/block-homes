f76c2d4c4bb8a33d957887d071f9420bfb8b5e17
giseon <pakox123@gmail.com>
2024-05-06 19:31:26 +0900

    ✨ feat : 위험도 리스트 디테일 틀 작성

---
+++ frontend/src/components/Report/style/DangerListStyle.tsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+
+export const DangerListContainer = styled.div`
+
+
+  margin-top: 1rem;
+  width: 95%;
+  min-height: 10vh;
+  border-radius: 0.93313rem;
+  background: #f3f0f7;
+  box-shadow: 0px 5.972px 5.972px 0px rgba(0, 0, 0, 0.25);
+  flex-shrink: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1rem ;
+
+  .title-container{
+    width: 89%;
+    font-size: 1.2rem;
+    font-weight: 600;
+    margin-bottom: 2rem;
+    align-items: center;
+    display: flex;
+  }
+    
+
+`
