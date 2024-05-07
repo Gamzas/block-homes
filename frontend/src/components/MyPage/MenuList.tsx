56698870c01bf6e461d7d7cc2c253ce006bec45d
정유나 <jyn7077@naver.com>
2024-05-07 14:07:58 +0900

    ✨ feat : 마이페이지 메뉴 추가

---
+++ frontend/src/components/MyPage/MenuList.tsx
@@ -0,0 +1,21 @@
+import * as m from '@components/MyPage/style/MenuList'
+
+const MenuList = () => {
+  const menuArr = ['내 관심매물', '현재 계약중인 부동산']
+  return (
+    <m.MenuWrapper>
+      <m.MenuContainer>
+        {menuArr.map((menu, index) => (
+          <div className="title-box">
+            <div className="title" key={index}>
+              {menu}
+            </div>
+            <img className="arrow-icon" src="icon/icon_back_arrow.png" alt="" />
+          </div>
+        ))}
+      </m.MenuContainer>
+    </m.MenuWrapper>
+  )
+}
+
+export default MenuList
