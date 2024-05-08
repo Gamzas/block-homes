acff4aacc6cfc578d6dd73e208649d26fc4a58b4
송강산 <rkdtks0816@naver.com>
2024-05-08 18:03:46 +0900

    🧪 test : ClaimCredential 테스트

--- frontend/src/apis/kaikasApi.ts
+++ frontend/src/apis/kaikasApi.ts
@@ -1,17 +1,160 @@
-import axios from 'axios';
-import {PrepareResponse, ResultResponse} from "@/types/components/kaikasType";
-import {KAIKAS_API_BASE_HEADERS, KAIKAS_API_BASE_PREPARE_REQUEST_BODY, KAIKAS_API_BASE_URL} from "@constants/kaikas";
+import axios from 'axios'
+import { PrepareResponse, ResultResponse } from '@/types/components/kaikasType'
+import { KAIKAS_API_BASE_HEADERS, KAIKAS_API_BASE_PREPARE_REQUEST_BODY, KAIKAS_API_BASE_URL } from '@constants/kaikas'
 
 
 export const prepareAuthRequest = async (): Promise<PrepareResponse> => {
-    const response = await axios.post<PrepareResponse>(`${KAIKAS_API_BASE_URL}/prepare`, {
-        ...KAIKAS_API_BASE_PREPARE_REQUEST_BODY,
-        type: 'auth',
-    }, KAIKAS_API_BASE_HEADERS);
-    return response.data;
+  const response = await axios.post<PrepareResponse>(`${KAIKAS_API_BASE_URL}/prepare`, {
+    ...KAIKAS_API_BASE_PREPARE_REQUEST_BODY,
+    type: 'auth',
+  }, KAIKAS_API_BASE_HEADERS)
+  return response.data
+}
+
+export const prepareClaimCredentialRequest = async (): Promise<PrepareResponse> => {
+  const response = await axios.post<PrepareResponse>(`${KAIKAS_API_BASE_URL}/prepare`, {
+    ...KAIKAS_API_BASE_PREPARE_REQUEST_BODY,
+    type: 'execute_contract',
+    transaction: {
+      abi: {
+        'inputs': [
+          {
+            'components': [
+              {
+                'internalType': 'string',
+                'name': 'context',
+                'type': 'string',
+              },
+              {
+                'internalType': 'string',
+                'name': 'id',
+                'type': 'string',
+              },
+              {
+                'internalType': 'string',
+                'name': 'credentialType',
+                'type': 'string',
+              },
+              {
+                'internalType': 'string',
+                'name': 'issuer',
+                'type': 'string',
+              },
+              {
+                'internalType': 'uint256',
+                'name': 'issuanceDate',
+                'type': 'uint256',
+              },
+              {
+                'components': [
+                  {
+                    'internalType': 'string',
+                    'name': 'id',
+                    'type': 'string',
+                  },
+                  {
+                    'components': [
+                      {
+                        'internalType': 'string',
+                        'name': 'id',
+                        'type': 'string',
+                      },
+                      {
+                        'components': [
+                          {
+                            'internalType': 'string',
+                            'name': 'value',
+                            'type': 'string',
+                          },
+                          {
+                            'internalType': 'string',
+                            'name': 'lang',
+                            'type': 'string',
+                          },
+                        ],
+                        'internalType': 'struct OwnershipVCStruct.Name',
+                        'name': 'name',
+                        'type': 'tuple',
+                      },
+                    ],
+                    'internalType': 'struct OwnershipVCStruct.OwnerOf',
+                    'name': 'ownerOf',
+                    'type': 'tuple',
+                  },
+                ],
+                'internalType': 'struct OwnershipVCStruct.CredentialSubject',
+                'name': 'credentialSubject',
+                'type': 'tuple',
+              },
+              {
+                'components': [
+                  {
+                    'internalType': 'string',
+                    'name': 'proofType',
+                    'type': 'string',
+                  },
+                  {
+                    'internalType': 'string',
+                    'name': 'proofPurpose',
+                    'type': 'string',
+                  },
+                  {
+                    'internalType': 'string',
+                    'name': 'verificationMethod',
+                    'type': 'string',
+                  },
+                  {
+                    'internalType': 'string',
+                    'name': 'jws',
+                    'type': 'string',
+                  },
+                ],
+                'internalType': 'struct OwnershipVCStruct.Proof',
+                'name': 'proof',
+                'type': 'tuple',
+              },
+            ],
+            'internalType': 'struct OwnershipVCStruct.Credential',
+            'name': '_inputCredential',
+            'type': 'tuple',
+          },
+        ],
+        'name': 'claimCredential',
+        'outputs': [],
+        'stateMutability': 'nonpayable',
+        'type': 'function',
+      },
+      value: '0',
+      to: '0x98f5E0a37d17877125d4B00De9E474dbEbE8A01C',
+      params: {
+        context: 'http://example.org',
+        id: 'credential-123',
+        credentialType: 'OwnershipCredential',
+        issuer: '0xIssuerAddress',
+        issuanceDate: 1625097600,
+        credentialSubject: {
+          id: '0xSubjectAddress',
+          ownerOf: {
+            id: '0xOwnerAddress',
+            name: {
+              value: 'John Doe',
+              lang: 'en',
+            },
+          },
+        },
+        proof: {
+          proofType: 'Ecdsa',
+          proofPurpose: 'assertionMethod',
+          verificationMethod: '0xVerificationMethod',
+          jws: 'signatureValue',
+        },
+      },
+    },
+  }, KAIKAS_API_BASE_HEADERS)
+  return response.data
 }
 
 export const getResult = async (request_key: string): Promise<ResultResponse> => {
-    const response = await axios.get<ResultResponse>(`${KAIKAS_API_BASE_URL}/result/${request_key}`, KAIKAS_API_BASE_HEADERS);
-    return response.data;
+  const response = await axios.get<ResultResponse>(`${KAIKAS_API_BASE_URL}/result/${request_key}`, KAIKAS_API_BASE_HEADERS)
+  return response.data
 }
--- frontend/src/constants/contract.ts
+++ frontend/src/constants/contract.ts
@@ -1,106 +1,347 @@
 // 네트워크 주소
 export const blockChainEndpoint = 'https://api.baobab.klaytn.net:8651'
 // 스마트 컨트랙트 주소
-export const contractAddress = '0x10FA45F4A36d12A936e918FB6Bb63EF36fcE5aFB'
+export const contractAddress = '0x98f5E0a37d17877125d4B00De9E474dbEbE8A01C'
 // ABI
 export const contractABI = [
   {
-    inputs: [],
-    stateMutability: 'nonpayable',
-    type: 'constructor',
+    'inputs': [],
+    'stateMutability': 'nonpayable',
+    'type': 'constructor',
   },
   {
-    inputs: [],
-    name: 'getContractBalance',
-    outputs: [
+    'inputs': [
       {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
+        'internalType': 'address',
+        'name': 'owner',
+        'type': 'address',
       },
     ],
-    stateMutability: 'view',
-    type: 'function',
+    'name': 'OwnableInvalidOwner',
+    'type': 'error',
   },
   {
-    inputs: [],
-    name: 'getMyAddress',
-    outputs: [
+    'inputs': [
       {
-        internalType: 'address',
-        name: '',
-        type: 'address',
+        'internalType': 'address',
+        'name': 'account',
+        'type': 'address',
       },
     ],
-    stateMutability: 'view',
-    type: 'function',
+    'name': 'OwnableUnauthorizedAccount',
+    'type': 'error',
   },
   {
-    inputs: [],
-    name: 'getMyBalance',
-    outputs: [
+    'anonymous': false,
+    'inputs': [
       {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
+        'indexed': true,
+        'internalType': 'address',
+        'name': 'previousOwner',
+        'type': 'address',
+      },
+      {
+        'indexed': true,
+        'internalType': 'address',
+        'name': 'newOwner',
+        'type': 'address',
       },
     ],
-    stateMutability: 'view',
-    type: 'function',
+    'name': 'OwnershipTransferred',
+    'type': 'event',
   },
   {
-    inputs: [],
-    name: 'owner',
-    outputs: [
+    'anonymous': false,
+    'inputs': [
       {
-        internalType: 'address',
-        name: '',
-        type: 'address',
+        'indexed': false,
+        'internalType': 'string',
+        'name': 'id',
+        'type': 'string',
       },
     ],
-    stateMutability: 'view',
-    type: 'function',
+    'name': 'VCcreated',
+    'type': 'event',
   },
   {
-    inputs: [
+    'anonymous': false,
+    'inputs': [
       {
-        internalType: 'uint256',
-        name: '_value',
-        type: 'uint256',
+        'indexed': false,
+        'internalType': 'string',
+        'name': 'id',
+        'type': 'string',
       },
     ],
-    name: 'robFromContract',
-    outputs: [
-      {
-        internalType: 'bool',
-        name: '',
-        type: 'bool',
-      },
-    ],
-    stateMutability: 'nonpayable',
-    type: 'function',
+    'name': 'VCdeleted',
+    'type': 'event',
   },
   {
-    inputs: [
+    'inputs': [
       {
-        internalType: 'uint256',
-        name: '_value',
-        type: 'uint256',
+        'components': [
+          {
+            'internalType': 'string',
+            'name': 'context',
+            'type': 'string',
+          },
+          {
+            'internalType': 'string',
+            'name': 'id',
+            'type': 'string',
+          },
+          {
+            'internalType': 'string',
+            'name': 'credentialType',
+            'type': 'string',
+          },
+          {
+            'internalType': 'string',
+            'name': 'issuer',
+            'type': 'string',
+          },
+          {
+            'internalType': 'uint256',
+            'name': 'issuanceDate',
+            'type': 'uint256',
+          },
+          {
+            'components': [
+              {
+                'internalType': 'string',
+                'name': 'id',
+                'type': 'string',
+              },
+              {
+                'components': [
+                  {
+                    'internalType': 'string',
+                    'name': 'id',
+                    'type': 'string',
+                  },
+                  {
+                    'components': [
+                      {
+                        'internalType': 'string',
+                        'name': 'value',
+                        'type': 'string',
+                      },
+                      {
+                        'internalType': 'string',
+                        'name': 'lang',
+                        'type': 'string',
+                      },
+                    ],
+                    'internalType': 'struct OwnershipVCStruct.Name',
+                    'name': 'name',
+                    'type': 'tuple',
+                  },
+                ],
+                'internalType': 'struct OwnershipVCStruct.OwnerOf',
+                'name': 'ownerOf',
+                'type': 'tuple',
+              },
+            ],
+            'internalType': 'struct OwnershipVCStruct.CredentialSubject',
+            'name': 'credentialSubject',
+            'type': 'tuple',
+          },
+          {
+            'components': [
+              {
+                'internalType': 'string',
+                'name': 'proofType',
+                'type': 'string',
+              },
+              {
+                'internalType': 'string',
+                'name': 'proofPurpose',
+                'type': 'string',
+              },
+              {
+                'internalType': 'string',
+                'name': 'verificationMethod',
+                'type': 'string',
+              },
+              {
+                'internalType': 'string',
+                'name': 'jws',
+                'type': 'string',
+              },
+            ],
+            'internalType': 'struct OwnershipVCStruct.Proof',
+            'name': 'proof',
+            'type': 'tuple',
+          },
+        ],
+        'internalType': 'struct OwnershipVCStruct.Credential',
+        'name': '_inputCredential',
+        'type': 'tuple',
       },
     ],
-    name: 'transferToContract',
-    outputs: [
-      {
-        internalType: 'bool',
-        name: '',
-        type: 'bool',
-      },
-    ],
-    stateMutability: 'payable',
-    type: 'function',
+    'name': 'claimCredential',
+    'outputs': [],
+    'stateMutability': 'nonpayable',
+    'type': 'function',
   },
   {
-    stateMutability: 'payable',
-    type: 'receive',
+    'inputs': [
+      {
+        'internalType': 'string',
+        'name': 'id',
+        'type': 'string',
+      },
+    ],
+    'name': 'deleteVC',
+    'outputs': [],
+    'stateMutability': 'nonpayable',
+    'type': 'function',
+  },
+  {
+    'inputs': [
+      {
+        'internalType': 'string',
+        'name': 'id',
+        'type': 'string',
+      },
+    ],
+    'name': 'getVC',
+    'outputs': [
+      {
+        'components': [
+          {
+            'internalType': 'string',
+            'name': 'context',
+            'type': 'string',
+          },
+          {
+            'internalType': 'string',
+            'name': 'id',
+            'type': 'string',
+          },
+          {
+            'internalType': 'string',
+            'name': 'credentialType',
+            'type': 'string',
+          },
+          {
+            'internalType': 'string',
+            'name': 'issuer',
+            'type': 'string',
+          },
+          {
+            'internalType': 'uint256',
+            'name': 'issuanceDate',
+            'type': 'uint256',
+          },
+          {
+            'components': [
+              {
+                'internalType': 'string',
+                'name': 'id',
+                'type': 'string',
+              },
+              {
+                'components': [
+                  {
+                    'internalType': 'string',
+                    'name': 'id',
+                    'type': 'string',
+                  },
+                  {
+                    'components': [
+                      {
+                        'internalType': 'string',
+                        'name': 'value',
+                        'type': 'string',
+                      },
+                      {
+                        'internalType': 'string',
+                        'name': 'lang',
+                        'type': 'string',
+                      },
+                    ],
+                    'internalType': 'struct OwnershipVCStruct.Name',
+                    'name': 'name',
+                    'type': 'tuple',
+                  },
+                ],
+                'internalType': 'struct OwnershipVCStruct.OwnerOf',
+                'name': 'ownerOf',
+                'type': 'tuple',
+              },
+            ],
+            'internalType': 'struct OwnershipVCStruct.CredentialSubject',
+            'name': 'credentialSubject',
+            'type': 'tuple',
+          },
+          {
+            'components': [
+              {
+                'internalType': 'string',
+                'name': 'proofType',
+                'type': 'string',
+              },
+              {
+                'internalType': 'string',
+                'name': 'proofPurpose',
+                'type': 'string',
+              },
+              {
+                'internalType': 'string',
+                'name': 'verificationMethod',
+                'type': 'string',
+              },
+              {
+                'internalType': 'string',
+                'name': 'jws',
+                'type': 'string',
+              },
+            ],
+            'internalType': 'struct OwnershipVCStruct.Proof',
+            'name': 'proof',
+            'type': 'tuple',
+          },
+        ],
+        'internalType': 'struct OwnershipVCStruct.Credential',
+        'name': '',
+        'type': 'tuple',
+      },
+    ],
+    'stateMutability': 'view',
+    'type': 'function',
+  },
+  {
+    'inputs': [],
+    'name': 'owner',
+    'outputs': [
+      {
+        'internalType': 'address',
+        'name': '',
+        'type': 'address',
+      },
+    ],
+    'stateMutability': 'view',
+    'type': 'function',
+  },
+  {
+    'inputs': [],
+    'name': 'renounceOwnership',
+    'outputs': [],
+    'stateMutability': 'nonpayable',
+    'type': 'function',
+  },
+  {
+    'inputs': [
+      {
+        'internalType': 'address',
+        'name': 'newOwner',
+        'type': 'address',
+      },
+    ],
+    'name': 'transferOwnership',
+    'outputs': [],
+    'stateMutability': 'nonpayable',
+    'type': 'function',
   },
 ]
