685b4b582c861cbc27d22a5ad43a3cc25cb00a90
천세진 <sejinnnnnn@naver.com>
2024-05-09 07:14:36 +0900

    ✨ feat : 전세 컨트랙트 임시저장

---
+++ blockchain/contract-real-estate/contracts/long-term-rent/LongTermRentStruct.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library LongTermRentStruct {
+
+    struct LongTermRentContract {
+        string landlordDID; // 임대인의 분산식 식별자
+        string tenantDID; // 임차인의 분산식 식별자
+        uint16 leasePeriod; // 임대 기간 (월 단위)
+        uint256 deposit; // 보증금
+        string propertyDID; // 부동산의 분산식 식별자
+        uint256 contractDate; // 계약 날짜 (timestamp)
+        string[] terms; // 계약의 세부 조건들
+
+        bytes landlordSignature; // 임대인의 서명
+        bytes tenantSignature; // 임차인의 서명
+    }
+    
+}
