72cdd5adbb90bd0a4d2b1873bfb86b4bdd8a933a
천세진 <sejinnnnnn@naver.com>
2024-05-10 02:16:17 +0900

    ✨ feat : 채팅 서버 구현중

---
+++ backend/chattings/src/main/java/com/blockhomes/chattings/controller/ChatRoomController.java
@@ -0,0 +1,11 @@
+package com.blockhomes.chattings.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@RequiredArgsConstructor
+@Slf4j
+@Controller
+public class ChatRoomController {
+}
