04aa22c7ae37578ef2db497c998377ecbeca9e6c
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-10 01:51:53 +0900

    ✨ feat : Infopage들에서 공통으로 쓰이는 InfoPageHeader 생성

---
+++ frontend/src/common/InfoPageHeader.tsx
@@ -0,0 +1,7 @@
+import * as i from '@common/style/InfoPageHeaderStyle'
+
+const InfoPageHeader = () => {
+  return <i.InfoPageHeaderContainer></i.InfoPageHeaderContainer>
+}
+
+export default InfoPageHeader
