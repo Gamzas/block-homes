968fdda50b14851c02da132174a63036d237cad1
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-09 16:06:59 +0900

    ✨ feat : 숫자로 넘어오는 매물 type을 한글로 맞춰서 변경

--- frontend/src/components/ChatPage/ChatPreviewComponent.tsx
+++ frontend/src/components/ChatPage/ChatPreviewComponent.tsx
@@ -1,5 +1,5 @@
 import * as c from '@components/ChatPage/style/ChatPreviewComponentStyle'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 interface ChatComponentProps {
   representativeImage: string
@@ -16,12 +16,28 @@ const ChatPreviewComponent: React.FC<ChatComponentProps> = ({
   price,
   lastChat,
 }) => {
+  const [typeOfNumber, setTypeOfNumber] = useState('type')
+
+  useEffect(() => {
+    switch (type) {
+      case 0:
+        setTypeOfNumber('매매')
+        break
+      case 1:
+        setTypeOfNumber('전세')
+        break
+      case 2:
+        setTypeOfNumber('월세')
+        break
+    }
+  }, [price])
+
   return (
     <c.ChatComponentContainer>
       <img className="estate-image" alt="매물 사진" src={representativeImage} />
       <c.ChatComponentRightContainer>
         <div className="address">{address}</div>
-        <div className="type">{type}</div>
+        <div className="type">{typeOfNumber}</div>
         <div className="price">{price} 원</div>
         <div className="last-chat">{lastChat}</div>
       </c.ChatComponentRightContainer>
