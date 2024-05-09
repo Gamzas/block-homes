58763a219b2a209baebe53acc864f75057e6740a
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-09 15:02:11 +0900

    ✨ feat : ChatPage 컴포넌트 조합

--- frontend/src/pages/ChatPage.tsx
+++ frontend/src/pages/ChatPage.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import * as c from '@pages/style/ChatPageStyle'
+import Footer from '@common/Footer'
+import ChatList from '@components/ChatPage/ChatList'
 
 const ChatPage = () => {
-  return <div></div>
+  return (
+    <c.ChatPageContainer>
+      <ChatList />
+      <Footer />
+    </c.ChatPageContainer>
+  )
 }
 
 export default ChatPage
