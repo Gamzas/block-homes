196fd246302832f9e79e963422e4e37c837d7345
정유나 <jyn7077@naver.com>
2024-05-09 14:25:52 +0900

    ✨ feat : 검색어 입력 기능 구현

--- frontend/src/common/SearchBar.tsx
+++ frontend/src/common/SearchBar.tsx
@@ -1,13 +1,37 @@
 import * as s from '@common/style/SearchBarStyle'
+import { useRef, useState } from 'react'
 
 const SearchBar = () => {
+  const [searchKeyword, setSearchKeyword] = useState<string>('')
+  const searchInput = useRef<HTMLInputElement>()
+  const goSearch = () => {
+    if (searchKeyword.trim().length < 1) {
+      alert('최소 1글자 이상 입력해주세요')
+      if (searchInput.current) {
+        searchInput.current.focus()
+      }
+      return
+    } else {
+      console.log('검색!!')
+      return
+    }
+  }
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchKeyword(e.target.value)
+  }
   return (
     <s.SearchBarContainer>
-      <input className="input-box" />
+      <input
+        className="input-box"
+        value={searchKeyword}
+        onChange={onChangeHandler}
+        ref={searchInput}
+      />
       <img
         alt="검색"
         className="icon-find"
         src="/icon/icon_reading_glass.png"
+        onClick={goSearch}
       />
     </s.SearchBarContainer>
   )
