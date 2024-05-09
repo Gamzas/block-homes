0fa4c3034fdce3cda8c630bc84917e0a0e34117c
정유나 <jyn7077@naver.com>
2024-05-10 08:02:45 +0900

    ✨ feat : 로그아웃 컴포넌트 추가

---
+++ frontend/src/components/MyPage/Logout.tsx
@@ -0,0 +1,12 @@
+import * as l from '@components/MyPage/style/LogoutStyle'
+
+
+const Logout = () => {
+    return (
+        <l.LogoutContainer>
+            
+        </l.LogoutContainer>
+    );
+};
+
+export default Logout;
