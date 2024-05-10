7303a162ab34d640c52360d367dc9abfd89926de
정유나 <jyn7077@naver.com>
2024-05-10 14:13:38 +0900

    🎨 style : 로그아웃 레이아웃 설정

--- frontend/src/components/MyPage/style/LogoutStyle.tsx
+++ frontend/src/components/MyPage/style/LogoutStyle.tsx
@@ -2,8 +2,22 @@ import styled from 'styled-components'
 
 export const LogoutContainer = styled.div`
   width: 100%;
-  height: 5rem;
-  /* border: solid 1px red; */
-  /* position: relative; */
-  /* bottom: 60px; */
+  height: 2rem;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 1rem;
+  .txt {
+    width: 90%;
+    color: #5c5c5c;
+    font-size: 0.875rem;
+    font-weight: 400;
+    margin-top: 1rem;
+  }
+`
+export const EmptyContainer = styled.div`
+  width: 100%;
+  height: 0.6875rem;
+  background: #f7f8fa;
+  margin-bottom: 4rem;
+  /* margin-top: 2rem; */
 `
