1f292ad4b52e22ea249db3e0a5f6691ac1af251d
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-10 23:17:26 +0900

    ✨ feat : RealEstateCheckListPage Header 생성

--- frontend/src/pages/RealEstateCheckListPage.tsx
+++ frontend/src/pages/RealEstateCheckListPage.tsx
@@ -1,5 +1,21 @@
+import * as r from '@pages/style/RealEstateCheckListPageStyle'
+
 const RealEstateCheckListPage = () => {
-  return <div></div>
+  return (
+    <r.RealEstateCheckListPageContainer>
+      <r.RealEstateCheckListPageHeader>
+        <div className="check-list-title">매물 체크리스트</div>
+        <div className="check-list-info">
+          해당 체크리스트는 임대(매도)인에게도 공유됩니다.
+        </div>
+        <img
+          alt="체크리스트"
+          src="/image/image_check_list.png"
+          className="check-list-image"
+        />
+      </r.RealEstateCheckListPageHeader>
+    </r.RealEstateCheckListPageContainer>
+  )
 }
 
 export default RealEstateCheckListPage
