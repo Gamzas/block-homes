935a6286c9ca93f985b220d8c0c9fbf618d83d78
정유나 <jyn7077@naver.com>
2024-05-10 14:45:17 +0900

    🎨 style : 마이페이지 레이아웃 수정

--- frontend/src/components/MyPage/Logout.tsx
+++ frontend/src/components/MyPage/Logout.tsx
@@ -2,12 +2,11 @@ import * as l from '@components/MyPage/style/LogoutStyle'
 
 const Logout = () => {
   return (
-    <>
+    <l.LogoutWrapper>
       <l.LogoutContainer>
         <div className="txt">로그아웃</div>
       </l.LogoutContainer>
-      <l.EmptyContainer />
-    </>
+    </l.LogoutWrapper>
   )
 }
 
