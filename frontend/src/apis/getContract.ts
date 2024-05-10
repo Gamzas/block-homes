a3255124a15e70068f677caf836382b32ea16986
송강산 <rkdtks0816@naver.com>
2024-05-10 18:02:21 +0900

    ✨ feat : ether 테스트 코드

--- frontend/src/apis/getContract.ts
+++ frontend/src/apis/getContract.ts
@@ -1,17 +1,18 @@
-import Web3 from 'web3'
+import { ethers } from 'ethers'
 import { ABI_ARRAY, BLOCK_CHAIN_ENDPOINT, CONTRACT_ADDRESS } from '@constants/abi'
 
 export const getContract = () => {
-  const web3 = new Web3(BLOCK_CHAIN_ENDPOINT)
-  return new web3.eth.Contract(ABI_ARRAY, CONTRACT_ADDRESS).methods
+  const provider = new ethers.providers.JsonRpcProvider(BLOCK_CHAIN_ENDPOINT)
+  return new ethers.Contract(CONTRACT_ADDRESS, ABI_ARRAY, provider)
 }
 
 // 예시
 // const getContractBalance = async () => {
 //   try {
-//     const result = await getContract().getContractBalance().call()
-//     console.log('Contract Balance:', result)
+//     const contract = getContract();
+//     const result = await contract.getContractBalance();
+//     console.log('계약 잔액:', result.toString());
 //   } catch (error) {
-//     console.error('Failed to get contract balance:', error)
+//     console.error('계약 잔액 가져오기 실패:', error);
 //   }
-// }
+// };
