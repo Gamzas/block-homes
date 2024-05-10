8f4a003992f73199e615cec59869f3087b2f20c2
정유나 <jyn7077@naver.com>
2024-05-10 13:41:01 +0900

    🎨 style : 지갑주소 레이아웃 설정

---
+++ frontend/src/components/MyPage/style/WalletAddressStyle.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const WalletAddressContainer = styled.div`
+  width: 13.5rem;
+  height: 0.9375rem;
+  border-radius: 0.33419rem;
+  background: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0.3rem;
+  .address {
+    color: #555050;
+    font-size: 0.49444rem;
+    font-weight: 600;
+  }
+`
