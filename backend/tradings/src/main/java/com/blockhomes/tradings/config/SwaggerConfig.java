45087f20627c661c0bb98fb33bab90e437f3641a
천세진 <sejinnnnnn@naver.com>
2024-05-11 12:44:05 +0900

    ✨ feat : springdoc (Swagger) 초기 설정

---
+++ backend/tradings/src/main/java/com/blockhomes/tradings/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.blockhomes.tradings.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+            .components(new Components())
+            .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+            .title("Block Homes Tradings Server")
+            .description("블록홈즈 매물 등록 및 거래 백엔드 서버입니다.")
+            .version("1.0.0");
+    }
+
+}
