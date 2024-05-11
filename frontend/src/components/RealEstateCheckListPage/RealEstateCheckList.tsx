fb052e58cd85672194296c9a85e408f7a766bd1c
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-11 22:39:18 +0900

    ✨ feat : store에서 각 질문에 대한 체크 여부를 판단해서 렌더링 하도록 설정

--- frontend/src/components/RealEstateCheckListPage/RealEstateCheckList.tsx
+++ frontend/src/components/RealEstateCheckListPage/RealEstateCheckList.tsx
@@ -1,19 +1,43 @@
 import * as r from '@components/RealEstateCheckListPage/style/RealEstateCheckListStyle'
 import { useAtom } from 'jotai'
-import { selectedCheckListTypeIndex } from '@stores/atoms/checkListStore'
-import { REAL_ESTATE_CHECK_LIST_DATA } from '@constants/RealEstateCheckListData'
+import {
+  checkListState,
+  selectedCheckListTypeIndex,
+} from '@stores/atoms/checkListStore'
 
 const RealEstateCheckList = () => {
   const [selectedCheckListType] = useAtom(selectedCheckListTypeIndex)
+  const [checkListResult, setCheckListResult] = useAtom(checkListState)
+
+  const updateCheckListResult = (
+    sectionIndex: number,
+    itemIndex: number,
+    isChecked: boolean,
+  ) => {
+    const newCheckList = [...checkListResult]
+    newCheckList[sectionIndex].content[itemIndex].checked = isChecked
+    setCheckListResult(newCheckList)
+  }
 
   return (
     <r.RealEstateCheckListContainer>
-      {REAL_ESTATE_CHECK_LIST_DATA[selectedCheckListType].content.map(
+      {checkListResult[selectedCheckListType].content.map(
         (realEstateCheckListContent, index) => (
           <r.RealEstateCheckListContentContainer key={index}>
-            <input className="checklist-button" type="checkbox" />
+            <input
+              className="checklist-button"
+              type="checkbox"
+              checked={realEstateCheckListContent.checked}
+              onChange={e =>
+                updateCheckListResult(
+                  selectedCheckListType,
+                  index,
+                  e.target.checked,
+                )
+              }
+            />
             <label className="checklist-content">
-              {realEstateCheckListContent}
+              {realEstateCheckListContent.question}
             </label>
           </r.RealEstateCheckListContentContainer>
         ),
