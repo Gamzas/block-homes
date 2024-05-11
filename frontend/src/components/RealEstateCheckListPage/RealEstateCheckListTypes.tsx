ff7eb95412af4d66cad2d8842804cd5942120bad
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-11 16:04:24 +0900

    ♻️ refactor : REAL_ESTATE_CHECK_LIST_DATA를 바로 사용하도록 수정

--- frontend/src/components/RealEstateCheckListPage/RealEstateCheckListTypes.tsx
+++ frontend/src/components/RealEstateCheckListPage/RealEstateCheckListTypes.tsx
@@ -3,28 +3,24 @@ import * as r from '@components/RealEstateCheckListPage/style/RealEstateCheckLis
 import { REAL_ESTATE_CHECK_LIST_DATA } from '@constants/RealEstateCheckListData'
 
 const RealEstateCheckListTypes = () => {
-  const realEstateCheckListTypesData = REAL_ESTATE_CHECK_LIST_DATA
-
   const selectType = (index: number) => {
     console.log(index)
   }
 
   return (
     <r.RealEstateCheckListTypesContainer>
-      {realEstateCheckListTypesData.map(
-        (realEstateCheckListTypeData, index) => (
-          <r.RealEstateCheckListType onClick={() => selectType(index)}>
-            <img
-              src={realEstateCheckListTypeData.src}
-              alt={realEstateCheckListTypeData.title}
-              className="real-estate-checklist-type"
-            />
-            <div className="real-estate-checklist-title">
-              {realEstateCheckListTypeData.title}
-            </div>
-          </r.RealEstateCheckListType>
-        ),
-      )}
+      {REAL_ESTATE_CHECK_LIST_DATA.map((realEstateCheckListTypeData, index) => (
+        <r.RealEstateCheckListType onClick={() => selectType(index)}>
+          <img
+            src={realEstateCheckListTypeData.src}
+            alt={realEstateCheckListTypeData.title}
+            className="real-estate-checklist-type"
+          />
+          <div className="real-estate-checklist-title">
+            {realEstateCheckListTypeData.title}
+          </div>
+        </r.RealEstateCheckListType>
+      ))}
     </r.RealEstateCheckListTypesContainer>
   )
 }
