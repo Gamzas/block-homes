905ae5b9b573c8c783efa729280c2f30cb47e2b3
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-11 22:38:20 +0900

    ✨ feat : REAL_ESTATE_CHECK_LIST_DATA에서 type에 맞는 질문 렌더링 하도록 설정

--- frontend/src/components/RealEstateCheckListPage/RealEstateCheckList.tsx
+++ frontend/src/components/RealEstateCheckListPage/RealEstateCheckList.tsx
@@ -1,5 +1,25 @@
+import * as r from '@components/RealEstateCheckListPage/style/RealEstateCheckListStyle'
+import { useAtom } from 'jotai'
+import { selectedCheckListTypeIndex } from '@stores/atoms/checkListStore'
+import { REAL_ESTATE_CHECK_LIST_DATA } from '@constants/RealEstateCheckListData'
+
 const RealEstateCheckList = () => {
-  return <div></div>
+  const [selectedCheckListType] = useAtom(selectedCheckListTypeIndex)
+
+  return (
+    <r.RealEstateCheckListContainer>
+      {REAL_ESTATE_CHECK_LIST_DATA[selectedCheckListType].content.map(
+        (realEstateCheckListContent, index) => (
+          <r.RealEstateCheckListContentContainer key={index}>
+            <input className="checklist-button" type="checkbox" />
+            <label className="checklist-content">
+              {realEstateCheckListContent}
+            </label>
+          </r.RealEstateCheckListContentContainer>
+        ),
+      )}
+    </r.RealEstateCheckListContainer>
+  )
 }
 
 export default RealEstateCheckList
