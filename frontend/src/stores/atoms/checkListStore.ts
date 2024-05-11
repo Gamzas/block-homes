ff13d05c9f8259ef9aea315cff816ba60bc3b3c7
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-11 22:12:38 +0900

    ✨ feat : checkListState를 관리할 store 생성

--- frontend/src/stores/atoms/checkListStore.ts
+++ frontend/src/stores/atoms/checkListStore.ts
@@ -1,3 +1,14 @@
+import { REAL_ESTATE_CHECK_LIST_DATA } from '@/constants/RealEstateCheckListData'
 import { atom } from 'jotai'
 
 export const selectedCheckListTypeIndex = atom<number>(0)
+
+export const checkListState = atom(
+  REAL_ESTATE_CHECK_LIST_DATA.map(section => ({
+    ...section,
+    content: section.content.map(question => ({
+      question,
+      checked: false,
+    })),
+  })),
+)
