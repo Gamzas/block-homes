ae1f7534ce5841f86be2c7d3801a98d3c0cd67f1
천세진 <sejinnnnnn@naver.com>
2024-05-12 01:15:40 +0900

    ✨ feat : SwaggerConfig 수정 (Server 추가)

--- backend/tradings/src/main/java/com/blockhomes/tradings/config/SwaggerConfig.java
+++ backend/tradings/src/main/java/com/blockhomes/tradings/config/SwaggerConfig.java
@@ -1,32 +1,34 @@
 package com.blockhomes.tradings.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
-import io.swagger.v3.oas.annotations.servers.Servers;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@Servers(value = {
-    @Server(url = "https://block-homes.kr", description = "블록홈즈 배포 서버"),
-
-})
+@OpenAPIDefinition(
+    servers = {
+        @Server(url = "https://block-homes.kr", description = "블록홈즈 배포 서버"),
+        @Server(url = "http://localhost:8081", description = "Localhost")
+    },
+    info = @Info(
+        title = "블록홈즈 Tradings 서버",
+        description = "블록홈즈 매물 등록 및 거래 백엔드 서버입니다.",
+        version = "v1.0",
+        contact = @Contact(name = "Gamzas", email = "gamzas.ssafy@gmail.com")
+    )
+)
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
-            .components(new Components())
-            .info(apiInfo());
-    }
-
-    private Info apiInfo() {
-        return new Info()
-            .title("Block Homes Tradings Server")
-            .description("블록홈즈 매물 등록 및 거래 백엔드 서버입니다.")
-            .version("1.0.0");
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+            .group("controller")
+            .pathsToMatch("/api/**")
+            .build();
     }
 
 }
