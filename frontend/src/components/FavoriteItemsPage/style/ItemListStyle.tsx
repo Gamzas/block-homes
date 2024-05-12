49e082d35f6936f767aba032f41cd97bf0b62189
정유나 <jyn7077@naver.com>
2024-05-12 16:39:35 +0900

    🎨 style: 체크박스 레이아웃

--- frontend/src/components/FavoriteItemsPage/style/ItemListStyle.tsx
+++ frontend/src/components/FavoriteItemsPage/style/ItemListStyle.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const ItmeListWrapper = styled.div`
+export const ItemListWrapper = styled.div`
   width: 100%;
   height: fit-content;
   display: flex;
@@ -21,5 +21,25 @@ export const EditContainer = styled.div`
   text-align: end;
   color: #000;
   font-size: 0.875rem;
-  font-weight: 400;
+  font-weight: 500;
+  .disabled {
+    display: flex;
+    align-items: center;
+    justify-content: end;
+    div:first-child {
+      margin-right: 1rem; /* 첫 번째 div 요소에만 마진 적용 */
+    }
+  }
+  .abled {
+  }
+`
+
+export const selectedItmeContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+
+  input {
+    /* margin-left: 0.7rem; */
+  }
 `
