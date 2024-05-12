7c78d6950e2bdec4a3ef400bb5748e9b68e6ecaf
정유나 <jyn7077@naver.com>
2024-05-13 07:58:49 +0900

    🎨 style : 매물 리스트 레이아웃 설정

---
+++ frontend/src/components/MyEstatePage/style/MyEstateListStyle.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const MyEstateListWrapper = styled.div`
+  width: 100%;
+  border: 1px solid red;
+`
+
+export const MyEstateListContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
