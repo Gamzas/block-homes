56f5c4c6b201c1599667f0524d3ef3f7402c38e0
천세진 <sejinnnnnn@naver.com>
2024-05-12 12:57:44 +0900

    ✨ feat : Image, ItemImage, Likes 레포지토리 추가

---
+++ backend/tradings/src/main/java/com/blockhomes/tradings/repository/ItemImageRepository.java
@@ -0,0 +1,10 @@
+package com.blockhomes.tradings.repository;
+
+import com.blockhomes.tradings.entity.item.ItemImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemImageRepository extends JpaRepository<ItemImage, Integer> {
+
+
+
+}
