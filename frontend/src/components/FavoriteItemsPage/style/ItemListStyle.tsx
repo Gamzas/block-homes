1702d51b1bf51959220dcd570fe1e9fa387cea0a
정유나 <jyn7077@naver.com>
2024-05-12 13:47:42 +0900

    🎨 style : 찜한 매물 목록 레이아웃 설정

---
+++ frontend/src/components/FavoriteItemsPage/style/ItemListStyle.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export const ItmeListWrapper = styled.div`
+  width: 100%;
+  height: fit-content;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 1rem;
+`
+
+export const ItemContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const EditContainer = styled.div`
+  width: 90%;
+  text-align: end;
+  color: #000;
+  font-size: 0.875rem;
+  font-weight: 400;
+`
