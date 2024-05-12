849566577077f2da92b4edfbe8a073be36f2af6b
정유나 <jyn7077@naver.com>
2024-05-12 17:24:41 +0900

    ✨ feat : 더미데이터 상태 변경

--- frontend/src/components/FavoriteItemsPage/ItemList.tsx
+++ frontend/src/components/FavoriteItemsPage/ItemList.tsx
@@ -32,7 +32,7 @@ const FavoriteItems = [
 
 // TODO 삭제로직 수정
 const ItemList = () => {
-  const [editActive, setEditActive] = useState(true)
+  const [editActive, setEditActive] = useState(false)
   // const [selectedItems, setSelectedItems] = useState(new Set())
   // TODO 서버 연결 시 찜한 목록 아래 변수 사용하기!
   // const [favoriteItems, setFavoriteItems] = useState(FavoriteItems) // 상태에 초기 아이템 목록을 저장합니다.
