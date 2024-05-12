b825b842dd00864d2bc683eb19e96d92e4062983
정유나 <jyn7077@naver.com>
2024-05-13 07:58:41 +0900

    🎨 style : 매물 카드 레이아웃 설정

---
+++ frontend/src/components/MyEstatePage/style/MyEstateCardStyle.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const MyEstateCardContainer = styled.div`
+  width: 90%;
+  height: 4.1875rem;
+  border-radius: 1.3125rem;
+  border: 1px solid #808080;
+  margin-bottom: 1.5rem;
+`
