1a515cd0f5bfa886ad57aecff9333f5e70fce84b
정유나 <jyn7077@naver.com>
2024-05-13 00:04:35 +0900

    ✨ feat : 매물 상세정보 컴포넌트화

---
+++ frontend/src/components/EstateDetailPage/DetailInfo.tsx
@@ -0,0 +1,55 @@
+import * as d from '@components/EstateDetailPage/style/DetailInfoStyle'
+import { useParams } from 'react-router-dom'
+import DetailTabMenu from './DetailTabMenu'
+import DetailFooter from './DetailFooter'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { selectedItemAtom } from '@/stores/atoms/EstateListStore'
+import { useAtom } from 'jotai'
+import ItemSafetyCard from './ItemSafetyCard'
+import RoomInfo from './RoomInfo'
+
+const DetailInfo = () => {
+  // DELETE 삭제하기
+  const [selectedItem] = useAtom(selectedItemAtom)
+
+  const { id } = useParams()
+  const [item, setItem] = useState(null)
+  const [loading, setLoading] = useState(true)
+  // DELETE 임시 , 삭제하기
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`/api/items/${id}`)
+        setItem(response.data)
+        // setLoading(false)
+      } catch (error) {
+        if (error.response && error.response.status === 404) {
+          alert('잘못된 접근입니다.')
+        } else {
+          alert('오류가 발생했습니다. 다시 시도해주세요')
+        }
+        // setLoading(false)
+      }
+    }
+    fetchData()
+  }, [id])
+
+  // if (loading) return <div>Loading...</div>
+  // if (error) return <div>Error: {error}</div>
+
+  return (
+    <d.DetailInfoWrapper>
+      {selectedItem !== 'not' && (
+        <>
+          <DetailTabMenu />
+          <ItemSafetyCard condition={selectedItem.condition} />
+          <RoomInfo info={selectedItem} />
+          <DetailFooter />
+        </>
+      )}
+    </d.DetailInfoWrapper>
+  )
+}
+
+export default DetailInfo
