d3cc621c656af5dae62a3fbadabf06d23c3ff11c
정유나 <jyn7077@naver.com>
2024-05-13 06:43:01 +0900

    ✨ feat	: 마이페이지 메뉴 navigate 설정

--- frontend/src/components/MyPage/MyPageMenu.tsx
+++ frontend/src/components/MyPage/MyPageMenu.tsx
@@ -1,14 +1,20 @@
 import * as m from '@components/MyPage/style/MyPageMenuStyle'
+import { useNavigate } from 'react-router-dom'
 
 const MyPageMenu = () => {
+  const navigate = useNavigate()
   const menuArr = ['내가 등록한 매물', '서비스 소개']
+  const address = ['/myestate', '/info-how-safe']
+  const goMenu = (index: number) => {
+    navigate(`${address[index]}`)
+  }
   return (
     <>
       <m.EmptyContainer />
       <m.MenuContainer>
         {menuArr.map((menu, index) => (
           <>
-            <div className="menu" key={index}>
+            <div className="menu" key={index} onClick={() => goMenu(index)}>
               {menu}
               <img
                 className="arrow-icon"
