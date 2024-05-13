43d35ea1027d07587c92a9dc7234360399e792b9
정유나 <jyn7077@naver.com>
2024-05-13 11:22:31 +0900

    ✨ feat	: 매물 등록 버튼 활성화

--- frontend/src/components/MyEstatePage/NoMyEstate.tsx
+++ frontend/src/components/MyEstatePage/NoMyEstate.tsx
@@ -1,13 +1,22 @@
+import { userTypeAtom } from '@/stores/atoms/userStore'
 import * as n from '@components/MyEstatePage/style/NoMyEstateStyle'
+import { useSetAtom } from 'jotai'
+import { useNavigate } from 'react-router-dom'
 
 const NoMyEstate = () => {
+  const navigate = useNavigate()
+  const setUserType = useSetAtom(userTypeAtom)
+  const goRegister = () => {
+    setUserType({ type: 1 })
+    navigate('/')
+  }
   return (
     <n.NoMyItemsContainer>
       <n.GuideContainer>
         <img className="house" src="/image/image_oneroom.png" alt="집ㅋㅎ" />
         <div className="guide-txt">등록한 매물이 없습니다</div>
       </n.GuideContainer>
-      <n.RegisterBtnContainer>
+      <n.RegisterBtnContainer onClick={goRegister}>
         <img
           className="btn-img"
           src="/image/image_VC_certification.png"
