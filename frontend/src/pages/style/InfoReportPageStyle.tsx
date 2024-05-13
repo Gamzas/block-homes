c2ed37f830f925706d2a6bdee3c8ee45fa1c6868
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-13 11:59:10 +0900

    🎨 style : InfoReportPage style 적용

---
+++ frontend/src/pages/style/InfoReportPageStyle.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const InfoReportContainer = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background: #f3f0f7;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
