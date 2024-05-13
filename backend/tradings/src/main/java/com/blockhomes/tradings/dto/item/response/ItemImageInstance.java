83ca2ab17b290819132cf83b5ad6a479c0335914
천세진 <sejinnnnnn@naver.com>
2024-05-13 10:55:21 +0900

    ✨ feat : 매물 상세보기 응답 및 이미지 DTO 추가

---
+++ backend/tradings/src/main/java/com/blockhomes/tradings/dto/item/response/ItemImageInstance.java
@@ -0,0 +1,15 @@
+package com.blockhomes.tradings.dto.item.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemImageInstance {
+
+    private String imageUrl;
+    private Integer itemImageCategory;
+
+}
