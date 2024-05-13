f045302e0fb0209317425693766844f8848ce765
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-13 23:08:58 +0900

    ✨ feat : TRANSACTION_PROCESS_STEP_DATA를 map으로 렌더링

--- frontend/src/components/InfoPage/InfoProcessContent.tsx
+++ frontend/src/components/InfoPage/InfoProcessContent.tsx
@@ -1,11 +1,22 @@
-import InfoProcessMap from '@components/InfoPage/InfoProcessMap'
 import InfoSelectedProcessComponent from '@components/InfoPage/InfoSelectedProcessComponent'
+import { useState } from 'react'
+import { TRANSACTION_PROCESS_STEP_DATA } from '@constants/TransactionProcessData'
 
 const InfoProcessContent = () => {
+  const [selectedProcessIndex, setSelectedProcessIndex] = useState(0)
+
   return (
     <div>
-      <InfoSelectedProcessComponent />
-      <InfoProcessMap />
+      <InfoSelectedProcessComponent
+        selectedProcessIndex={selectedProcessIndex}
+      />
+      {TRANSACTION_PROCESS_STEP_DATA.map((transactionStep, index) => (
+        <img
+          src={transactionStep.src}
+          alt={transactionStep.title}
+          key={index}
+        />
+      ))}
     </div>
   )
 }
