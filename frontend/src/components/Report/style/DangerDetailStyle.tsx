46b3cf4f889790ab21616dd791818cf83cacb8a2
오기선 <pakox123@gmail.com>
2024-05-13 11:04:47 +0900

    🎨 style : 위험요소 토글 메뉴 글씨 강조 추가

--- frontend/src/components/Report/style/DangerDetailStyle.tsx
+++ frontend/src/components/Report/style/DangerDetailStyle.tsx
@@ -1,51 +1,51 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const DangerDetailContainer = styled.div`
-    width: 20.1875rem;
-    border-radius: 0.43rem;
-    background: #f9f9f9;
-    box-shadow: 0px 5.972px 5.972px 0px rgba(0, 0, 0, 0.25);
-    padding: 1rem 0.5rem;
-    margin-bottom: 1rem;
-    font-weight: 500;
-    .title{
-        font-size: 1rem;
-        padding: 0 0.7rem;
-        width: 100%;
-        display: flex;
-        align-items: center;
-        color: #43C259;
-    }
+  width: 20.1875rem;
+  border-radius: 0.43rem;
+  background: #f9f9f9;
+  box-shadow: 0px 5.972px 5.972px 0px rgba(0, 0, 0, 0.25);
+  padding: 1rem 0.5rem;
+  margin-bottom: 1rem;
+  font-weight: 500;
+  .title {
+    font-size: 1rem;
+    padding: 0 0.7rem;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    color: #43c259;
+  }
 
-    .title.danger {
-    color: #FA5655; 
-    }
+  .title.danger {
+    color: #fa5655;
+  }
 
-    .title-box{
-        width: 18rem;
-    }
+  .title-box {
+    width: 18rem;
+  }
 
-    .rotate-arrow {
+  .rotate-arrow {
     transform: rotateX(180deg);
     transition: transform 0.3s ease-in-out;
-    }
-
+  }
 `
 
-
 interface ToggleMessageProps {
-    isVisible: boolean;
-  }
-  
+  isVisible: boolean
+}
+
 export const ToggleMessage = styled.div<ToggleMessageProps>`
-  opacity: ${props => props.isVisible ? '0' : '1'};
-  max-height: ${props => props.isVisible ? '0' : '10rem'};
+  opacity: ${props => (props.isVisible ? '0' : '1')};
+  max-height: ${props => (props.isVisible ? '0' : '10rem')};
   overflow: hidden;
-  transition: opacity 0.5s ease-in-out, max-height 0.5s ease-in-out;
+  transition:
+    opacity 0.5s ease-in-out,
+    max-height 0.5s ease-in-out;
   color: #808080;
-  font-weight: 400;
+  font-weight: 600;
   font-size: 0.85rem;
   padding: 0 2rem;
-  padding-top: ${props => props.isVisible ? '0' : '1rem'};
+  padding-top: ${props => (props.isVisible ? '0' : '1rem')};
   line-height: 1.4rem;
-`;
+`
