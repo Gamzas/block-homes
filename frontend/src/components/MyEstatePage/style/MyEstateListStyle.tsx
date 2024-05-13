ac3aec9cd3f3cf9ccd104d150fd97405bcdb4708
정유나 <jyn7077@naver.com>
2024-05-13 09:51:47 +0900

    🎨 style : 내가 등록한 매물 페이지 레이아웃 적용

--- frontend/src/components/MyEstatePage/style/MyEstateListStyle.tsx
+++ frontend/src/components/MyEstatePage/style/MyEstateListStyle.tsx
@@ -2,7 +2,19 @@ import styled from 'styled-components'
 
 export const MyEstateListWrapper = styled.div`
   width: 100%;
-  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 1rem;
+`
+export const EditContainer = styled.div`
+  width: 90%;
+  display: flex;
+  justify-content: end;
+  color: #000;
+  font-size: 1rem;
+  font-weight: 500;
+  text-decoration-line: underline;
 `
 
 export const MyEstateListContainer = styled.div`
@@ -11,4 +23,5 @@ export const MyEstateListContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  margin-top: 1rem;
 `
