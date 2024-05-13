c2f96245927a805e5d789c2f3ba5977e3a20a4d7
천세진 <sejinnnnnn@naver.com>
2024-05-13 10:56:17 +0900

    ✨ feat : Repository 쿼리 메서드 추가

--- backend/tradings/src/main/java/com/blockhomes/tradings/repository/item/ItemAdditionalOptionRepository.java
+++ backend/tradings/src/main/java/com/blockhomes/tradings/repository/item/ItemAdditionalOptionRepository.java
@@ -1,10 +1,13 @@
 package com.blockhomes.tradings.repository.item;
 
+import com.blockhomes.tradings.entity.item.Item;
 import com.blockhomes.tradings.entity.item.ItemAdditionalOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemAdditionalOptionRepository extends JpaRepository<ItemAdditionalOption, Integer> {
 
-
+    List<ItemAdditionalOption> getItemAdditionalOptionsByItem(Item item);
 
 }
