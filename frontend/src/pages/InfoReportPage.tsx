87dea3fd68f3ece06c936e54a9413bcda1619561
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-13 21:45:51 +0900

    🐛 fix : 이미지 호출 경로 수정

--- frontend/src/pages/InfoReportPage.tsx
+++ frontend/src/pages/InfoReportPage.tsx
@@ -5,7 +5,7 @@ import InfoReportList from '@components/InfoPage/InfoReportList'
 
 const InfoReportPage = () => {
   const infoHeaderProps = {
-    backgroundImageSrc: '/public/image/image_info_report.png',
+    backgroundImageSrc: '/image/image_info_report.png',
     questionTitle:
       '<span class="highlight-text"> 매물 레포트</span>에는 어떤 내용이 <br/>  담겨있을까요?',
   }
