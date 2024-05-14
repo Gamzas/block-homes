c7f3566a65bc49daef769d375f0a953397c05982
정유나 <jyn7077@naver.com>
2024-05-14 15:24:51 +0900

    🎨 style : 동네 선택 모달 스타일

--- frontend/src/components/EstateList/styles/LocationListModalStyle.tsx
+++ frontend/src/components/EstateList/styles/LocationListModalStyle.tsx
@@ -3,54 +3,51 @@ import styled from 'styled-components'
 export const ModalWrapper = styled.div`
   width: 100%;
   max-width: 390px;
-  min-height: 80vh;
+  min-height: 90vh;
   position: absolute;
   top: 50px;
   left: 0;
   background-color: #b5b4b49f;
-  /* z-index: 100; */
   /* border: 2px solid red; */
   display: flex;
   justify-content: center;
   align-items: center;
+  padding-bottom: 1rem;
 `
 export const ModalListContainer = styled.div`
   position: absolute;
-  top: 1rem;
-  /* z-index: 1000; */
+  /* border: 2px solid black; */
+  /* top: 1rem; */
   width: 80%;
-  height: 100%;
+  max-height: 60%;
+  height: fit-content;
   border-radius: 0.82531rem;
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
   background: #f3f0f7;
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 2rem 0 5rem 0;
+  padding: 0rem 0 2rem 0;
   overflow-y: scroll;
+  margin-bottom: rem;
+
   /* 스크롤바 숨기기를 위한 스타일 */
   ::-webkit-scrollbar {
     display: none; // 웹킷(Chrome, Safari 등) 브라우저에서 스크롤바 숨김
   }
-
   -ms-overflow-style: none; // IE, Edge에서 스크롤바 숨김
   scrollbar-width: none; // Firefox에서 스크롤바 숨김
 `
 
-export const ModalListBox = styled.div`
-  width: 80%;
-  height: 3rem;
+export const LocationTitleContainer = styled.div`
+  position: sticky;
+  top: 0;
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-top: 1rem;
-  border-radius: 0.9375rem;
-  background: #fff;
-  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-`
-export const LocationTitleContainer = styled.div`
-  display: flex;
-  align-items: center;
+  background: #f3f0f7;
+  padding: 1rem 0 1rem 0;
+  width: 100%;
   .title-img {
     width: 4rem;
     height: 4rem;
@@ -62,17 +59,23 @@ export const LocationTitleContainer = styled.div`
     font-weight: 500;
   }
 `
-
-export const CloseBtnContainer = styled.div`
-  position: absolute;
-  bottom: 0rem;
-  width: 3rem;
-  height: 2rem;
-  border-radius: 0.5rem;
-  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-  background: #fff;
+// 주소 선택 창
+export const ModalListBox = styled.div`
+  width: 80%;
+  height: 3rem;
   display: flex;
   align-items: center;
   justify-content: center;
-  font-size: 0.8rem;
+  margin-top: 1rem;
+  border-radius: 0.9375rem;
+  background: #fff;
+  box-shadow: 0px 2px 2px 0px rgba(0, 0, 0, 0.25);
+  padding: 1rem 0;
+`
+
+export const CloseBtnContainer = styled.div`
+  position: absolute;
+  top: 0.5rem;
+  right: 0.5rem;
+  width: 1.5rem;
 `
