0b594abb933a218a2a2bda49407fd3915f1a7695
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-14 17:27:47 +0900

    🐛 fix : 이미지 경로 수정

--- frontend/src/pages/InfoProcessPage.tsx
+++ frontend/src/pages/InfoProcessPage.tsx
@@ -5,7 +5,7 @@ import InfoProcessContent from '@components/InfoPage/InfoProcessContent'
 
 const InfoProcessPage = () => {
   const infoHeaderProps = {
-    backgroundImageSrc: '/public/image/image_info_certification.png',
+    backgroundImageSrc: '/image/image_info_certification.png',
     questionTitle:
       '블록홈즈에서는 <span class="highlight-text">어떤 과정</span>을 거쳐 집을 거래 할까요?',
   }
