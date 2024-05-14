cc336cff2ab902e4192c1c8c6f3c9a5fa22b54d7
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-14 09:30:37 +0900

    ✨ feat : index를 변경하는 setSelectedProcessIndex 함수 props로 전달받아 작동하도록 기능 추가

--- frontend/src/components/InfoPage/InfoProcessSelectedStep.tsx
+++ frontend/src/components/InfoPage/InfoProcessSelectedStep.tsx
@@ -1,20 +1,41 @@
 import * as i from '@components/InfoPage/style/InfoProcessSelectedStepStyle'
 import { TRANSACTION_PROCESS_STEP_DATA } from '@constants/TransactionProcessData'
 
-const InfoProcessSelectedStep = ({ selectedProcessIndex }) => {
+const InfoProcessSelectedStep = ({
+  selectedProcessIndex,
+  setSelectedProcessIndex,
+}) => {
   const selectedStepData = TRANSACTION_PROCESS_STEP_DATA[selectedProcessIndex]
 
-  const createMarkup = (htmlString: string) => {
+  const handleNext = () => {
+    if (selectedProcessIndex < TRANSACTION_PROCESS_STEP_DATA.length - 1) {
+      setSelectedProcessIndex(selectedProcessIndex + 1)
+    }
+  }
+
+  const handlePrev = () => {
+    if (selectedProcessIndex > 0) {
+      setSelectedProcessIndex(selectedProcessIndex - 1)
+    }
+  }
+
+  const createMarkup = htmlString => {
     return { __html: htmlString }
   }
 
   return (
     <i.InfoProcessSelectedStepContainer>
-      <img className="left-arrow" src="/icon/icon_puple_arrow.png" alt="왼쪽" />
+      <img
+        className="left-arrow"
+        src="/icon/icon_puple_arrow.png"
+        alt="왼쪽"
+        onClick={handlePrev}
+      />
       <i.InfoProcessSelectedStepContent>
         <div className="icon-container">
           <img src={selectedStepData.src} alt={selectedStepData.title} />
         </div>
+
         <div
           className="detail-container"
           dangerouslySetInnerHTML={createMarkup(selectedStepData.detail)}
@@ -24,6 +45,7 @@ const InfoProcessSelectedStep = ({ selectedProcessIndex }) => {
         className="right-arrow"
         src="/icon/icon_puple_arrow.png"
         alt="오른쪽"
+        onClick={handleNext}
       />
     </i.InfoProcessSelectedStepContainer>
   )
