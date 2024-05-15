bc64e76131b012b4470dc84dcabe9c1a976941b6
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-15 16:43:01 +0900

    🎨 style : 렌더링할 데이터가 없을 경우 보여지는 컴포넌트 NoItem style 적용

--- frontend/src/components/FavoriteItemsPage/style/NoItemsStyle.tsx
+++ frontend/src/components/FavoriteItemsPage/style/NoItemsStyle.tsx
@@ -1,40 +1,58 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
+export const NoItemContainer = styled.div`
   width: 100%;
-  min-height: 100%;
+  height: 100%;
+  padding: 0 5%;
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin-top: 10rem;
+  justify-content: center;
+  background-color: #f3f0f7;
 `
-export const ContentContainer = styled.div`
-  width: 50%;
-  height: fit-content;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  .txt {
-    margin-top: 2rem;
-    color: #000;
-    text-align: center;
-    font-size: 0.9375rem;
-    font-weight: 500;
+
+export const ImageContainer = styled.div`
+  width: 15rem;
+  position: absolute;
+  top: 11.5rem;
+
+  img {
+    width: 100%;
   }
 `
+
+export const ContentContainer = styled.div`
+  width: 85%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  border-radius: 1rem;
+  background-color: #d2c5f1;
+  padding: 1rem;
+
+  .alarm-text {
+    width: 100%;
+    text-align: center;
+    font-size: 1.3rem;
+    font-weight: 600;
+    margin: 1rem 0;
+  }
+`
+
 export const BtnContainer = styled.div`
-  width: 9.8125rem;
-  height: 2.25rem;
+  width: 10rem;
+  height: 3rem;
   border-radius: 0.4375rem;
   background: #845bd3;
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-top: 3rem;
+
   .title {
     color: #fff;
     text-align: center;
-    font-size: 0.8125rem;
+    font-size: 1rem;
     font-weight: 500;
   }
 `
