7f8b4bcecdb66067260672ae8dceb82b82d01d83
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-15 23:52:29 +0900

    ✨ feat : chatList Dummy data 삭제

--- frontend/src/components/ChatListPage/ChatList.tsx
+++ frontend/src/components/ChatListPage/ChatList.tsx
@@ -29,38 +29,15 @@ const ChatList = () => {
     queryFn: () => fetchChatRooms(chatRoomRequestData),
   })
 
-  const chatRoomDatas = [
-    // {
-    //   representativeImage:
-    //     'https://i0.wp.com/www.gangnamapt.com/wp-content/uploads/2023/01/20230105_180953_HDR.jpg?resize=480%2C360',
-    //   address: '광주광역시 광산구 남동길',
-    //   transactionType: 1,
-    //   price: 300000000,
-    //   lastChat: '집 상태 좋아요',
-    //   dangerType: 'good',
-    //   chatRoomNum: 1,
-    // },
-    // {
-    //   representativeImage:
-    //     'https://i0.wp.com/www.gangnamapt.com/wp-content/uploads/2023/01/20230105_180953_HDR.jpg?resize=480%2C360',
-    //   address: '광주광역시 광산구 남동길',
-    //   transactionType: 1,
-    //   price: 300000000,
-    //   lastChat: '집 상태 좋아요',
-    //   dangerType: 'normal',
-    //   chatRoomNum: 2,
-    // },
-  ]
-
   return (
     <c.ChatListContainer>
-      {chatRoomDatas && chatRoomDatas.length === 0 ? (
+      {data && data.length === 0 ? (
         <NoItems
           src={'image/image_sad_pig.png'}
           alarmText={'현재 진행중인 채팅방이 없어요.'}
         />
       ) : (
-        chatRoomDatas.map(chatData => <ChatPreviewComponent {...chatData} />)
+        data.map(chatData => <ChatPreviewComponent {...chatData} />)
       )}
     </c.ChatListContainer>
   )
