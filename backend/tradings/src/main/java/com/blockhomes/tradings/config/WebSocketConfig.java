b05f198e982ff23f871c4e47995e012a4efaf795
천세진 <sejinnnnnn@naver.com>
2024-05-15 18:39:45 +0900

    ✨ feat : WebSocket, RabbitMQ (메세지 브로커) 설정파일 추가

---
+++ backend/tradings/src/main/java/com/blockhomes/tradings/config/WebSocketConfig.java
@@ -0,0 +1,53 @@
+package com.blockhomes.tradings.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Slf4j
+@Configuration
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Value("${spring.rabbitmq.username}")
+    private String userName;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Value("${spring.rabbitmq.port}")
+    private Integer port;
+
+    @Value("${spring.rabbitmq.virtual-host}")
+    private String virtualHost;
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws/chat").setAllowedOrigins("*");
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setPathMatcher(new AntPathMatcher(".")); // Url을 슬래시에서 .으로 (RabbitMQ 컨벤션)
+        registry.setApplicationDestinationPrefixes("/pub"); // @MessageMapping 메서드로 라우팅
+        registry.enableStompBrokerRelay("/queue", "/topic", "/exchange", "/amq/queue")
+            .setRelayHost(host)
+            .setRelayPort(port)
+            .setVirtualHost(virtualHost)
+            .setSystemLogin(userName)
+            .setSystemPasscode(password)
+            .setClientLogin(userName)
+            .setClientPasscode(password);
+    }
+
+}
