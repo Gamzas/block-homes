5d16894e4a57747c91447393d83e3bdd8d9571fa
정유나 <jyn7077@naver.com>
2024-05-15 17:32:35 +0900

    🐛 fix : credential 수정

--- frontend/src/hooks/requestMethods.tsx
+++ frontend/src/hooks/requestMethods.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { API_BASE } from '@constants/api'
 
-axios.defaults.withCredentials = true
+axios.defaults.withCredentials = false
 
 export const publicRequest = axios.create({
   baseURL: API_BASE,
