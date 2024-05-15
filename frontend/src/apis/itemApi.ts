b7cffb61f8e501836b42ac232c229b7712c013a9
정유나 <jyn7077@naver.com>
2024-05-15 17:25:46 +0900

    ✨ feat : 찜한 매물 삭제 등록 코드 추가

--- frontend/src/apis/itemApi.ts
+++ frontend/src/apis/itemApi.ts
@@ -1,6 +1,7 @@
 import { useMutation } from '@tanstack/react-query'
 import axios from 'axios'
 import { API_FAVORITE_ITEM, API_ITEM } from '@constants/api'
+import { publicRequest } from '@/hooks/requestMethods'
 
 export const usePostItemRegister = () => {
   return useMutation({
@@ -9,11 +10,40 @@ export const usePostItemRegister = () => {
 }
 
 export const getFavoriteItem = async (address: string) => {
-  const response = await axios.get(API_FAVORITE_ITEM, {
+  const response = await publicRequest.get(API_FAVORITE_ITEM, {
     params: {
-      userAddress: address,
+      userAddress: '0xe0fa92495dfa8e7188e72f593ef2f2988b6b5a87',
       localDateTime: 'string',
     },
   })
   return response.data
 }
+
+
+export const postFavoritItem = async (
+  itemNo: number,
+  walletAddress: string,
+) => {
+  await publicRequest.post(API_FAVORITE_ITEM, {
+    data: {
+      itemNo,
+      walletAddress,
+    },
+  })
+}
+
+
+
+export const deleteFavoritItem = async (
+  itemNo: number,
+  walletAddress: string,
+) => {
+  await publicRequest.delete(API_FAVORITE_ITEM, {
+    data: {
+      itemNo,
+      walletAddress,
+    },
+  })
+}
+
+
