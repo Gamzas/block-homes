0b1a19862c40fd6df7d5386fc4dc4f4fa438a5f7
rkdtks0816 <rkdtks0816@naver.com>
2024-05-15 23:15:20 +0900

    ✨ feat : 부동산 DID 조회 abi 생성

--- frontend/src/components/IntroPage/Intro.tsx
+++ frontend/src/components/IntroPage/Intro.tsx
@@ -5,6 +5,8 @@ import { useGetWallet } from '@apis/walletApi'
 import { useState } from 'react'
 import { ethers } from 'ethers'
 import { useClaimCredential } from '@/abi/ownershipVCRegistry/claimCredential'
+import { BLOCK_CHAIN_ENDPOINT } from '@constants/abi/abi'
+import { MOIS_PRIVATE_KEY } from '@constants/abi/MOISPrivateKey'
 
 const Intro = () => {
   const currentUser = useAtomValue(userAtom)
@@ -29,8 +31,10 @@ const Intro = () => {
 
   const handleClaimCredentialButtonClick = async () => {
     if (userWallet) {
-      const subject = '{holder : did:klay:0x..., property : did:klay:0x...}'
-      const value = 'did:klay:0x...'
+      // const provider = new ethers.providers.JsonRpcProvider(BLOCK_CHAIN_ENDPOINT)
+      // const MOISWallet = new ethers.Wallet(MOIS_PRIVATE_KEY, provider)
+      const subject = 'did:klay:0x15b84A76cd54E0D086FE0E40Cb3eAc3dB3e9a593'
+      const value = 'did:klay:0x12345'
       const message = ethers.utils.solidityKeccak256(
         ['string', 'uint256', 'string'],
         [subject, Math.floor(Date.now() / 1000), value],
