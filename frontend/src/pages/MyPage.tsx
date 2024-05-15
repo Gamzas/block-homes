e7baac239b031eee1e7dde07d0de006a59d84731
정유나 <jyn7077@naver.com>
2024-05-15 18:18:17 +0900

    ✨ feat	: 마이페이지 접속 시 로그인 여부 확인 후 리다이렉션

--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -1,32 +1,22 @@
 import Footer from '@/common/Footer'
-import Login from '@/components/MyPage/Login'
 import Logout from '@/components/MyPage/Logout'
 import MemberCard from '@/components/MyPage/MemberCard'
 import MyPageMenu from '@/components/MyPage/MyPageMenu'
 import MyVCCard from '@/components/MyPage/MyVCCard'
 import MyWallet from '@/components/MyPage/MyWallet'
 import * as m from '@pages/style/MyPageStyle'
-import { memberAtom } from '@/stores/atoms/userStore'
-import { useAtom } from 'jotai'
+import useLoginStatus from '@/hooks/useLoginStatus'
 
 const MyPage = () => {
-  // DELETE 임시
-  const [member] = useAtom(memberAtom)
-
+  useLoginStatus()
   return (
     <m.Container>
-      {member ? (
-        <>
-          <MemberCard />
-          <MyWallet />
-          <MyVCCard />
-          <MyPageMenu />
-          <Logout />
-          <Footer />
-        </>
-      ) : (
-        <Login />
-      )}
+      <MemberCard />
+      <MyWallet />
+      <MyVCCard />
+      <MyPageMenu />
+      <Logout />
+      <Footer />
     </m.Container>
   )
 }
