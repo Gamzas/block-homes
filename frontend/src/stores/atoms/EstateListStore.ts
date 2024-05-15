a37e0c17a9dbeff7d2283b025186f13f7142d2a7
정유나 <jyn7077@naver.com>
2024-05-15 21:44:33 +0900

    🌱 chore : 더미데이터 변경

--- frontend/src/stores/atoms/EstateListStore.ts
+++ frontend/src/stores/atoms/EstateListStore.ts
@@ -1,6 +1,7 @@
 import { atomWithStorage } from 'jotai/utils'
 import { atom } from 'jotai'
 import { CoordType, EstateItem } from '@/types/components/estateListType'
+import { EsetateItemResType, EstateItemListType } from '@/types/api/itemType'
 
 export const filterAtom = atom<boolean>(false) // false 필터 닫힘, true 필터 열림
 
@@ -19,84 +20,33 @@ export const matchAtom = atom<boolean>(false)
 
 const EstateList = [
   {
-    condition: 'normal',
-    address: '남동길 30번길 13 3층',
-    infos: ['남향', '교육시설', '역세권'],
-    leaseType: '전세',
-    price: '3억',
-    roomSize: '37',
-    roomCount: '투룸',
-    createDate: '2024년 3월 21일',
-    latitude: 35.2097245,
-    longitude: 126.7805472,
-  },
-  {
-    condition: 'bad',
-    address: '광주광역시 광산구 오선동',
-    infos: ['남향', '교육시설', '역세권'],
-    leaseType: '매매',
-    price: '3억',
-    roomSize: '37',
-    roomCount: '투룸',
-    createDate: '2024년 3월 21일',
-    latitude: 35.205615,
-    longitude: 126.812546,
-  },
-  {
-    condition: 'good',
-    address: '광주광역시 광산구 장덕동',
-    infos: ['남향', '교육시설', '역세권'],
-    leaseType: '전세',
-    price: '3억',
-    roomSize: '37',
-    roomCount: '투룸',
-    createDate: '2024년 3월 21일',
-    latitude: 35.204315,
-    longitude: 126.812546,
-  },
-  {
-    condition: 'normal',
-    address: '남동길 30번길 13 3층',
-    infos: ['남향', '교육시설', '역세권'],
-    leaseType: '전세',
-    price: '3억',
-    roomSize: '37',
-    roomCount: '투룸',
-    createDate: '2024년 3월 21일',
-    latitude: 35.204615,
-    longitude: 126.813546,
-  },
-  {
-    condition: 'bad',
-    address: '광주광역시 광산구 오선동',
-    infos: ['남향', '교육시설', '역세권'],
-    leaseType: '매매',
-    price: '3억',
-    roomSize: '37',
-    roomCount: '투룸',
-    createDate: '2024년 3월 21일',
-    latitude: 35.202615,
-    longitude: 126.814546,
-  },
-  {
-    condition: 'good',
-    address: '광주광역시 광산구 장덕동',
-    infos: ['남향', '교육시설', '역세권'],
-    leaseType: '월세',
-    price: '3억',
-    roomSize: '37',
-    roomCount: '투룸',
-    createDate: '2024년 3월 21일',
-    latitude: 35.204615,
-    longitude: 126.815546,
+    itemList: [
+      {
+        itemNo: 3,
+        realEstateDID: 'did:klay:0x00',
+        roadNameAddress: '광주광역시 광산구 장덕동 0000',
+        transactionType: 1,
+        realEstateType: 1,
+        reportRank: 1,
+        transactionStatus: 1,
+        area: 33,
+        pyeong: 9.9825,
+        price: 1,
+        monthlyPrice: 1,
+        administrationCost: 1,
+        contractMonths: 1,
+        latitude: 0,
+        longitude: 0,
+      },
+    ],
+    count: 1,
   },
 ]
-
 // 전체 부동산 매물
-export const estateItemListAtom = atom<EstateItem[] | null>([])
+export const estateItemListAtom = atom<EsetateItemResType[] | null>(EstateList)
 
 // 선택된 부동산 매물
-export const selectedItemAtom = atomWithStorage<EstateItem | 'not'>(
+export const selectedItemAtom = atomWithStorage<EstateItemListType | 'not'>(
   'selectedItem',
   'not',
 )
