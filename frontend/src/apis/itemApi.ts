ec658a5b7de1074df2dc4041e6173cdd0e7dcd9a
정유나 <jyn7077@naver.com>
2024-05-15 22:16:38 +0900

    ✨ feat : 매물 조회 위한 api 추가

--- frontend/src/apis/itemApi.ts
+++ frontend/src/apis/itemApi.ts
@@ -1,11 +1,13 @@
 import { useMutation } from '@tanstack/react-query'
 import axios from 'axios'
 import {
+  API_ESTATE_ITEM,
   API_FAVORITE_ITEM,
   API_HEADERS_FORM_DATA,
   API_ITEM,
 } from '@constants/api'
 import { publicRequest } from '@/hooks/requestMethods'
+import { EstateItemReqType } from '@/types/api/itemType'
 
 export const usePostItemRegister = () => {
   return useMutation({
@@ -23,6 +25,14 @@ export const getFavoriteItem = async (address: string) => {
   })
   return response.data
 }
+
+// 등록 된 매물 조회,상세 조회
+
+export const getEstateItems = async (data: EstateItemReqType) => {
+  const res = await publicRequest.get(`${API_ESTATE_ITEM}`, { data })
+  return res.data
+}
+
 
 export const postFavoriteItem = async (
   itemNo: number,
