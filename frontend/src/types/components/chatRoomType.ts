33d474bed75dae5e45f30a633246486a7c528f66
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-15 14:42:25 +0900

    🚚 rename ; chatRoomType으로 파일 명 직관적으로 변경
    - chatType -> chatRoomType

---
+++ frontend/src/types/components/chatRoomType.ts
@@ -0,0 +1,9 @@
+export interface ChatComponentProps {
+  representativeImage: string
+  address: string
+  transactionType: number
+  price: number
+  lastChat: string
+  dangerType: string
+  chatRoomNum: number
+}
