7715da5c1799ff53ce912bcf2b18b819ebefc3fd
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-15 19:16:15 +0900

    🐛 fix : props로 이미지 경로를 전달 받도록 수정

--- frontend/src/common/NoItems.tsx
+++ frontend/src/common/NoItems.tsx
@@ -1,23 +1,25 @@
 import * as n from '@common/style/NoItemsStyle'
 import { useNavigate } from 'react-router-dom'
 
-const NoItems = ({ alarmText }) => {
+const NoItems = ({ src, alarmText }) => {
   const navigate = useNavigate()
-
+  console.log(src === '/image/image_sad_pig.png')
   const goEstate = () => {
     navigate('/estate')
   }
 
   return (
     <n.NoItemContainer>
-      <n.ImageContainer>
-        <img src="/image/image_sad_pig.png" alt="슬픈 돼지" />
+      <n.ImageContainer $isSad={src === 'image/image_sad_pig.png'}>
+        <img src={src} alt="슬픈 돼지" />
       </n.ImageContainer>
       <n.ContentContainer>
         <div className="alarm-text">{alarmText}</div>
-        <n.BtnContainer onClick={goEstate}>
-          <div className="title">매물 구경하러 가기</div>
-        </n.BtnContainer>
+        {src !== 'image/image_warning_pig.png' && (
+          <n.BtnContainer onClick={goEstate}>
+            <div className="title">매물 구경하러 가기</div>
+          </n.BtnContainer>
+        )}
       </n.ContentContainer>
     </n.NoItemContainer>
   )
