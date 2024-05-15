a8de22002a7b78b7a6f48adecfa0f6ce82b8f138
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-15 19:15:05 +0900

    🎨 style : padding 삭제

--- frontend/src/components/EstateList/styles/EstateItemListStyle.tsx
+++ frontend/src/components/EstateList/styles/EstateItemListStyle.tsx
@@ -15,8 +15,6 @@ export const EstateItemListContainer = styled.div`
   flex-direction: column;
   align-items: center;
   position: relative;
-  padding-top: 3rem;
-  padding-bottom: 1rem;
 `
 
 export const EstateFilterContainer = styled.div`
@@ -28,8 +26,7 @@ export const EstateFilterContainer = styled.div`
   background: rgba(130, 130, 130, 0.7);
 `
 
-
-// DELETE 컨테이너 삭제 
+// DELETE 컨테이너 삭제
 
 export const EstateMapContainer = styled.div`
   width: 100%;
