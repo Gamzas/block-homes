5b09b04ba5cfb530fd104ed47ae774dd268929d2
오기선 <pakox123@gmail.com>
2024-05-16 09:00:40 +0900

    ✨ feat : 서명 후 상대방 지갑 돈 보내는 함수 적용

--- frontend/src/pages/SmartContractPage.tsx
+++ frontend/src/pages/SmartContractPage.tsx
@@ -1,3 +1,5 @@
+// SmartContractPage.tsx
+import React, { useEffect, useState } from 'react'
 import * as s from './style/SmartContract'
 import Header from '@/common/Header'
 import ContractPayment from '@/components/SmartContract/ContractPayment'
@@ -5,32 +7,45 @@ import ContractAgree from '@/components/SmartContract/ContractAgree'
 import ContractMain from '@/components/SmartContract/ContractMain'
 import ContractStart from '@/components/SmartContract/ContractStart'
 import { contractStepAtom, readContractStepAtom } from '@/stores/smartcontract'
-import { useAtom } from 'jotai'
+import { useAtom, useAtomValue } from 'jotai'
 import ContractComplete from '../components/SmartContract/ContractComplete'
 import WaveContainer from '@/common/WaveContainer'
-import { useEffect, useState } from 'react'
-import { Button, Modal } from '@mui/material'
+import { Button } from '@mui/material'
 import CustomModal from '@/common/CustomModal'
-import { deployContract } from '@/abi/userSmartContract/DeployLongTermRentContract'
+import {
+  deployContract,
+  getContractInstance,
+  payDeposit,
+} from '@/abi/userSmartContract/DeployLongTermRentContract'
 import { ethers } from 'ethers'
-
-// // 더미 서명 생성 함수
-// function generateDummySignature() {
-//   let signature =
-//     '0x' +
-//     [...Array(128)] // 64바이트(128자리 16진수)
-//       .map(() => Math.floor(Math.random() * 16).toString(16))
-//       .join('')
-//   return signature
-// }
-
-// function isValidHexString(str) {
-//   return /^0x[0-9A-Fa-f]{128}$/.test(str) // 128자리 16진수 문자열인지 확인
-// }
+import { userAtom } from '@stores/atoms/userStore'
+import { useGetWallet } from '@/apis/walletApi'
+import { BLOCK_CHAIN_ENDPOINT } from '@constants/abi/abi'
+import CustomPasswordModal from '@/components/SmartContract/CustomPasswordModal'
+import { Snackbar } from '@mui/material'
 
 const SmartContractPage = () => {
   const [step, setStep] = useAtom(contractStepAtom)
   const [open, setOpen] = useState(false)
+
+  // 비밀번호 오류 컴포넌트
+  const [snackbarOpen, setSnackbarOpen] = useState(false)
+  const [snackbarMessage, setSnackbarMessage] = useState('')
+
+  const [password, setPassword] = useState('')
+  // const [userWallet, setUserWallet] = useState(null)
+  const [passwordModalOpen, setPasswordModalOpen] = useState(false)
+
+  // 지갑 불러오기(wallet) 비밀번호 쳐서 여는 로직 필요~~
+  const currentUser = useAtomValue(userAtom)
+  const { data: getWalletData } = useGetWallet({
+    walletAddress: currentUser.walletAddress,
+  })
+  // 콘솔 확인창
+  console.log('getWalletData', getWalletData)
+  console.log('password', password)
+
+  // 계약서 주소
   const [deploymentInfo, setDeploymentInfo] = useState('')
 
   useEffect(() => {
@@ -40,37 +55,47 @@ const SmartContractPage = () => {
   const handleOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
   const dummyData = {
-    landlordDID: 'did:example:123456789abcdef',
-    tenantDID: 'did:example:fedcba987654321',
+    // 임대인 지갑주소
+    landlordDID: '0x6A81426736fa17f8d6e4daf982F69969c420347F',
+    // 임차인 지갑주소
+    tenantDID: '0x9F9CA159B609bc5d54674d857426e7d5186E513F',
     leasePeriod: 12,
-    deposit: ethers.utils.parseEther('1.0').toString(), // 1 ETH in wei
-    propertyDID: 'did:example:abcdef123456789',
+    deposit: ethers.utils.parseEther('1.0').toString(), // 1 ETH in wei // 보증금
+    propertyDID: 'did:example:abcdef123456789', // 부동산의 분산식 식별자
     contractDate: Math.floor(Date.now() / 1000), // Current Unix timestamp
     terms: ['No smoking', 'No pets'],
   }
-  const privateKey =
-    '0x094221b92b0197f6eae4952006599011951104c4f48ae7af301ef319b7109e73'
 
+  // // 여기 변경 필요
+  // const privateKey =
+  //   '0x094221b92b0197f6eae4952006599011951104c4f48ae7af301ef319b7109e73'
+
+  const handlePasswordConfirm = (password: string) => {
+    setPassword(password)
+    setPasswordModalOpen(false)
+  }
+  /// 거래시작 시 버튼 누르는 과정이 필요~~
   const handleDeploy = async () => {
-    if (!privateKey) {
-      alert('Please enter a private key.')
+    if (!getWalletData && !password) {
+      alert('getWalletData,password')
       return
     }
 
     try {
-      // 실제 스마트컨트랙트 배포하도록 인증 받은 지갑이나 암호를 제공해야함..!
-      // const landlordSignature = generateDummySignature()
-      // const tenantSignature = generateDummySignature()
-
-      // if (
-      //   !isValidHexString(landlordSignature) ||
-      //   !isValidHexString(tenantSignature)
-      // ) {
-      //   throw new Error('Invalid signature format.')
-      // }
+      // 1.암호화된 지갑 데이터 복원
+      const userWallet = await ethers.Wallet.fromEncryptedJson(
+        getWalletData.data.encPrivateKey,
+        password,
+      )
+      // 2. 이더리움 네트워크 연결
+      const provider = new ethers.providers.JsonRpcProvider(
+        BLOCK_CHAIN_ENDPOINT,
+      )
+      // 3. 지갑을provider에 연결
+      const signer = userWallet.connect(provider)
 
       const result = await deployContract(
-        privateKey,
+        signer,
         dummyData.landlordDID,
         dummyData.tenantDID,
         dummyData.leasePeriod,
@@ -79,13 +104,68 @@ const SmartContractPage = () => {
         dummyData.contractDate,
         dummyData.terms,
       )
+
+      console.log()
+      // 1차 완료계약서 주소 저장완료 전역 저장해서 관리하기
       setDeploymentInfo(result)
-      console.log('성공요', deploymentInfo)
+
+      console.log('성공요result', result)
+
+      console.log('성공요deploymentInfo', deploymentInfo)
+
+      setSnackbarMessage('계약서가 성공적으로 블록체인에 등록되었습니다.')
+      setSnackbarOpen(true)
     } catch (error) {
-      setDeploymentInfo(error.message)
-      console.log('실패요', deploymentInfo)
+      if (error.message.includes('invalid password')) {
+        setSnackbarMessage('잘못된 비밀번호입니다. 다시 시도하세요.')
+      } else {
+        setSnackbarMessage(`실패: ${error.message}`)
+        console.log('실패요', deploymentInfo)
+      }
+      setSnackbarOpen(true)
     }
   }
+
+  // 임차인이 보증금을 지불하는 함수
+  const handlePayDeposit = async () => {
+    // 지갑 데이터와 비밀번호가 있는지 확인
+    if (getWalletData && password) {
+      try {
+        // EncryptedJson을 사용하여 지갑을 복원
+        const tenantWallet = await ethers.Wallet.fromEncryptedJson(
+          getWalletData.data.encPrivateKey,
+          password,
+        )
+
+        // 이더리움 메인넷에 연결하기 위해 provider 생성
+        const provider = new ethers.providers.JsonRpcProvider(
+          BLOCK_CHAIN_ENDPOINT,
+        )
+
+        // 지갑을 provider에 연결하여 signer 생성
+        const tenantSigner = tenantWallet.connect(provider)
+
+        // 스마트 계약 인스턴스 생성
+        const contract = getContractInstance(deploymentInfo, tenantSigner)
+
+        // 보증금 지불 트랜잭션 생성
+        const tx = await contract.payDeposit({
+          value: ethers.utils.parseEther('1.0'), // dummyData.deposit을 사용하는 경우 이를 대체
+        })
+
+        // 트랜잭션이 완료될 때까지 대기
+        await tx.wait()
+        console.log('보증금 지불 성공:', tx)
+        setSnackbarMessage('보증금 지불 성공')
+        setSnackbarOpen(true)
+      } catch (error) {
+        console.error('보증금 지불 실패:', error)
+      }
+    } else {
+      alert('지갑 정보를 불러오거나 비밀번호를 입력하세요.')
+    }
+  }
+
   return (
     <s.ContractContainer>
       <WaveContainer></WaveContainer>
@@ -101,12 +181,28 @@ const SmartContractPage = () => {
       {step === 3 && <ContractPayment />}
       {step === 4 && <ContractComplete />}
       <Button onClick={handleOpen}></Button>
+
+      <Button onClick={() => setPasswordModalOpen(true)}>Enter Password</Button>
       <Button onClick={handleDeploy}>Deploy Contract</Button>
+      <Button onClick={handlePayDeposit}>Pay Deposit</Button>
       <CustomModal
         open={open}
         handleClose={handleClose}
         title="나가시겠습니까?"
         description="계약 중 나가면 처음부터 시작해야합니다."
+      />
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={6000}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        onClose={() => setSnackbarOpen(false)}
+        message={snackbarMessage}
+      />
+
+      <CustomPasswordModal
+        open={passwordModalOpen}
+        handleClose={() => setPasswordModalOpen(false)}
+        handleConfirm={handlePasswordConfirm}
       />
     </s.ContractContainer>
   )
