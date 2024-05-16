fc2b3d3010af9196191cd27f651084587c8a21fe
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-16 23:52:15 +0900

    🐛 fix : async await으로 변경

--- frontend/src/apis/chatApi.ts
+++ frontend/src/apis/chatApi.ts
@@ -12,15 +12,15 @@ export const fetchChatRooms = async (params: fetchChatRoomsRequestType) => {
 export const checkChatRoomExistence = async (
   params: chatRoomRequestDataType,
 ): Promise<ChatRoomCheckResponseType> => {
-  return publicRequest
-    .get(`/chatrooms/check`, { params })
-    .then(response => response)
-    .catch(error => {
-      if (error.response && error.response.status === 404) {
-        return { status: 404 }
-      }
-      throw error
-    })
+  try {
+    const response = await publicRequest.get(`/chatrooms/check`, { params })
+    return response
+  } catch (error) {
+    if (error.response && error.response.status === 404) {
+      return { status: 404 }
+    }
+    throw error
+  }
 }
 
 export const createChatRoom = async (
