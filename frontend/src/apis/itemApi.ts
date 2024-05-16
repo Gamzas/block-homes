248bd59ddcfe5f481e983cd58d284e5c8d7e4186
정유나 <jyn7077@naver.com>
2024-05-16 09:43:51 +0900

    ✨ feat : 매물 찜 및 찜 삭제 api 호출 코드 작성

--- frontend/src/apis/itemApi.ts
+++ frontend/src/apis/itemApi.ts
@@ -1,4 +1,4 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import {
   API_ESTATE_ITEM,
@@ -7,7 +7,11 @@ import {
   API_ITEM,
 } from '@constants/api'
 import { publicRequest } from '@/hooks/requestMethods'
-import { EstateItemReqType } from '@/types/api/itemType'
+import {
+  EstateItemReqType,
+  GetFavoritItemParamsType,
+  PostFavoriteDataType,
+} from '@/types/api/itemType'
 
 export const usePostItemRegister = () => {
   return useMutation({
@@ -16,6 +20,15 @@ export const usePostItemRegister = () => {
   })
 }
 
+// 등록 된 매물 조회,상세 조회
+export const getEstateItems = async (data: EstateItemReqType) => {
+  const res = await publicRequest.get(`${API_ESTATE_ITEM}`, { data })
+  return res.data
+}
+
+
+
+// 찜 목록 조회
 export const getFavoriteItem = async (address: string) => {
   const response = await publicRequest.get(API_FAVORITE_ITEM, {
     params: {
@@ -26,26 +39,27 @@ export const getFavoriteItem = async (address: string) => {
   return response.data
 }
 
-// 등록 된 매물 조회,상세 조회
+// 매물 찜 등록
+// export const postFavoriteItem = async (
+//   itemNo: number,
+//   walletAddress: string,
+// ) => {
+//   await publicRequest.post(API_FAVORITE_ITEM, {
+//     data: {
+//       itemNo,
+//       walletAddress,
+//     },
+//   })
+// }
 
-export const getEstateItems = async (data: EstateItemReqType) => {
-  const res = await publicRequest.get(`${API_ESTATE_ITEM}`, { data })
-  return res.data
-}
-
-
-export const postFavoriteItem = async (
-  itemNo: number,
-  walletAddress: string,
-) => {
-  await publicRequest.post(API_FAVORITE_ITEM, {
-    data: {
-      itemNo,
-      walletAddress,
-    },
+export const usePostFavoriteItem = () => {
+  return useMutation({
+    mutationFn: (data: PostFavoriteDataType) =>
+      publicRequest.post(API_FAVORITE_ITEM, data),
   })
 }
 
+// 찜 삭제
 export const deleteFavoriteItem = async (
   itemNo: number,
   walletAddress: string,
@@ -55,5 +69,12 @@ export const deleteFavoriteItem = async (
       itemNo,
       walletAddress,
     },
+  })
+}
+
+export const useDeleteFavoriteItem = () => {
+  return useMutation({
+    mutationFn: (data: PostFavoriteDataType) =>
+      publicRequest.delete(API_FAVORITE_ITEM, { data }),
   })
 }
