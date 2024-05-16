3a2cf7fe8fbe8c575fd5d91d20eae6b51ebf2d9a
오기선 <pakox123@gmail.com>
2024-05-16 09:17:57 +0900

    🐛 fix : 채팅룸 주석

--- frontend/src/pages/ChattingRoomPage.tsx
+++ frontend/src/pages/ChattingRoomPage.tsx
@@ -10,66 +10,66 @@ import { useQuery } from '@tanstack/react-query'
 import { fetchChatRoomDetail } from '@apis/chatApi'
 
 const ChattingRoomPage = () => {
-  const { chatNo } = useParams()
-  const [user] = useAtom(userAtom)
+  // const { chatNo } = useParams()
+  // const [user] = useAtom(userAtom)
 
-  const defaultMessage = {
-    chatNo: 0,
-    createdAt: '',
-    isRead: false,
-    message: '',
-    type: null,
-    userImage: null,
-    userName: user.name,
-    userWalletAddress: user.walletAddress,
-  }
+  // const defaultMessage = {
+  //   chatNo: 0,
+  //   createdAt: '',
+  //   isRead: false,
+  //   message: '',
+  //   type: null,
+  //   userImage: null,
+  //   userName: user.name,
+  //   userWalletAddress: user.walletAddress,
+  // }
 
-  const [messages, setMessages] = useState<MessageType[]>([])
-  const [newMessage, setNewMessage] = useState<MessageType>(defaultMessage)
-  const scrollRef = useRef<HTMLDivElement>(null)
+  // const [messages, setMessages] = useState<MessageType[]>([])
+  // const [newMessage, setNewMessage] = useState<MessageType>(defaultMessage)
+  // const scrollRef = useRef<HTMLDivElement>(null)
 
-  const scrollToBottom = () => {
-    if (scrollRef.current) {
-      scrollRef.current.scrollTop = scrollRef.current.scrollHeight
-    }
-  }
-  const { data, isLoading, refetch } = useQuery({
-    queryKey: ['fetchChatRoomDetail', chatRoomNo],
-    queryFn: () => fetchChatRoomDetail(Number(chatRoomNo)),
-  })
+  // const scrollToBottom = () => {
+  //   if (scrollRef.current) {
+  //     scrollRef.current.scrollTop = scrollRef.current.scrollHeight
+  //   }
+  // }
+  // const { data, isLoading, refetch } = useQuery({
+  //   queryKey: ['fetchChatRoomDetail', chatRoomNo],
+  //   queryFn: () => fetchChatRoomDetail(Number(chatRoomNo)),
+  // })
 
-
-  const sendTextMessage = () => {
-    if (newMessage.message.trim() !== '') {
-      client.current!.publish({
-        destination: `/sub/chat.talk.${chatRoomNo}`,
-        body: JSON.stringify({
-          chatRoomNo: chatRoomNo,
-          senderWalletAddress: user.walletAddress,
-          message: newMessage.message,
-          type: 2,
-        }),
-      })
-      setNewMessage(defaultMessage)
-      scrollToBottom()
-    }
-  }
+  // const sendTextMessage = () => {
+  //   if (newMessage.message.trim() !== '') {
+  //     client.current!.publish({
+  //       destination: `/sub/chat.talk.${chatRoomNo}`,
+  //       body: JSON.stringify({
+  //         chatRoomNo: chatRoomNo,
+  //         senderWalletAddress: user.walletAddress,
+  //         message: newMessage.message,
+  //         type: 2,
+  //       }),
+  //     })
+  //     setNewMessage(defaultMessage)
+  //     scrollToBottom()
+  //   }
+  // }
 
   return (
-    <c.ChattingRoomPageContainer>
-      <Header
-        title="채팅"
-        isSearch={false}
-        rightIconSrc="/icon/icon_safety_card_report.png"
-      />
-      <SendMessageInput
-        sendButtonClick={sendTextMessage}
-        message={newMessage.message}
-        onChange={e =>
-          setNewMessage(prev => ({ ...prev, message: e.target.value }))
-        }
-      />
-    </c.ChattingRoomPageContainer>
+    <></>
+    // <c.ChattingRoomPageContainer>
+    //   <Header
+    //     title="채팅"
+    //     isSearch={false}
+    //     rightIconSrc="/icon/icon_safety_card_report.png"
+    //   />
+    //   <SendMessageInput
+    //     sendButtonClick={sendTextMessage}
+    //     message={newMessage.message}
+    //     onChange={e =>
+    //       setNewMessage(prev => ({ ...prev, message: e.target.value }))
+    //     }
+    //   />
+    // </c.ChattingRoomPageContainer>
   )
 }
 
