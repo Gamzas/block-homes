67050bc3e269614316c13f7edf5aa6cf121985b4
오기선 <pakox123@gmail.com>
2024-05-16 11:43:25 +0900

    ✨ feat : 계약서 더미데이터 및 틀 제공

--- frontend/src/components/SmartContract/style/ContractMainStyle.tsx
+++ frontend/src/components/SmartContract/style/ContractMainStyle.tsx
@@ -6,15 +6,72 @@ export const ContractMainContainer = styled.div`
 `
 export const Contract = styled.div`
   position: relative;
-  left: 16px;
-  margin-top: 5px;
+  left: 1rem;
+  margin-top: 0.3125rem;
   width: 92%;
   height: 64vh;
-  border: 1px solid #f8bbbb;
-  background: #fff url('/contract.PNG') no-repeat center center;
+  border: 0.0625rem solid #f8bbbb;
+  background: #fff url('/image/contract.png') no-repeat center center;
   background-size: 100% 100%;
-  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  box-shadow: 0px 0.25rem 0.25rem 0px rgba(0, 0, 0, 0.25);
   display: flex;
-  align-items: center;
-  justify-content: center;
+  flex-direction: column;
+  /* align-items: center; */
+  /* justify-content: center; */
+  .content-box {
+    margin-left: 4.7rem;
+    display: flex;
+    margin-top: 1.8rem;
+    width: 7.5rem;
+    height: 1rem;
+    font-size: 0.7rem;
+    font-weight: 600;
+    justify-content: space-between;
+  }
+  .content-box2 {
+    display: flex;
+    margin-top: 1rem;
+    margin-left: 3.8rem;
+    font-size: 0.7rem;
+    font-weight: 600;
+    width: 15rem;
+    height: 0.8rem;
+    text-align: center;
+  }
+  .content-box3 {
+    display: flex;
+    justify-content: space-between;
+    margin-left: 6.2rem;
+    margin-top: 0.1rem;
+    width: 14rem;
+    height: 0.83rem;
+    font-size: 0.7rem;
+    font-weight: 600;
+  }
+  .content-box4 {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 0.1rem;
+    width: 16.4rem;
+    margin-left: 3.8rem;
+    height: 0.9rem;
+    font-size: 0.7rem;
+    font-weight: 600;
+  }
+  .content-box5 {
+    margin-top: 2rem;
+    width: 16.4rem;
+    margin-left: 5rem;
+    height: 0.9rem;
+    font-size: 0.7rem;
+    font-weight: 600;
+  }
+  .content-box6 {
+    margin-top: 0.07rem;
+    width: 16.4rem;
+    margin-left: 5rem;
+    height: 0.9rem;
+    font-size: 0.7rem;
+    font-weight: 600;
+  }
 `
