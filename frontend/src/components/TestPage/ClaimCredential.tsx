63d82c5a187a45077b4567e6da93c2bcfa6258ba
송강산 <rkdtks0816@naver.com>
2024-05-16 11:17:24 +0900

    ✨ feat : test 페이지 생성

---
+++ frontend/src/components/TestPage/ClaimCredential.tsx
@@ -0,0 +1,43 @@
+import { ethers } from 'ethers'
+import { useClaimCredential } from '@/abi/ownershipVCRegistry/claimCredential'
+import { MOIS_PRIVATE_KEY } from '@constants/abi/MOISPrivateKey'
+import { BLOCK_CHAIN_ENDPOINT } from '@constants/abi/abi'
+import { TestContainer } from '@components/TestPage/style/TestStyle'
+
+const ClaimCredential = () => {
+  const { mutate: claimCredentialMutate } = useClaimCredential()
+  const handleClaimCredentialButtonClick = async () => {
+    const provider = new ethers.providers.JsonRpcProvider(BLOCK_CHAIN_ENDPOINT)
+    const MOISWallet = new ethers.Wallet(MOIS_PRIVATE_KEY, provider)
+    const subject = 'did:klay:0x15b84A76cd54E0D086FE0E40Cb3eAc3dB3e9a593'
+    const value = 'did:klay:0x12345'
+    const message = ethers.utils.solidityKeccak256(
+      ['string', 'uint256', 'string'],
+      [subject, Math.floor(Date.now() / 1000), value],
+    )
+    const messageBytes = ethers.utils.arrayify(message)
+    const flatSig = await MOISWallet.signMessage(messageBytes)
+    const sig = ethers.utils.splitSignature(flatSig)
+    console.log(sig)
+
+    const params = {
+      _subject: subject,
+      _issuanceDate: Math.floor(Date.now() / 1000),
+      _r: sig.r,
+      _s: sig.s,
+      _v: sig.v,
+      _value: value,
+    }
+    claimCredentialMutate(params)
+  }
+
+  return (
+    <TestContainer>
+      <div>VC 생성 테스트</div>
+      <br />
+      <button onClick={handleClaimCredentialButtonClick}>VC 생성</button>
+    </TestContainer>
+  )
+}
+
+export default ClaimCredential
--- frontend/src/pages/TestPage.tsx
+++ frontend/src/pages/TestPage.tsx
@@ -1,12 +1,12 @@
-import Intro from '@components/IntroPage/Intro'
 import WaveContainer from '@common/WaveContainer'
 import { TestPageContainer } from '@pages/style/TestPageStyle'
+import Test from '@components/TestPage/test'
 
 const TestPage = () => {
   return (
     <TestPageContainer>
       <WaveContainer />
-      <Intro />
+      <Test />
     </TestPageContainer>
   )
 }
