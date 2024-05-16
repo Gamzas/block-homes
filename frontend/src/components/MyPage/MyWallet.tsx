5b1ad144193b39261d6b9354730d5e1085ffd31a
정유나 <jyn7077@naver.com>
2024-05-16 23:23:25 +0900

    ✨ feat	: 지갑 잔액 조회

--- frontend/src/components/MyPage/MyWallet.tsx
+++ frontend/src/components/MyPage/MyWallet.tsx
@@ -1,12 +1,18 @@
 import * as w from '@components/MyPage/style/MyWalletStyle'
+import useCheckBalance from '@/hooks/useCheckBalance'
+import { useEffect } from 'react'
 
 const MyWallet = () => {
+  const { handleCheckBalance, balance } = useCheckBalance()
+  useEffect(() => {
+    handleCheckBalance()
+  }, [handleCheckBalance])
   return (
     <w.WalletWrapper>
-      <w.WalletContainer>
+      <w.WalletContainer onClick={handleCheckBalance}>
         <w.BalanceBox>
-          <div className="title">나의 지갑</div>
-          <div className="balance">1,000 원</div>
+          <div className="title">나의 지갑 잔액</div>
+          <div className="balance">{balance} ETH</div>
         </w.BalanceBox>
         <img className="wallet" src="image/image_wallet.png" alt="지갑" />
       </w.WalletContainer>
