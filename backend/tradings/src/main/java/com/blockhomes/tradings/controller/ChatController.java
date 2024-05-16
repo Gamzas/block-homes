366ef3624a160a801bd9d479cad5dcd242e96e70
천세진 <sejinnnnnn@naver.com>
2024-05-16 17:28:25 +0900

    ✨ feat : 채팅 관련 API 추가

--- backend/tradings/src/main/java/com/blockhomes/tradings/controller/ChatController.java
+++ backend/tradings/src/main/java/com/blockhomes/tradings/controller/ChatController.java
@@ -1,8 +1,14 @@
 package com.blockhomes.tradings.controller;
 
-import com.blockhomes.tradings.dto.chat.ChatPayload;
+import com.blockhomes.tradings.dto.chat.payload.ChatPayload;
+import com.blockhomes.tradings.dto.chat.payload.EnterPayload;
+import com.blockhomes.tradings.dto.chat.payload.ProgressPayload;
+import com.blockhomes.tradings.dto.chat.payload.ProvisionPayload;
 import com.blockhomes.tradings.dto.chat.response.ChatRes;
+import com.blockhomes.tradings.dto.chat.response.ProgressRes;
+import com.blockhomes.tradings.dto.chat.response.ProvisionRes;
 import com.blockhomes.tradings.service.ChatService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -20,21 +26,28 @@ public class ChatController {
     private final RabbitTemplate rabbitTemplate;
 
     @MessageMapping("chat.enter.{chatRoomNo}")
-    public ChatRes enterUser(@DestinationVariable Integer chatRoomNo, @Payload ChatPayload chatPayload) {
-        return ChatRes.builder().build();
+    public ChatRes enterUser(@DestinationVariable Integer chatRoomNo, @Payload @Valid EnterPayload enterPayload) {
+        return chatService.enterUser(chatRoomNo, enterPayload);
     }
 
     @MessageMapping("chat.talk.{chatRoomNo}")
-    public ChatRes chatTalk(@DestinationVariable Integer chatRoomNo, @Payload ChatPayload chatPayload) {
-        return ChatRes.builder().build();
+    public ChatRes chatTalk(@DestinationVariable Integer chatRoomNo, @Payload @Valid ChatPayload chatPayload) {
+        return chatService.chatTalk(chatRoomNo, chatPayload);
     }
 
     @MessageMapping("chat.progress.{chatRoomNo}")
-    public ChatRes chatImage(@DestinationVariable Integer chatRoomNo, @Payload ChatPayload chatPayload) {
-        return ChatRes.builder().build();
+    public ProgressRes progressContract(@DestinationVariable Integer chatRoomNo, @Payload @Valid ProgressPayload progressPayload) {
+        return chatService.progressContract(chatRoomNo, progressPayload);
     }
 
-//    @MessageMapping("chat.signature.{chatRoomNo}")
+    @MessageMapping("chat.provision.{chatRoomNo}")
+    public ProvisionRes createSpecialProvision(@DestinationVariable Integer chatRoomNo, @Payload @Valid ProvisionPayload provisionPayload) {
+        return chatService.createSpecialProvision(chatRoomNo, provisionPayload);
+    }
 
+    @MessageMapping("chat.reject.{chatRoomNo}")
+    public ChatRes rejectProvision(@DestinationVariable Integer chatRoomNo, @Payload @Valid ChatPayload chatPayload) {
+        return chatService.rejectProvision(chatRoomNo, chatPayload);
+    }
 
 }
