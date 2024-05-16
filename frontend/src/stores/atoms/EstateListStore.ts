9ef6328eaaa49891b89de6528aee28db5b562387
정유나 <jyn7077@naver.com>
2024-05-16 11:17:23 +0900

    🌱 chore : 데이터 변경

--- frontend/src/stores/atoms/EstateListStore.ts
+++ frontend/src/stores/atoms/EstateListStore.ts
@@ -30,83 +30,8 @@ export const userCoordAtom = atom<CoordType>({
 // 위치 일치여부 상태관리
 export const matchAtom = atom<boolean>(false)
 
-const EstateList = [
-  {
-    condition: 'normal',
-    address: '남동길 30번길 13 3층',
-    infos: ['남향', '교육시설', '역세권'],
-    leaseType: '전세',
-    price: '3억',
-    roomSize: '37',
-    roomCount: '투룸',
-    createDate: '2024년 3월 21일',
-    latitude: 35.2097245,
-    longitude: 126.7805472,
-  },
-  {
-    condition: 'bad',
-    address: '광주광역시 광산구 오선동',
-    infos: ['남향', '교육시설', '역세권'],
-    leaseType: '매매',
-    price: '3억',
-    roomSize: '37',
-    roomCount: '투룸',
-    createDate: '2024년 3월 21일',
-    latitude: 35.205615,
-    longitude: 126.812546,
-  },
-  {
-    condition: 'good',
-    address: '광주광역시 광산구 장덕동',
-    infos: ['남향', '교육시설', '역세권'],
-    leaseType: '전세',
-    price: '3억',
-    roomSize: '37',
-    roomCount: '투룸',
-    createDate: '2024년 3월 21일',
-    latitude: 35.204315,
-    longitude: 126.812546,
-  },
-  {
-    condition: 'normal',
-    address: '남동길 30번길 13 3층',
-    infos: ['남향', '교육시설', '역세권'],
-    leaseType: '전세',
-    price: '3억',
-    roomSize: '37',
-    roomCount: '투룸',
-    createDate: '2024년 3월 21일',
-    latitude: 35.204615,
-    longitude: 126.813546,
-  },
-  {
-    condition: 'bad',
-    address: '광주광역시 광산구 오선동',
-    infos: ['남향', '교육시설', '역세권'],
-    leaseType: '매매',
-    price: '3억',
-    roomSize: '37',
-    roomCount: '투룸',
-    createDate: '2024년 3월 21일',
-    latitude: 35.202615,
-    longitude: 126.814546,
-  },
-  {
-    condition: 'good',
-    address: '광주광역시 광산구 장덕동',
-    infos: ['남향', '교육시설', '역세권'],
-    leaseType: '월세',
-    price: '3억',
-    roomSize: '37',
-    roomCount: '투룸',
-    createDate: '2024년 3월 21일',
-    latitude: 35.204615,
-    longitude: 126.815546,
-  },
-]
-
 // 전체 부동산 매물
-export const estateItemListAtom = atom<EstateItem[] | null>(EstateList)
+export const estateItemListAtom = atom<EstateItem[] | null>([])
 
 // 선택된 부동산 매물
 export const selectedItemAtom = atomWithStorage<EstateItem | 'not'>(
