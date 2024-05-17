f36b61a089ef2b2ac14d6f6bb62db57d1ee6bdee
정유나 <jyn7077@naver.com>
2024-05-17 09:08:13 +0900

    🐛 fix : 카테고리 별 아이템 출력

--- frontend/src/components/EstateList/EstateItemList.tsx
+++ frontend/src/components/EstateList/EstateItemList.tsx
@@ -5,32 +5,47 @@ import { useEffect } from 'react'
 import EstateItemFilter from './EstateItemFilter'
 import * as l from '@components/EstateList/styles/EstateItemListStyle'
 import { useAtom } from 'jotai'
-import {
-  estateItemListAtom,
-  filterAtom,
-  mapAtom,
-} from '@/stores/atoms/EstateListStore'
+import { filterAtom, mapAtom } from '@/stores/atoms/EstateListStore'
 import EstateListMap from './EstateListMap'
 import NoItems from '@common/NoItems'
 import { EstateItemListType } from '@/types/api/itemType'
+import { useParams } from 'react-router-dom'
+import { useGetEstateItems } from '@/apis/itemApi'
+import ItemLoading from '@/common/ItemLoading'
+import { itemList } from '../../utils/item'
 
 const EstateItemList = () => {
+  const { category } = useParams()
   const { getCurrentLocation } = useCurrentLocation()
   const [filter] = useAtom(filterAtom)
   const [menu] = useAtom(mapAtom)
-  const [estateItemArr] = useAtom(estateItemListAtom)
-  const estateItemList: EstateItemListType[] = estateItemArr.itemList
   useEffect(() => {
     getCurrentLocation()
   }, [])
+  const { data, isLoading, error } = useGetEstateItems(Number(category))
 
+  if (isLoading) {
+    return <ItemLoading />
+  }
+
+  if (error || !data || !data.itemList) {
+    return (
+      <NoItems
+        src={'/image/image_warning_pig.png'}
+        alarmText={'데이터를 불러오는 중 오류가 발생했습니다.'}
+      />
+    )
+  }
+
+  const estateItemList: EstateItemListType[] = data.itemList
+  console.log(data.itemList)
   return (
     <l.EstateItemListContainer>
       <l.StatusBarContainer>
         <CurrentStatus getCurrentLocation={getCurrentLocation} />
       </l.StatusBarContainer>
       {menu ? (
-        estateItemList === null || estateItemList.length === 0 ? (
+        estateItemList === undefined || estateItemList.length === 0 ? (
           // TODO 매물 없을때 컴포넌트 분리해서 완성하기
           <NoItems
             src={'/image/image_warning_pig.png'}
