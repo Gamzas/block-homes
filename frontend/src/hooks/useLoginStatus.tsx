be9b61c73cbc1298680b264242d1449b4178efd7
rkdtks0816 <rkdtks0816@naver.com>
2024-05-17 22:32:36 +0900

    🐛 fix : 로그인 확인 수정

--- frontend/src/hooks/useLoginStatus.tsx
+++ frontend/src/hooks/useLoginStatus.tsx
@@ -7,15 +7,8 @@ const useLoginStatus = () => {
   const navigate = useNavigate()
   const [user, setUser] = useAtom(userAtom)
   useEffect(() => {
-    const currentUser = localStorage.getItem('currentUser')
-    const currentUserData = currentUser ? JSON.parse(currentUser) : null
-    if (currentUserData && user.name === '') {
-      setUser(currentUserData)
-    } else if (!currentUserData && user.name !== '') {
-      localStorage.setItem('currentUser', JSON.stringify(user))
-    } else if (!currentUserData && user.name === '') {
+    if (user.name === '') {
       alert('로그인 후 이용해주세요')
-      navigate('/signin')
     }
   }, [navigate, user, setUser])
 }
