6861793e821d46416f57417fd9b887e7f2c140b7
정유나 <jyn7077@naver.com>
2024-05-18 20:17:13 +0900

    🐛 fix : 상세페이지 비로그인시 접근 불가

--- frontend/src/pages/RealEstateDetailPage.tsx
+++ frontend/src/pages/RealEstateDetailPage.tsx
@@ -1,8 +1,10 @@
 import * as r from '@pages/style/RealEstatePageStyle'
 import Header from '@/common/Header'
 import DetailInfo from '@/components/EstateDetailPage/DetailInfo'
+import useLoginStatus from '@/hooks/useLoginStatus'
 
 const RealEstateDetailPage = () => {
+  useLoginStatus()
   return (
     <r.Container>
       <Header title={'매물 상세 조회'} isSearch={false} rightIconSrc={''} />
