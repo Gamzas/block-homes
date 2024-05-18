ef421568e15a5455403a7e50f1ec7a129794c4a3
천세진 <sejinnnnnn@naver.com>
2024-05-18 15:15:34 +0900

    🌱 chore : TimeZone 설정 추가

--- backend/tradings/src/main/java/com/blockhomes/tradings/TradingsApplication.java
+++ backend/tradings/src/main/java/com/blockhomes/tradings/TradingsApplication.java
@@ -1,10 +1,18 @@
 package com.blockhomes.tradings;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class TradingsApplication {
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TradingsApplication.class, args);
