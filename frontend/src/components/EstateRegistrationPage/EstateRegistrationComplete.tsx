3a904e53bd59130511f13c53a581aa4d59971895
rkdtks0816 <rkdtks0816@naver.com>
2024-05-19 01:59:09 +0900

    ✨ feat : 매물 등록 완료페이지 로티 추가

--- frontend/src/components/EstateRegistrationPage/EstateRegistrationComplete.tsx
+++ frontend/src/components/EstateRegistrationPage/EstateRegistrationComplete.tsx
@@ -1,24 +1,33 @@
-import CompletePigContainer from '@/common/CompletePigContainer'
-import {CustomButtonStyle} from '@common/style/CustomButtonStyle'
-import {useNavigate} from 'react-router-dom'
-import Gauge from '../Report/Gauge'
-import * as e from "@components/EstateRegistrationPage/style/EstateRegistrationCompleteStyle";
+import Lottie from 'react-lottie'
+import { CustomButtonStyle } from '@common/style/CustomButtonStyle'
+import { useNavigate } from 'react-router-dom'
+import * as e from '@components/EstateRegistrationPage/style/EstateRegistrationCompleteStyle'
+import House from '@assets/lotties/House.json'
 
 const EstateRegistrationComplete = () => {
-    const navigate = useNavigate()
-    const hadleHome = () => {
-        navigate('/')
-    }
-    return (
-        <e.EstateRegistrationCompleteContainer>
-            <Gauge steps={5}/>
-            <CompletePigContainer></CompletePigContainer>
-            <div className="complete-text">매물 등록이 완료되었습니다</div>
-            <div className="button-box">
-                <CustomButtonStyle onClick={hadleHome}>등록 완료</CustomButtonStyle>
-            </div>
-        </e.EstateRegistrationCompleteContainer>
-    )
+  const navigate = useNavigate()
+  const hadleHome = () => {
+    navigate('/')
+  }
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: House,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  }
+  return (
+    <e.EstateRegistrationCompleteContainer>
+      <div className="lottie-container">
+        <Lottie options={defaultOptions} height={350} width={350} />
+      </div>
+      <div className="complete-text">매물 등록이 완료되었습니다</div>
+      <div className="button-box">
+        <CustomButtonStyle onClick={hadleHome}>등록 완료</CustomButtonStyle>
+      </div>
+    </e.EstateRegistrationCompleteContainer>
+  )
 }
 
 export default EstateRegistrationComplete
--- frontend/src/components/EstateRegistrationPage/style/EstateRegistrationCompleteStyle.tsx
+++ frontend/src/components/EstateRegistrationPage/style/EstateRegistrationCompleteStyle.tsx
@@ -1,21 +1,22 @@
 import styled from 'styled-components'
 
 export const EstateRegistrationCompleteContainer = styled.div`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 35%;
 
-    .button-box {
-        position: absolute;
-        bottom: 10vh;
-    }
+  .button-box {
+    position: absolute;
+    bottom: 10vh;
+  }
 
-    .complete-text {
-        font-weight: 600;
-        font-size: 20px;
-        z-index: 100;
-    }
+  .complete-text {
+    font-weight: 600;
+    font-size: 20px;
+    z-index: 100;
+  }
 `
