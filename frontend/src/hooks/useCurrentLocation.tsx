76e63e7c9c478d66d26020d952c5f7b5602ac6c0
정유나 <jyn7077@naver.com>
2024-05-18 13:24:18 +0900

    ✨ feat	  : 현재 위치 설정 hook 수정`

--- frontend/src/hooks/useCurrentLocation.tsx
+++ frontend/src/hooks/useCurrentLocation.tsx
@@ -11,24 +11,18 @@ import {
   KakaoMapsStatus,
 } from '@/types/components/kakaomapType'
 import { useAtom, useSetAtom } from 'jotai'
-import { useEffect } from 'react'
+import { useCallback, useEffect, useRef } from 'react'
 
 const { kakao } = window
 
 const useCurrentLocation = () => {
-  // 지도 중심의 위치(상태바 출력)
   const setTown = useSetAtom(currentPositonAtom)
-  // 마커 좌표
   const [coord, setCoord] = useAtom(currentCoordAtom)
-  // 지도 중심 위치 와 사용자 위치 일지 여부
   const [match, setMatch] = useAtom(matchAtom)
-  // 위치 출력을 위한 좌표(지도 이동 시 지도의 센터 좌표값 설정 됨)
-  // 지도 중심
   const [location, setLocation] = useAtom(mapCenterCoordAtom)
-  // 사용자 현재 위치
   const [userCoord, setUserCoord] = useAtom(userCoordAtom)
+  const lastLocationRef = useRef<CoordType>({ latitude: 0, longitude: 0 })
 
-  // 현재 위치와 지도 중심의 위치를 비교
   const checkCoordinatesMatch = (
     coord: CoordType,
     location: CoordType,
@@ -39,41 +33,35 @@ const useCurrentLocation = () => {
     )
   }
 
-  // 현재 위치와 지도 중심의 위치가 바뀔때마다 비교
   useEffect(() => {
-    // 지도 중심과 사용자 현재 위치 비교
     const isMatch = checkCoordinatesMatch(location, userCoord)
     setMatch(isMatch)
   }, [coord, location, setMatch])
 
-  // 현재 좌표 받아오면 location 에 set 하기
   useEffect(() => {
     setLocation(coord)
-  }, [coord])
+  }, [coord, setLocation])
 
-  // 위치 받아오기
-  const getCurrentLocation = () => {
+  const getCurrentLocation = useCallback(() => {
     navigator.geolocation.getCurrentPosition(successHandler, errorHandler)
-  }
+  }, [])
 
   const successHandler = (res: GeolocationPosition) => {
     const { latitude, longitude } = res.coords
-    // 사용자 현재 위치
-    setUserCoord({ latitude, longitude })
-    // 마커 위치
-    setCoord({ latitude, longitude })
+    setUserCoord({ latitude, longitude }) // 사용자 위치
+    setCoord({ latitude, longitude }) // 마커 위치
+    // setLocation({ latitude, longitude }) // 지도 중심을 사용자의 위치로 설정
   }
+
   const errorHandler = (err: GeolocationPositionError) => {
     console.log(err)
   }
 
   useEffect(() => {
-    // 앞 지역만 추출
     const extractFirstWord = (text: string) => {
       return text.split(' ')[0]
     }
 
-    // 좌표 => 주소 변환
     const getAddress = (lat: number, lng: number) => {
       const geocoder = new kakao.maps.services.Geocoder()
       const coord = new kakao.maps.LatLng(lat, lng)
@@ -85,9 +73,24 @@ const useCurrentLocation = () => {
       geocoder.coord2Address(coord.getLng(), coord.getLat(), callback)
     }
     getAddress(location.latitude, location.longitude)
+
+    // Update the address only if the location has changed significantly
+    const distanceChanged = (
+      loc1: CoordType,
+      loc2: CoordType,
+      threshold = 0.001,
+    ) => {
+      const latDiff = Math.abs(loc1.latitude - loc2.latitude)
+      const lngDiff = Math.abs(loc1.longitude - loc2.longitude)
+      return latDiff > threshold || lngDiff > threshold
+    }
+
+    if (distanceChanged(location, lastLocationRef.current)) {
+      getAddress(location.latitude, location.longitude)
+      lastLocationRef.current = location
+    }
   }, [location])
 
-  // 위도 경도 , 현재 위치, 현재위치 재설정
   return { getCurrentLocation, location, setLocation, match }
 }
 
