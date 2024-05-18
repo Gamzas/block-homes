c0240783242a6384a7f750feaee28d64a3e37c5f
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-19 01:06:22 +0900

    ✨ feat : provisionsAtom 생성

--- frontend/src/stores/atoms/chat.ts
+++ frontend/src/stores/atoms/chat.ts
@@ -3,3 +3,12 @@ import { atom } from 'jotai/index'
 export const isGoNextStepAtom = atom<boolean>(false)
 
 export const chatRoomNoAtom = atom<number>(0)
+
+export const provisionsAtom = atom<boolean[]>([
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+])
