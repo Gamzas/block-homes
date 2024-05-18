21db72fdc4a8988205ce5bfeb7fc70de400a0c4f
정유나 <jyn7077@naver.com>
2024-05-18 20:40:53 +0900

    🐛 fix : 비로그인시에도 상세 페이지 조회할 수 있게 수정

--- frontend/src/pages/RealEstateDetailPage.tsx
+++ frontend/src/pages/RealEstateDetailPage.tsx
@@ -1,10 +1,8 @@
 import * as r from '@pages/style/RealEstatePageStyle'
 import Header from '@/common/Header'
 import DetailInfo from '@/components/EstateDetailPage/DetailInfo'
-import useLoginStatus from '@/hooks/useLoginStatus'
 
 const RealEstateDetailPage = () => {
-  useLoginStatus()
   return (
     <r.Container>
       <Header title={'매물 상세 조회'} isSearch={false} rightIconSrc={''} />
