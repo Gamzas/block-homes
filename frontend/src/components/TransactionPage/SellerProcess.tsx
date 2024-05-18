9d6bdb5bd64894343ceacb0e4dc6f2b33220849d
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-18 23:59:41 +0900

    🐛 fix : sellerStepAtom 대신 userStepAtom 사용

--- frontend/src/components/TransactionPage/SellerProcess.tsx
+++ frontend/src/components/TransactionPage/SellerProcess.tsx
@@ -1,10 +1,10 @@
 import * as b from '@components/TransactionPage/style/BuyerProcessStyle'
 import ProcessDetail from '@components/TransactionPage/ProcessDetail'
 import { useAtom } from 'jotai/index'
-import { sellerStepAtom } from '@stores/atoms/userStore'
+import { userStepAtom } from '@stores/atoms/userStore'
 
 const SellerProcess = () => {
-  const [sellerStep] = useAtom(sellerStepAtom)
+  const [userStep] = useAtom(userStepAtom)
 
   return (
     <div>
@@ -12,7 +12,7 @@ const SellerProcess = () => {
         <ProcessDetail
           title="[[부동산 거래 요청]]"
           content="구매자에게 거래 요청을 보내고 거래를 시작하세요"
-          currentStep={sellerStep}
+          currentStep={userStep}
           stepIndex={1}
         />
         <ProcessDetail
@@ -20,13 +20,13 @@ const SellerProcess = () => {
           content="원하는 [[특약사항]]을 추가해보세요!
           집주인과 대화를 통해 특약사항을 조합해
           나갈 수 있습니다."
-          currentStep={sellerStep}
+          currentStep={userStep}
           stepIndex={3}
         />
         <ProcessDetail
           title="[[계약서 블록체인 등록 및 서명]]"
           content="블록체인에 스마트 컨트랙트를 배포 후 서명을 해주세요. 거래가 완료된 이후 지갑에 돈이 자동으로 입금 되어집니다!"
-          currentStep={sellerStep}
+          currentStep={userStep}
           stepIndex={5}
         />
         <ProcessDetail
@@ -34,7 +34,7 @@ const SellerProcess = () => {
           content="거래가 완료되었습니다.
           거래 내역은 블록체인에 자동으로 기록되어
           투명하게 관리되니 걱정마세요!"
-          currentStep={sellerStep}
+          currentStep={userStep}
           stepIndex={7}
         />
       </b.BuyerProcessContainer>
