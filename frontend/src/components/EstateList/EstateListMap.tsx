2a109a29abc75e8fa018f920548cf8fd5a1a113f
정유나 <jyn7077@naver.com>
2024-05-18 15:40:25 +0900

    🐛 fix : 스토어에 저장 된 매물 리스트로 오버레이 출력 및 지도 조금씩 움직이는 오류 수정

--- frontend/src/components/EstateList/EstateListMap.tsx
+++ frontend/src/components/EstateList/EstateListMap.tsx
@@ -1,14 +1,7 @@
-import {
-  useEffect,
-  useState,
-  useRef,
-  forwardRef,
-  useImperativeHandle,
-} from 'react'
+import { useEffect, useState, useRef, forwardRef } from 'react'
 import * as e from '@components/EstateList/styles/EstateListMapStyle'
 import { renderToString } from 'react-dom/server'
 import CustomOverlay from '@components/EstateList/CustomOverlay'
-import useCurrentLocation from '@/hooks/useCurrentLocation'
 import { useAtom } from 'jotai'
 import {
   selectedItemAtom,
@@ -16,6 +9,7 @@ import {
   filterAtom,
   mapCenterCoordAtom,
   userCoordAtom,
+  estateItemListAtom,
 } from '@/stores/atoms/EstateListStore'
 import EstateItemCard from './EstateItemCard'
 import { EstateItemListType } from '@/types/api/itemType'
@@ -29,52 +23,27 @@ declare global {
 const { kakao } = window
 
 const EstateListMap = forwardRef((props, ref) => {
-  const { match } = useCurrentLocation()
   const [coord] = useAtom(currentCoordAtom)
   const [location, setLocation] = useAtom(mapCenterCoordAtom)
   const [userCoord] = useAtom(userCoordAtom)
+  const [items] = useAtom(estateItemListAtom)
 
   const [filter] = useAtom(filterAtom)
-  const { category } = useParams()
-  const estateItemList: EstateItemListType[] = []
+  const estateItemList: EstateItemListType[] = items.itemList || []
   const [marker, setMarker] = useState(null)
   const [item, setItem] = useAtom(selectedItemAtom)
-  const [previousCenter, setPreviousCenter] = useState({
-    latitude: coord.latitude,
-    longitude: coord.longitude,
-  })
 
   const mapRef = useRef(null)
-
-  const distanceThreshold = 2.5 // 2500m
   const initialLoad = useRef(true)
   const centerChangeTimeout = useRef(null)
+  const isUserInteraction = useRef(false)
 
   const handleDetailCardClose = () => {
     setItem('not')
   }
 
-  // useImperativeHandle(ref, () => ({
-  //   setCenter: () => {
-  //     if (mapRef.current) {
-  //       const map = mapRef.current
-  //       const moveLatLon = new kakao.maps.LatLng(
-  //         userCoord.latitude,
-  //         userCoord.longitude,
-  //       )
-  //       map.panTo(moveLatLon)
-  //       // setLocation({
-  //       //   latitude: moveLatLon.getLat(),
-  //       //   longitude: moveLatLon.getLng(),
-  //       // })
-  //     }
-  //   },
-  // }))
-
   useEffect(() => {
     if (!initialLoad.current) return
-    console.log('렌더링!')
-
     const container = document.getElementById('map')
     const options = {
       center: new kakao.maps.LatLng(userCoord.latitude, userCoord.longitude),
@@ -87,7 +56,7 @@ const EstateListMap = forwardRef((props, ref) => {
     mapRef.current = map
     initialLoad.current = false
 
-    map.setMaxLevel(10)
+    map.setMaxLevel(7)
     const zoomControl = new kakao.maps.ZoomControl()
     map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT)
 
@@ -95,23 +64,27 @@ const EstateListMap = forwardRef((props, ref) => {
     const imageSize = new kakao.maps.Size(48, 48)
     const imageOption = { offset: new kakao.maps.Point(32, 45) }
 
-    // kakao.maps.event.addListener(map, 'center_changed', () => {
-    //   const center = map.getCenter()
-    //   const newCenter = { latitude: center.Ma, longitude: center.La }
-    //   setLocation(newCenter)
-    // })
-
     kakao.maps.event.addListener(map, 'center_changed', () => {
       if (centerChangeTimeout.current) {
         clearTimeout(centerChangeTimeout.current)
       }
       centerChangeTimeout.current = setTimeout(() => {
-        const center = map.getCenter()
-        setLocation({
-          latitude: center.Ma,
-          longitude: center.La,
-        })
-      }, 100) // 300ms 타임아웃
+        if (isUserInteraction.current) {
+          const center = map.getCenter()
+          setLocation({
+            latitude: center.Ma,
+            longitude: center.La,
+          })
+        }
+      }, 100)
+    })
+
+    kakao.maps.event.addListener(map, 'dragstart', () => {
+      isUserInteraction.current = true
+    })
+
+    kakao.maps.event.addListener(map, 'dragend', () => {
+      isUserInteraction.current = false
     })
 
     const markerImage = new kakao.maps.MarkerImage(
@@ -190,17 +163,16 @@ const EstateListMap = forwardRef((props, ref) => {
 
   useEffect(() => {
     const map = mapRef.current
-    const setCenter = () => {
-      // 이동할 위도 경도 위치를 생성
-      const moveLatLon = new kakao.maps.LatLng(
-        location.latitude,
-        location.longitude,
-      )
-
-      // 지도 중심을 이동 시킵니다
-      map.panTo(moveLatLon)
+    if (!isUserInteraction.current) {
+      const setCenter = () => {
+        const moveLatLon = new kakao.maps.LatLng(
+          location.latitude,
+          location.longitude,
+        )
+        map.panTo(moveLatLon)
+      }
+      setCenter()
     }
-    setCenter()
   }, [location])
 
   return (
