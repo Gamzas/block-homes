df56c59dcc78e6c0fd2f191e510cbb472d47d697
정유나 <jyn7077@naver.com>
2024-05-18 23:12:26 +0900

    🎨 style : 삭제버튼 레이아웃 및 페이지 레이아웃 재설정

--- frontend/src/components/MyEstatePage/style/MyEstateListStyle.tsx
+++ frontend/src/components/MyEstatePage/style/MyEstateListStyle.tsx
@@ -23,5 +23,24 @@ export const MyEstateListContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin-top: 1rem;
+  overflow-y: scroll;
+  background-color: white;
+  padding-bottom: 5rem;
+  /* 스크롤바 숨기기를 위한 스타일 */
+  ::-webkit-scrollbar {
+    display: none; // 웹킷(Chrome, Safari 등) 브라우저에서 스크롤바 숨김
+  }
+  -ms-overflow-style: none; // IE, Edge에서 스크롤바 숨김
+  scrollbar-width: none; // Firefox에서 스크롤바 숨김
+`
+export const ItemContainer = styled.div`
+  width: 100%;
+  position: relative;
+  /* border: 1px solid red; */
+  .delete-box {
+    position: absolute;
+    bottom: 1rem;
+    right: 2rem;
+    /* border: 1px solid black; */
+  }
 `
