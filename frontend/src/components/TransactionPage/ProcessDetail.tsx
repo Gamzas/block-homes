372797dc0e8501f746988dec37a50f53975c1e41
KIMHYEONJI <hyeonji0718@gmail.com>
2024-05-19 00:01:27 +0900

    ✨ feat : userModeAtom와 userStepAtom으로 각 버튼 렌더링

--- frontend/src/components/TransactionPage/ProcessDetail.tsx
+++ frontend/src/components/TransactionPage/ProcessDetail.tsx
@@ -6,7 +6,7 @@ import SpecialContract from '@components/TransactionPage/SpecialContract'
 import { useAtom } from 'jotai/index'
 import { isGoNextStepAtom } from '@stores/atoms/chat'
 import { useNavigate } from 'react-router-dom'
-import { buyerStepAtom, sellerStepAtom } from '@stores/atoms/userStore'
+import { userModeAtom, userStepAtom } from '@stores/atoms/userStore'
 
 interface ProcessDetailProps {
   title: string
@@ -19,12 +19,15 @@ const ProcessDetail = (props: ProcessDetailProps) => {
   const navigate = useNavigate()
   const [isSpecialOpen, setIsSpecialOpen] = useState(false)
   const [, setIsGoNextStep] = useAtom(isGoNextStepAtom)
-  const [sellerStep] = useAtom(sellerStepAtom)
-  const [buyerStep] = useAtom(buyerStepAtom)
+  // const [sellerStep] = useAtom(sellerStepAtom)
+  // const [buyerStep] = useAtom(buyerStepAtom)
+  const [userMode] = useAtom(userModeAtom)
+  const [userStep] = useAtom(userStepAtom)
+  console.log(userStep)
 
   useEffect(() => {
-    console.log(sellerStep, buyerStep)
-  }, [sellerStep, buyerStep])
+    console.log(userStep)
+  }, [])
 
   const handleCloseModal = () => {
     setIsSpecialOpen(false)
@@ -70,6 +73,22 @@ const ProcessDetail = (props: ProcessDetailProps) => {
               />
               <circle cx="12" cy="12" r="11" stroke="#845BD3" strokeWidth="2" />
             </svg>
+          ) : props.stepIndex < props.currentStep ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="17"
+              height="17"
+              viewBox="0 0 17 17"
+              fill="#845BD3"
+            >
+              <circle
+                cx="8.5"
+                cy="8.5"
+                r="7.5"
+                stroke="#845BD3"
+                strokeWidth="2"
+              />
+            </svg>
           ) : (
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -107,130 +126,148 @@ const ProcessDetail = (props: ProcessDetailProps) => {
         </p.ProcessContentContainer>
 
         <div className="button-box">
-          {props.currentStep === props.stepIndex && props.stepIndex === 1 && (
-            <CustomButtonStyle
-              style={{
-                width: '7rem',
-                height: '2.5rem',
-                fontSize: '0.8rem',
-                marginLeft: '0.5rem',
-                fontWeight: '500',
-                boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
-              }}
-              onClick={goNextStep}
-            >
-              요청 보내기
-            </CustomButtonStyle>
-          )}
+          {userMode === 2 &&
+            props.currentStep === props.stepIndex &&
+            props.stepIndex === 0 && (
+              <CustomButtonStyle
+                style={{
+                  width: '7rem',
+                  height: '2.5rem',
+                  fontSize: '0.8rem',
+                  marginLeft: '0.5rem',
+                  fontWeight: '500',
+                  boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
+                }}
+                onClick={goNextStep}
+              >
+                요청 보내기
+              </CustomButtonStyle>
+            )}
 
-          {props.currentStep === props.stepIndex && props.stepIndex === 2 && (
-            <CustomButtonStyle
-              style={{
-                width: '7rem',
-                height: '2.5rem',
-                fontSize: '0.8rem',
-                marginLeft: '0.5rem',
-                fontWeight: '500',
-                boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
-              }}
-              onClick={goNextStep}
-            >
-              요청 승낙 하기
-            </CustomButtonStyle>
-          )}
+          {userMode === 1 &&
+            props.currentStep === 2 &&
+            props.stepIndex === 2 && (
+              <CustomButtonStyle
+                style={{
+                  width: '7rem',
+                  height: '2.5rem',
+                  fontSize: '0.8rem',
+                  marginLeft: '0.5rem',
+                  fontWeight: '500',
+                  boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
+                }}
+                onClick={goNextStep}
+              >
+                요청 승낙 하기
+              </CustomButtonStyle>
+            )}
 
-          {props.currentStep === props.stepIndex && props.stepIndex === 3 && (
-            <CustomButtonStyle
-              style={{
-                width: '8rem',
-                height: '2.5rem',
-                fontSize: '0.8rem',
-                boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
-                fontWeight: '600',
-              }}
-              onClick={openModalAndGoNextStep}
-            >
-              특약사항 작성하기
-            </CustomButtonStyle>
-          )}
+          {userMode === 2 &&
+            props.currentStep === 3 &&
+            props.stepIndex === 3 && (
+              <CustomButtonStyle
+                style={{
+                  width: '8rem',
+                  height: '2.5rem',
+                  fontSize: '0.8rem',
+                  boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
+                  fontWeight: '600',
+                }}
+                // onClick={openModalAndGoNextStep}
+                onClick={goNextStep}
+              >
+                특약사항 작성하기
+              </CustomButtonStyle>
+            )}
 
-          {props.currentStep === props.stepIndex && props.stepIndex === 4 && (
-            <CustomButtonStyle
-              style={{
-                width: '8rem',
-                height: '2.5rem',
-                fontSize: '0.8rem',
-                boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
-                fontWeight: '600',
-              }}
-              onClick={openModalAndGoNextStep}
-            >
-              특약사항 작성하기
-            </CustomButtonStyle>
-          )}
+          {userMode === 1 &&
+            props.currentStep === 4 &&
+            props.stepIndex === 4 && (
+              <CustomButtonStyle
+                style={{
+                  width: '8rem',
+                  height: '2.5rem',
+                  fontSize: '0.8rem',
+                  boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
+                  fontWeight: '600',
+                }}
+                // onClick={openModalAndGoNextStep}
+                onClick={goNextStep}
+              >
+                특약사항 작성하기
+              </CustomButtonStyle>
+            )}
 
-          {props.currentStep === props.stepIndex && props.stepIndex === 5 && (
-            <CustomButtonStyle
-              style={{
-                width: '9rem',
-                height: '2.5rem',
-                fontSize: '0.8rem',
-                boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
-                fontWeight: '600',
-              }}
-              //계약서 서명 페이지로 이동
-              onClick={goNextStep}
-            >
-              계약서 생성 및 서명
-            </CustomButtonStyle>
-          )}
+          {userMode === 2 &&
+            props.currentStep === 5 &&
+            props.stepIndex === 5 && (
+              <CustomButtonStyle
+                style={{
+                  width: '9rem',
+                  height: '2.5rem',
+                  fontSize: '0.8rem',
+                  boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
+                  fontWeight: '600',
+                }}
+                //계약서 서명 페이지로 이동
+                onClick={goNextStep}
+              >
+                계약서 생성 및 서명
+              </CustomButtonStyle>
+            )}
 
-          {props.currentStep === props.stepIndex && props.stepIndex === 6 && (
-            <CustomButtonStyle
-              // onClick={handleOpenModal}
-              style={{
-                width: '8rem',
-                height: '2.5rem',
-                fontSize: '0.8rem',
-                marginLeft: '0.5rem',
-                fontWeight: '500',
-                boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
-              }}
-              onClick={goNextStep}
-            >
-              서명 및 결제 하기
-            </CustomButtonStyle>
-          )}
+          {userMode === 1 &&
+            props.currentStep === 6 &&
+            props.stepIndex === 6 && (
+              <CustomButtonStyle
+                // onClick={handleOpenModal}
+                style={{
+                  width: '8rem',
+                  height: '2.5rem',
+                  fontSize: '0.8rem',
+                  marginLeft: '0.5rem',
+                  fontWeight: '500',
+                  boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
+                }}
+                onClick={goNextStep}
+              >
+                서명 및 결제 하기
+              </CustomButtonStyle>
+            )}
 
-          {props.currentStep === props.stepIndex && props.stepIndex === 7 && (
-            <CustomButtonStyle
-              style={{
-                width: '9rem',
-                height: '2.5rem',
-                fontSize: '0.8rem',
-                boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
-                fontWeight: '600',
-              }}
-              onClick={goNextStep}
-            >
-              홈으로 돌아가기
-            </CustomButtonStyle>
-          )}
+          {userMode === 2 &&
+            props.currentStep === 7 &&
+            props.stepIndex === 7 && (
+              <CustomButtonStyle
+                style={{
+                  width: '9rem',
+                  height: '2.5rem',
+                  fontSize: '0.8rem',
+                  boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
+                  fontWeight: '600',
+                }}
+                onClick={goNextStep}
+              >
+                홈으로 돌아가기
+              </CustomButtonStyle>
+            )}
 
-          {props.currentStep === props.stepIndex && props.stepIndex === 8 && (
-            <CustomButtonStyle
-              style={{
-                width: '9rem',
-                height: '2.5rem',
-                fontSize: '0.8rem',
-                boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
-                fontWeight: '600',
-              }}
-              onClick={goNextStep}
-            >
-              홈으로 돌아가기
-            </CustomButtonStyle>
-          )}
+          {userMode === 1 &&
+            props.currentStep === 8 &&
+            props.stepIndex === 8 && (
+              <CustomButtonStyle
+                style={{
+                  width: '9rem',
+                  height: '2.5rem',
+                  fontSize: '0.8rem',
+                  boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
+                  fontWeight: '600',
+                }}
+                onClick={goNextStep}
+              >
+                홈으로 돌아가기
+              </CustomButtonStyle>
+            )}
         </div>
       </p.RightProcessDetailContainer>
     </p.ProcessDetailContainer>
