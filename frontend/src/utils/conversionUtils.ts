3aa8a87f9ff2baf8681148abe9161b3a5d0a4e66
정유나 <jyn7077@naver.com>
2024-05-19 17:12:08 +0900

    🌱 chore : 불필요한 코드 삭제

--- frontend/src/utils/conversionUtils.ts
+++ frontend/src/utils/conversionUtils.ts
@@ -22,10 +22,3 @@ export const transformData = (input: string): string[] => {
 
   return numberedList
 }
-
-// Example usage
-const data =
-  '잔금일까지 해당 주택에 근저당 추가 설정하지 않는다/ 계약기간이 만료되면 새 임차인을 구하는 여부와 관계 없이 만료일에 보증금을 반환해준다/ 만기전 퇴거 시 새 임차인의 중개보수를 임차인이 부담한다/ 만기전 퇴거 시 새 임차인의 중개보수를 임차인이 부담한다.'
-const result = transformData(data)
-
-console.log(result)
