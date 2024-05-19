e2d74c27fe4b45871ae92bb058e74de9eafbf3c8
정유나 <jyn7077@naver.com>
2024-05-19 19:30:36 +0900

    ✨ feat : 계약서 내용 렌더링

--- frontend/src/components/MyContractpage/ContractDetailCard.tsx
+++ frontend/src/components/MyContractpage/ContractDetailCard.tsx
@@ -10,13 +10,14 @@ import * as d from '@components/MyContractpage/style/ContractDetailCardStyle'
 interface PropType {
   contractInfo: LeaseContractType
   estateInfo: RealEstateInfoType
+  setShowContract: (show: boolean) => void
 }
 const ContractDetailCard = (props: PropType) => {
-  const data =
-    '잔금일까지 해당 주택에 근저당 추가 설정하지 않는다/ 계약기간이 만료되면 새 임차인을 구하는 여부와 관계 없이 만료일에 보증금을 반환해준다/ 만기전 퇴거 시 새 임차인의 중개보수를 임차인이 부담한다/ 만기전 퇴거 시 새 임차인의 중개보수를 임차인이 부담한다.'
-  const result = transformData(data)
-  const { contractInfo, estateInfo } = props
-  console.log(estateInfo)
+  // const data =
+  //   '잔금일까지 해당 주택에 근저당 추가 설정하지 않는다/ 계약기간이 만료되면 새 임차인을 구하는 여부와 관계 없이 만료일에 보증금을 반환해준다/ 만기전 퇴거 시 새 임차인의 중개보수를 임차인이 부담한다/ 만기전 퇴거 시 새 임차인의 중개보수를 임차인이 부담한다.'
+  const { contractInfo, estateInfo, setShowContract } = props
+  const result = transformData(contractInfo.terms)
+  // console.log(estateInfo)
   return (
     <d.DetailCardContainer>
       <img
@@ -38,10 +39,12 @@ const ContractDetailCard = (props: PropType) => {
           <div className="detail">{estateInfo.roadNameAddress}</div>
         </d.InfoContainer>
         <d.InfoContainer>
+          <div className="title">건물명</div>
+          <div className="detail">{estateInfo.buildingName}</div>
+        </d.InfoContainer>
+        <d.InfoContainer>
           <div className="title">세부정보</div>
-          <div className="detail">
-            {estateInfo.buildingName} {estateInfo.roomNumber}호
-          </div>
+          <div className="detail">{estateInfo.roomNumber}호</div>
         </d.InfoContainer>
         <d.InfoContainer>
           <div className="title">매물 용도</div>
@@ -67,24 +70,29 @@ const ContractDetailCard = (props: PropType) => {
         </d.InfoContainer>
         <d.InfoContainer>
           <div className="title">임대기간</div>
-          <div className="detail">6 년</div>
+          <div className="detail">{contractInfo.leasePeriod} 년</div>
         </d.InfoContainer>
         <d.InfoContainer>
           <div className="title">보증금 및 월세</div>
-          <div className="detail"> {convertBigNumber(contractInfo.deposit._hex)} </div>
+          <div className="detail">
+            {' '}
+            {convertBigNumber(contractInfo.deposit._hex)}{' '}
+          </div>
         </d.InfoContainer>
         <d.InfoContainer>
           <div className="title">계약 날짜</div>
           <div className="detail">{contractInfo.contractDate}</div>
         </d.InfoContainer>
-        <d.TermsBox>
-          <div className="title">특약사항</div>
-          {result.map(term => (
-            <div className="terms">{term}</div>
-          ))}
-        </d.TermsBox>
+        {result.length !== 0 && (
+          <d.TermsBox>
+            <div className="title">특약사항</div>
+            {result.map(term => (
+              <div className="terms">{term}</div>
+            ))}
+          </d.TermsBox>
+        )}
       </d.InfoWrapper>
-      <d.CloseBtn>닫기</d.CloseBtn>
+      <d.CloseBtn onClick={() => setShowContract(false)}>닫기</d.CloseBtn>
     </d.DetailCardContainer>
   )
 }
