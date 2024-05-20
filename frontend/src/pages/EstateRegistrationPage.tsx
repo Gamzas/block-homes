f81a220f7c53c13ffc86c899fb6d7c6c2e2cbba7
정유나 <jyn7077@naver.com>
2024-05-20 09:59:31 +0900

    수정

--- frontend/src/pages/EstateRegistrationPage.tsx
+++ frontend/src/pages/EstateRegistrationPage.tsx
@@ -146,7 +146,7 @@ const EstateRegistrationPage = () => {
   }
 
   const getStatusMessage = steps => {
-    if (steps <= 1) return 1
+    if (steps <= 0) return 1
     if (steps <= 2) return 2
     return 3
   }
